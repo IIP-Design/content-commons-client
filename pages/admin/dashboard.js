400394d5afbdadac4230c4a6a0a8b8311fb6d096
Terri Regan <contact@terriregan.com>
2019-02-04 17:32:35 -0500

    Added dashboard page holder

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dashboard from 'components/admin/Dashboard/Dashboard';
+
+const DashboardPage = props => (
+  <Dashboard />
+);
+
+DashboardPage.propTypes = {
+
+};
+
+export default DashboardPage;
