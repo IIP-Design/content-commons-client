18b30a1d8d4a8f8b38b818e0db2fe560971b1d8c
Terri Regan <contact@terriregan.com>
2019-02-11 18:06:49 -0500

    Moved search reducers to results component folder

---
+++ components/Results/constants.js
@@ -0,0 +1,13 @@
+export const SEARCH_TERM_UPDATE = 'search_term_update';
+export const SEARCH_LANGUAGE_UPDATE = 'search_language_update';
+export const SEARCH_SORT_UPDATE = 'search_sort_update';
+
+export const SEARCH_REQUEST_PENDING = 'search_request_pending';
+export const SEARCH_REQUEST_FAILED = 'search_request_failed';
+export const SEARCH_REQUEST_SUCCESS = 'search_request_success';
+export const SEARCH_PAGE_PENDING = 'search_page_pending';
+export const SEARCH_PAGE_FAILED = 'search_page_failed';
+export const SEARCH_PAGE_SUCCESS = 'search_page_success';
+export const SEARCH_SORT_PENDING = 'search_sort_pending';
+export const SEARCH_SORT_FAILED = 'search_sort_failed';
+export const SEARCH_SORT_SUCCESS = 'search_sort_success';
