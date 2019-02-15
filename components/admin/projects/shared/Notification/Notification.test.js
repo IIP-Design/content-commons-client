bba890a4c8b7c6c6696deccda8886b9ca9c5b840
Edwin Mah <me@edwinmah.com>
2019-02-15 13:08:21 -0500

    Add test for Notification component

---
+++ components/admin/projects/shared/Notification/Notification.test.js
@@ -0,0 +1,56 @@
+import { shallow } from 'enzyme';
+import Notification from './Notification';
+
+const Component = <Notification msg="A test message" />;
+
+describe( '<Notification />', () => {
+  it( 'renders without crashing', () => {
+    const wrapper = shallow( Component );
+    expect( wrapper.exists() ).toEqual( true );
+  } );
+
+  it( 'renders as a default `p` tag', () => {
+    const wrapper = shallow( Component );
+    expect( wrapper.name() ).toEqual( 'p' );
+  } );
+
+  it( 'renders the notification message', () => {
+    const wrapper = shallow( Component );
+    expect( wrapper.children().text() )
+      .toEqual( 'A test message' );
+  } );
+
+  it( 'renders as a `div` tag if passed as prop', () => {
+    const wrapper = shallow( Component );
+    wrapper.setProps( { el: 'div' } );
+    expect( wrapper.name() ).toEqual( 'div' );
+  } );
+
+  it( 'applies default styles', () => {
+    const wrapper = shallow( Component );
+    expect( wrapper.prop( 'style' ) )
+      .toEqual( {
+        padding: '1em 1.5em',
+        fontSize: '0.75em',
+        backgroundColor: 'pear'
+      } );
+  } );
+
+  it( 'applies custom styles if passed as prop', () => {
+    const wrapper = shallow( Component );
+    wrapper.setProps( {
+      customStyles: {
+        color: '#fff',
+        fontSize: '2em',
+        backgroundColor: 'blue'
+      }
+    } );
+    expect( wrapper.prop( 'style' ) )
+      .toEqual( {
+        padding: '1em 1.5em',
+        fontSize: '2em',
+        backgroundColor: 'blue',
+        color: '#fff'
+      } );
+  } );
+} );
