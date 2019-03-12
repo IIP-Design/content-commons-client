ea46c81242a0ac1c72e8f14a11d4fab1a51e4bdc
Scott Gustas <scottius@gmail.com>
2019-03-12 11:51:07 -0400

    Implement Image Page

---
+++ components/Image/Image.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { object } from 'prop-types';
+import './Image.scss';
+
+const Image = props => {
+  const { item } = props;
+  return (
+    <section>{ item.title || 'Image Item' }</section>
+  );
+};
+
+Image.propTypes = {
+  item: object
+};
+
+export default Image;
