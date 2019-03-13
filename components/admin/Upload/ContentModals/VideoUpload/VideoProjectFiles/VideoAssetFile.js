91b87ae1a5a6fb48cbdf49452f330d1c9c4f64c7
Shawn Mathew <shawn.mathew@agencychief.com>
2019-03-13 17:58:55 -0400

    mobiel style edits, add toggle actions functions

--- components/admin/Upload/ContentModals/VideoUpload/VideoProjectFiles/VideoAssetFile.js
+++ components/admin/Upload/ContentModals/VideoUpload/VideoProjectFiles/VideoAssetFile.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Icon } from 'semantic-ui-react';
 import replaceIcon from '../../../../../../static/icons/icon_replace.svg';
 import removeIcon from '../../../../../../static/icons/icon_remove.svg';
 import '../../../../../../styles/tooltip.scss';
 import { truncateAndReplaceStr } from '../../../../../../lib/utils';
+import { isMobile } from '../../../../../../lib/browser';
 import {
   SelectLangaugeOptions,
   SelectTypeUseOptionsImages,
@@ -12,8 +13,23 @@ import {
   SelectSubtitleOptions,
   SelectQualityOptions
 } from './VideoAssetOptions';
+import './VideoAssetFile.scss';
 
 class VideoAssetFile extends PureComponent {
+  state = {
+    isMobileDevice: false,
+    actionsMenuActive: false
+  }
+
+  componentDidMount() {
+    const isMobileDevice = isMobile();
+    this.setState( { isMobileDevice } );
+  }
+
+  openAssetFileActionsMenu = () => this.setState( { actionsMenuActive: true } );
+
+  closeAssetFileActionsMenu = () => this.setState( { actionsMenuActive: false } );
+
   renderOptionsForVideo = Component => {
     const { file } = this.props;
     if ( file.indexOf( 'mp4' ) > -1 ) return <Component />;
@@ -47,6 +63,8 @@ class VideoAssetFile extends PureComponent {
       replaceVideoAssetFile
     } = this.props;
 
+    const { isMobileDevice, actionsMenuActive } = this.state;
+
     let fileDisplayName;
 
     if ( file.length > 30 ) {
@@ -57,15 +75,16 @@ class VideoAssetFile extends PureComponent {
 
     return (
       <Grid.Row className="videoProjectFiles_asset">
-        <Grid.Column width={ 6 }>
+        <Grid.Column mobile={ 16 } computer={ 6 }>
           <p className="videoProjectFiles_asset_file" title={ file }>{ fileDisplayName }</p>
         </Grid.Column>
-        <Grid.Column width={ 10 }>
+        <Grid.Column mobile={ 16 } computer={ 10 }>
           { activeStep === 'step_1' && <SelectLangaugeOptions /> }
           { activeStep === 'step_1' && this.renderOptionsForVideo( SelectSubtitleOptions ) }
           { activeStep === 'step_2' && this.renderTypeUseOptions() }
           { activeStep === 'step_2' && this.renderOptionsForVideo( SelectQualityOptions ) }
-          <div className="videoProjectFiles_asset_actionBtns">
+
+          <div className={ `videoProjectFiles_asset_actionBtns ${actionsMenuActive ? 'active' : ''}` }>
             <span tooltip="Replace">
               <label htmlFor="replace_videoAssetFile" className="videoProjectFiles_asset_replaceBtn">
                 <input
@@ -76,24 +95,34 @@ class VideoAssetFile extends PureComponent {
                   onChange={ replaceVideoAssetFile }
                 />
                 <button type="button" onClick={ () => this.replaceFile.click() }>
-                  <img
-                    src={ replaceIcon }
-                    alt="Replace Video File Button"
-                  />
+                  { !isMobileDevice && <img src={ replaceIcon } alt="Replace Video File Button" /> }
+                  { isMobileDevice && <span>Replace File</span> }
                 </button>
               </label>
             </span>
             <span tooltip="Remove">
               <button type="button" onClick={ () => removeVideoAssetFile( file ) }>
-                <img
-                  src={ removeIcon }
-                  alt="Remove Video File Button"
-                  className="videoProjectFiles_asset_removeBtn"
-                />
+                { !isMobileDevice && <img src={ removeIcon } alt="Remove Video File Button" className="videoProjectFiles_asset_removeBtn" /> }
+                { isMobileDevice && <span>Delete File</span> }
               </button>
             </span>
+            <span
+              className="actionsMenuCloseButton"
+              role="button"
+              onClick={ this.closeAssetFileActionsMenu }
+              onKeyPress={ this.closeAssetFileActionsMenu }
+              tabIndex="0"
+            />
           </div>
         </Grid.Column>
+        { isMobile && !actionsMenuActive && (
+          <Icon
+            name="ellipsis vertical"
+            className="mobileActionsMenuToggle"
+            onClick={ this.openAssetFileActionsMenu }
+            onKeyPress={ this.openAssetFileActionsMenu }
+          />
+        ) }
       </Grid.Row>
     );
   }
