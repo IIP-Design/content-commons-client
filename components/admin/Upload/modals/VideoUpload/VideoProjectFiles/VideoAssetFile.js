70f6acda7a68a45b08c85950fde983d00c370d22
Shawn Mathew <shawn.mathew@agencychief.com>
2019-03-15 17:59:10 -0400

    merge refactor

--- components/admin/Upload/modals/VideoUpload/VideoProjectFiles/VideoAssetFile.js
+++ components/admin/Upload/modals/VideoUpload/VideoProjectFiles/VideoAssetFile.js
@@ -1,43 +1,33 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Icon } from 'semantic-ui-react';
 import replaceIcon from '../../../../../../static/icons/icon_replace.svg';
 import removeIcon from '../../../../../../static/icons/icon_remove.svg';
 import '../../../../../../styles/tooltip.scss';
 import { truncateAndReplaceStr } from '../../../../../../lib/utils';
-import {
-  SelectLangaugeOptions,
-  SelectTypeUseOptionsImages,
-  SelectTypeUseOptionsVideo,
-  SelectSubtitleOptions,
-  SelectQualityOptions
-} from './VideoAssetOptions';
+import { isMobile } from '../../../../../../lib/browser';
+import VideoAssetOptions from '../VideoAssetOptions/VideoAssetOptions';
+import './VideoAssetFile.scss';
 
 class VideoAssetFile extends PureComponent {
-  renderOptionsForVideo = Component => {
-    const { file } = this.props;
-    if ( file.indexOf( 'mp4' ) > -1 ) return <Component />;
-
-    return (
-      <p className="videoProjectFiles_asset_options videoProjectFiles_asset_options--notApplicable">
-        Not Applicable
-      </p>
-    );
+  state = {
+    isMobileDevice: false,
+    actionsMenuActive: false,
+    selectOptionsActive: false
   }
 
-  renderTypeUseOptions = () => {
-    const { file } = this.props;
-    const isImage = ( /\.(gif|jpg|jpeg|tiff|png)$/i ).test( file );
-    const isVideo = file.indexOf( 'mp4' ) > -1;
-
-    if ( isImage ) return <SelectTypeUseOptionsImages />;
-    if ( isVideo ) return <SelectTypeUseOptionsVideo />;
-    return (
-      <p className="videoProjectFiles_asset_options videoProjectFiles_asset_options--notApplicable">
-        Not Applicable
-      </p>
-    );
+  componentDidMount() {
+    const isMobileDevice = isMobile();
+    this.setState( { isMobileDevice } );
   }
+
+  toggleAssetFileActionsMenu = () => this.setState( prevState => ( {
+    actionsMenuActive: !prevState.actionsMenuActive
+  } ) );
+
+  toggleSelectOptionsDisplay = () => this.setState( prevState => ( {
+    selectOptionsActive: !prevState.selectOptionsActive
+  } ) );
 
   render() {
     const {
@@ -47,8 +37,13 @@ class VideoAssetFile extends PureComponent {
       replaceVideoAssetFile
     } = this.props;
 
-    let fileDisplayName;
+    const {
+      isMobileDevice,
+      actionsMenuActive,
+      selectOptionsActive
+    } = this.state;
 
+    let fileDisplayName;
     if ( file.length > 30 ) {
       fileDisplayName = truncateAndReplaceStr( file, 18, 9 );
     } else {
@@ -57,15 +52,14 @@ class VideoAssetFile extends PureComponent {
 
     return (
       <Grid.Row className="videoProjectFiles_asset">
-        <Grid.Column width={ 6 }>
+        <Grid.Column mobile={ 16 } computer={ 6 }>
           <p className="videoProjectFiles_asset_file" title={ file }>{ fileDisplayName }</p>
         </Grid.Column>
-        <Grid.Column width={ 10 }>
-          { activeStep === 'step_1' && <SelectLangaugeOptions /> }
-          { activeStep === 'step_1' && this.renderOptionsForVideo( SelectSubtitleOptions ) }
-          { activeStep === 'step_2' && this.renderTypeUseOptions() }
-          { activeStep === 'step_2' && this.renderOptionsForVideo( SelectQualityOptions ) }
-          <div className="videoProjectFiles_asset_actionBtns">
+        <Grid.Column mobile={ 16 } computer={ 10 }>
+          <div className={ `videoProjectFiles_asset_options_wrapper ${selectOptionsActive ? 'active' : ''}` }>
+            <VideoAssetOptions isMobileDevice={ isMobileDevice } activeStep={ activeStep } file={ file } />
+          </div>
+          <div className={ `videoProjectFiles_asset_actionBtns ${actionsMenuActive ? 'active' : ''}` }>
             <span tooltip="Replace">
               <label htmlFor="replace_videoAssetFile" className="videoProjectFiles_asset_replaceBtn">
                 <input
@@ -76,24 +70,42 @@ class VideoAssetFile extends PureComponent {
                   onChange={ replaceVideoAssetFile }
                 />
                 <button type="button" onClick={ () => this.replaceFile.click() }>
-                  <img
-                    src={ replaceIcon }
-                    alt="Replace Video File Button"
-                  />
+                  { !isMobileDevice && <img src={ replaceIcon } alt="Replace Video File Button" /> }
+                  { isMobileDevice && <span>Replace File</span> }
                 </button>
               </label>
             </span>
             <span tooltip="Remove">
               <button type="button" onClick={ () => removeVideoAssetFile( file ) }>
-                <img
-                  src={ removeIcon }
-                  alt="Remove Video File Button"
-                  className="videoProjectFiles_asset_removeBtn"
-                />
+                { !isMobileDevice && <img src={ removeIcon } alt="Remove Video File Button" className="videoProjectFiles_asset_removeBtn" /> }
+                { isMobileDevice && <span>Delete File</span> }
               </button>
             </span>
+            <span
+              className="actionsMenuCloseButton"
+              role="button"
+              onClick={ this.toggleAssetFileActionsMenu }
+              onKeyPress={ this.toggleAssetFileActionsMenu }
+              tabIndex="0"
+            />
           </div>
         </Grid.Column>
+        <Icon
+          name={ `${selectOptionsActive ? 'chevron up' : 'chevron down'}` }
+          size="small"
+          className="mobileSelectOptionsToggle"
+          onClick={ this.toggleSelectOptionsDisplay }
+          onKeyPress={ this.toggleSelectOptionsDisplay }
+        />
+        { !actionsMenuActive && (
+          <Icon
+            name="ellipsis vertical"
+            size="small"
+            className="mobileActionsMenuToggle"
+            onClick={ this.toggleAssetFileActionsMenu }
+            onKeyPress={ this.toggleAssetFileActionsMenu }
+          />
+        ) }
       </Grid.Row>
     );
   }
