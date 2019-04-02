6a15184e2399754beb3a8aba9f97c3fffb1cedfa
Edwin Mah <me@edwinmah.com>
2019-04-02 08:06:14 -0400

    Rename videos to units

--- components/admin/projects/ProjectReview/VideoProjectFiles/VideoProjectFiles.js
+++ components/admin/projects/ProjectReview/VideoProjectFiles/VideoProjectFiles.js
@@ -23,28 +23,28 @@ const VideoProjectFiles = props => {
 
   if ( !project || !Object.keys( project ).length ) return null;
 
-  const { videos } = project;
+  const { units } = project;
 
   const handleEdit = () => {
     const { id } = props;
     Router.push( {
-      pathname: `/admin/project/video/${id}/edit`
+      pathname: `/admin/project/unit/${id}/edit`
     } );
   };
 
   return (
     <section className="section section--project_files project_files">
       <h3 className="project_files_headline">VIDEOS IN PROJECT</h3>
-      { videos.map( video => (
-        <div key={ video.title } className="project_file">
+      { units.map( unit => (
+        <div key={ unit.title } className="project_file">
           <Grid>
             <Grid.Row
               className={
-                `project_file_header ${video.files[0].language.textDirection}`
+                `project_file_header ${unit.files[0].language.textDirection}`
               }
             >
               <Grid.Column floated="left" mobile={ 8 }>
-                <h3 className="title">{ video.title }</h3>
+                <h3 className="title">{ unit.title }</h3>
               </Grid.Column>
               <Grid.Column floated="right" mobile={ 8 } className="project_file_edit">
                 <Button
@@ -57,28 +57,28 @@ const VideoProjectFiles = props => {
 
             <Grid.Row className="project_file_contents">
               <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_meta">
-                <img src={ video.thumbnails[0].image.url } alt={ video.thumbnails[0].image.alt } />
-                <p><span className="label">File Name:</span> { video.files[0].filename }</p>
-                <p><span className="label">Filesize:</span> { formatBytes( video.files[0].filesize ) }</p>
-                <p><span className="label">Dimensions:</span> { `${video.files[0].dimensions.width} x ${video.files[0].dimensions.height}` }</p>
-                <p><span className="label">Uploaded:</span> { formatDate( video.files[0].createdAt ) }</p>
-                <p><span className="label">Duration:</span> { millisToMinutesAndSeconds( video.files[0].duration ) }</p>
+                <img src={ unit.thumbnails[0].image.url } alt={ unit.thumbnails[0].image.alt } />
+                <p><span className="label">File Name:</span> { unit.files[0].filename }</p>
+                <p><span className="label">Filesize:</span> { formatBytes( unit.files[0].filesize ) }</p>
+                <p><span className="label">Dimensions:</span> { `${unit.files[0].dimensions.width} x ${unit.files[0].dimensions.height}` }</p>
+                <p><span className="label">Uploaded:</span> { formatDate( unit.files[0].createdAt ) }</p>
+                <p><span className="label">Duration:</span> { millisToMinutesAndSeconds( unit.files[0].duration ) }</p>
               </Grid.Column>
 
               <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_info">
-                <p><span className="label">Language:</span> { video.files[0].language.displayName }</p>
-                <p><span className="label">Subtitles & Captions:</span> { video.files[0].videoBurnedInStatus }</p>
-                <p><span className="label">Video Type:</span> { video.video_type }</p>
-                <p><span className="label">Quality:</span> { video.files[0].quality }</p>
+                <p><span className="label">Language:</span> { unit.files[0].language.displayName }</p>
+                <p><span className="label">Subtitles & Captions:</span> { unit.files[0].videoBurnedInStatus }</p>
+                <p><span className="label">Video Type:</span> { unit.video_type }</p>
+                <p><span className="label">Quality:</span> { unit.files[0].quality }</p>
                 <p
                   className={
-                    `public_description ${video.files[0].language.textDirection}`
+                    `public_description ${unit.files[0].language.textDirection}`
                   }
                 >
                   <span className="label">Public Description:</span>
-                  <span>{ video.descPublic }</span>
+                  <span>{ unit.descPublic }</span>
                 </p>
-                <p><span className="label">YouTube URL:</span> { video.files[0].stream.url }</p>
+                <p><span className="label">YouTube URL:</span> { unit.files[0].stream.url }</p>
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -97,7 +97,7 @@ const VIDEO_PROJECT_REVIEW_PROJECT_FILES_QUERY = gql`
   query VideoProjectReviewProjectFiles($id: ID!) {
     project: videoProject(id: $id) {
       id
-      videos: units {
+      units {
         id
         title
         descPublic
