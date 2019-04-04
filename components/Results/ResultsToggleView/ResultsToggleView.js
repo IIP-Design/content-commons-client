55a34337dce6f6d5a7ffc2182244a91309ef37ba
Shawn Mathew <shawn.mathew@agencychief.com>
2019-04-04 17:44:07 -0400

    fix isMobile execution error in results

--- components/Results/ResultsToggleView/ResultsToggleView.js
+++ components/Results/ResultsToggleView/ResultsToggleView.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { func, string } from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import { isMobile } from 'lib/browser';
 import './ResultsToggleView.scss';
 import 'styles/tooltip.scss';
 
-const ResultsToggleView = props => (
-  <div className="results_toggleView">
-    <span tooltip="Gallery View" className={ isMobile() ? 'noTooltip' : '' }>
-      <Icon
-        name="grid layout"
-        data-view="gallery"
-        onClick={ props.toggle }
-        className={ props.currentView === 'gallery' ? 'active' : '' }
-      />
-    </span>
-    <span tooltip="List View" className={ isMobile() ? 'noTooltip' : '' }>
-      <Icon
-        name="list"
-        data-view="list"
-        onClick={ props.toggle }
-        className={ props.currentView === 'list' ? 'active' : '' }
-      />
-    </span>
-  </div>
-);
+const ResultsToggleView = props => {
+  const [isMobileDevice, setIsMobileDevice] = useState( null );
+
+  useEffect( () => setIsMobileDevice( isMobile() ) );
+
+  return (
+    <div className="results_toggleView">
+      <span tooltip="Gallery View" className={ isMobileDevice ? 'noTooltip' : '' }>
+        <Icon
+          name="grid layout"
+          data-view="gallery"
+          onClick={ props.toggle }
+          className={ props.currentView === 'gallery' ? 'active' : '' }
+        />
+      </span>
+      <span tooltip="List View" className={ isMobileDevice ? 'noTooltip' : '' }>
+        <Icon
+          name="list"
+          data-view="list"
+          onClick={ props.toggle }
+          className={ props.currentView === 'list' ? 'active' : '' }
+        />
+      </span>
+    </div>
+  );
+};
 
 ResultsToggleView.propTypes = {
   toggle: func,
