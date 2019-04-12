9576db7666972359dab823173920858da619abed
Terri Regan <contact@terriregan.com>
2019-04-12 09:08:56 -0400

    Added name attribute to dropdown

--- components/admin/dropdowns/LanguageDropdown.js
+++ components/admin/dropdowns/LanguageDropdown.js
@@ -16,50 +16,45 @@ const LANGUAGES_QUERY = gql`
 `;
 
 
-const LanguageDropdown = props => {
-  const handleChange = ( e, data ) => {
-    props.onChange( e, data, 'language' );
-  };
+const LanguageDropdown = props => (
+  <Query query={ LANGUAGES_QUERY }>
+    { ( { data, loading, error } ) => {
+      if ( error ) return `Error! ${error.message}`;
 
-  return (
-    <Query query={ LANGUAGES_QUERY }>
-      { ( { data, loading, error } ) => {
-        if ( error ) return `Error! ${error.message}`;
+      let options = [];
+      if ( data && data.languages ) {
+        options = data.languages.map( lang => ( { key: lang.id, text: lang.displayName, value: lang.id } ) );
+      }
 
-        let options = [];
-        if ( data && data.languages ) {
-          options = data.languages.map( lang => ( { key: lang.id, text: lang.displayName, value: lang.id } ) );
-        }
+      return (
+        <Fragment>
+          { /* eslint-disable jsx-a11y/label-has-for */ }
+          <VisuallyHidden>
+            <label htmlFor={ props.id }>
+              { `${props.forFn} language` }
+            </label>
+          </VisuallyHidden>
 
-        return (
-          <Fragment>
-            { /* eslint-disable jsx-a11y/label-has-for */ }
-            <VisuallyHidden>
-              <label htmlFor={ props.id }>
-                { `${props.forFn} language` }
-              </label>
-            </VisuallyHidden>
-
-            <Dropdown
-              id={ props.id }
-              onChange={ handleChange }
-              options={ options }
-              placeholder="–"
-              value={ props.selected }
+          <Dropdown
+            id={ props.id }
+            name="language"
+            onChange={ props.onChange }
+            options={ options }
+            placeholder="–"
+            value={ props.selected }
               // error={ !selectedLanguage }
-              fluid
-              required={ props.required }
-              selection
-              loading={ loading }
-            />
-          </Fragment>
-        );
-      } }
+            fluid
+            required={ props.required }
+            selection
+            loading={ loading }
+          />
+        </Fragment>
+      );
+    } }
 
 
-    </Query>
-  );
-};
+  </Query>
+);
 
 LanguageDropdown.propTypes = {
   id: PropTypes.string,
--- components/admin/dropdowns/QualityDropdown.js
+++ components/admin/dropdowns/QualityDropdown.js
@@ -25,57 +25,52 @@ query IMAGE_QUALITY_QUERY {
 }
 `;
 
-const QualityDropdown = props => {
-  const handleChange = ( e, data ) => {
-    props.onChange( e, data, 'quality' );
-  };
+const QualityDropdown = props => (
+  <Query query={ props.type === 'video' ? VIDEO_QUALITY_QUERY : IMAGE_QUALITY_QUERY }>
+    { ( { data, loading, error } ) => {
+      if ( error ) return `Error! ${error.message}`;
 
-  return (
-    <Query query={ props.type === 'video' ? VIDEO_QUALITY_QUERY : IMAGE_QUALITY_QUERY }>
-      { ( { data, loading, error } ) => {
-        if ( error ) return `Error! ${error.message}`;
+      let options = [];
 
-        let options = [];
+      if ( data && data.__type && data.__type.enumValues ) {
+        options = data.__type.enumValues.map( quality => {
+          const { name } = quality;
+          return {
+            key: name,
+            text: `For ${name.toLowerCase()}`,
+            value: name
+          };
+        } );
+      }
 
-        if ( data && data.__type && data.__type.enumValues ) {
-          options = data.__type.enumValues.map( quality => {
-            const { name } = quality;
-            return {
-              key: name,
-              text: `For ${name.toLowerCase()}`,
-              value: name
-            };
-          } );
-        }
+      return (
+        <Fragment>
+          { /* eslint-disable jsx-a11y/label-has-for */ }
+          <VisuallyHidden>
+            <label htmlFor={ props.id }>
+              { `${props.forFn} quality` }
+            </label>
+          </VisuallyHidden>
 
-        return (
-          <Fragment>
-            { /* eslint-disable jsx-a11y/label-has-for */ }
-            <VisuallyHidden>
-              <label htmlFor={ props.id }>
-                { `${props.forFn} quality` }
-              </label>
-            </VisuallyHidden>
-
-            <Dropdown
-              id={ props.id }
-              onChange={ handleChange }
-              options={ options }
-              placeholder="–"
-              value={ props.selected }
+          <Dropdown
+            id={ props.id }
+            name="quality"
+            onChange={ props.onChange }
+            options={ options }
+            placeholder="–"
+            value={ props.selected }
               // error={ !selectedLanguage }
-              fluid
-              required={ props.required }
-              selection
-              loading={ loading }
-            />
-          </Fragment>
-        );
-      } }
-    </Query>
+            fluid
+            required={ props.required }
+            selection
+            loading={ loading }
+          />
+        </Fragment>
+      );
+    } }
+  </Query>
 
-  );
-};
+);
 
 QualityDropdown.propTypes = {
   id: PropTypes.string,
