0dce05bd54097447ab6ca939ea3b8efb7f030176
Terri Regan <contact@terriregan.com>
2019-04-12 09:04:31 -0400

    Renamed file to account for mobile view

--- components/admin/FileRemoveReplaceButtonGroup/FileRemoveReplaceButtonGroup.js
+++ components/admin/FileRemoveReplaceButtonGroup/FileRemoveReplaceButtonGroup.js
@@ -5,27 +5,24 @@ import replaceIcon from 'static/icons/icon_replace.svg';
 import removeIcon from 'static/icons/icon_remove.svg';
 import ButtonAddFiles from 'components/ButtonAddFiles/ButtonAddFiles';
 import 'styles/tooltip.scss'; // importing here so a rule can be overridden
-import './ActionsMenuFileUpload.scss';
+import './FileRemoveReplaceButtonGroup.scss';
 
-
-const ActionsMenuFileUpload = props => {
-  const handleOnRemove = () => {
-    props.onRemove( props.id );
-  };
-
-  const handleOnReplace = e => {
-    props.onReplace( props.id, e.target.files[0] );
-  };
+/**
+ * Displays replace/remove buttons as inline icons
+ * @param {object} props
+ */
+const FileRemoveReplaceButtonGroup = props => {
+  const { onReplace, onRemove } = props;
 
   return (
-    <Button.Group basic className="file-upload__btn-group">
-      <ButtonAddFiles onChange={ handleOnReplace }>
+    <Button.Group basic className="FileRemoveReplaceButtonGroup__btn-group">
+      <ButtonAddFiles onChange={ onReplace }>
         <span tooltip="Replace">
           <img src={ replaceIcon } alt="Replace Video File Button" />
         </span>
       </ButtonAddFiles>
 
-      <Button onClick={ handleOnRemove }>
+      <Button onClick={ onRemove }>
         <span tooltip="Remove">
           <img src={ removeIcon } alt="Remove Video File Button" />
         </span>
@@ -34,10 +31,9 @@ const ActionsMenuFileUpload = props => {
   );
 };
 
-ActionsMenuFileUpload.propTypes = {
-  id: PropTypes.string,
+FileRemoveReplaceButtonGroup.propTypes = {
   onReplace: PropTypes.func,
   onRemove: PropTypes.func
 };
 
-export default ActionsMenuFileUpload;
+export default FileRemoveReplaceButtonGroup;
