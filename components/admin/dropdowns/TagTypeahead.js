1261c8d18eda462cd14b6750e8a3a3bb9c68ea74
mrewers <mrewers@users.noreply.github.com>
2019-04-24 16:48:51 -0400

    Add tag dropdown

---
+++ components/admin/dropdowns/TagTypeahead.js
@@ -0,0 +1,115 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import VisuallyHidden from 'components/VisuallyHidden/VisuallyHidden';
+import { Form } from 'semantic-ui-react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { titleCase } from 'lib/utils';
+
+// const TAG_QUERY = gql`
+//   query TAG_QUERY( $id: ID! ) {
+//     tags(
+//       where: {
+//         translations_some: {
+//           language: { id: $id }
+//         }
+//       }
+//     ) {
+//       id
+//       translations(
+//         where: {
+//           language: { id: $id }
+//         }
+//       ) {
+//         id
+//         name
+//       }
+//     }
+//   }
+// `;
+
+const TAG_QUERY = gql`
+  query TAG_QUERY {
+    tags {
+      id
+      translations {
+        id
+        name
+        language { id }
+      }
+    }
+  }
+`;
+
+const TagTypeahead = props => {
+  const getTagsByLang = ( tags, langId ) => (
+    tags.map( tag => ( {
+      id: tag.id,
+      translations: tag.translations.filter( translation => translation.language.id === langId )
+    } ) )
+  );
+
+  return (
+    <Query query={ TAG_QUERY } variables={ { langId: props.langId } }>
+      { ( { data, loading, error } ) => {
+        if ( error ) return `Error! ${error.message}`;
+        if ( loading || !data ) return <p>Loading...</p>;
+
+        let options = [];
+
+        if ( data && data.tags ) {
+          const tagsInLang = getTagsByLang( data.tags, props.langId );
+
+          options = tagsInLang.map( tag => ( {
+            key: tag.id,
+            text: tag.translations.length
+              ? titleCase( tag.translations[0].name )
+              : '',
+            value: tag.id
+          } ) );
+        }
+
+        return (
+          <Fragment>
+            { !props.label && (
+
+              <VisuallyHidden>
+                { /* eslint-disable jsx-a11y/label-has-for */ }
+                <label htmlFor={ props.id }>
+                  { `${props.id} tag` }
+                </label>
+              </VisuallyHidden>
+            ) }
+
+            <Form.Dropdown
+              id={ props.id }
+              fluid
+              loading={ loading }
+              multiple
+              name="tags"
+              options={ options }
+              placeholder="–"
+              search
+              selection
+              { ...props }
+            />
+          </Fragment>
+        );
+      } }
+    </Query>
+  );
+};
+
+TagTypeahead.defaultProps = {
+  id: ''
+};
+
+TagTypeahead.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  langId: PropTypes.string
+};
+
+export default TagTypeahead;
+export { TAG_QUERY };
