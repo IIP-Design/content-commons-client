7df73bd30d90bee779cea5c9a3b51913efd4aa56
Edwin Mah <me@edwinmah.com>
2019-04-26 07:08:38 -0400

    Add temporary dropdown values for development

--- components/ScrollableTableWithMenu/TableItemsDisplay/index.js
+++ components/ScrollableTableWithMenu/TableItemsDisplay/index.js
@@ -10,10 +10,12 @@ import { Grid, Dropdown } from 'semantic-ui-react';
 import './TableItemsDisplay.scss';
 
 const displaySizeOptions = [
+  { key: 2, value: 2, text: '2' }, // temp value, for development
+  { key: 4, value: 4, text: '4' }, // temp value, for development
   { key: 25, value: 25, text: '25' },
   { key: 50, value: 50, text: '50' },
   { key: 75, value: 75, text: '75' },
-  { key: 100, value: 100, text: '100' }
+  { key: 100, value: 100, text: '100' },
 ];
 
 const TableItemsDisplay = props => {
