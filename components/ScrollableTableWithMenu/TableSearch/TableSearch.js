98a5347599f60e00f56f7c7e07657e9ca73fbc19
Edwin Mah <me@edwinmah.com>
2019-05-03 11:31:42 -0400

    Handle clear search field

--- components/ScrollableTableWithMenu/TableSearch/TableSearch.js
+++ components/ScrollableTableWithMenu/TableSearch/TableSearch.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Grid, Input } from 'semantic-ui-react';
+import {
+  Button, Form, Grid, Input
+} from 'semantic-ui-react';
+import VisuallyHidden from 'components/VisuallyHidden/VisuallyHidden';
 import './TableSearch.scss';
 
 const TableSearch = props => {
@@ -12,17 +15,40 @@ const TableSearch = props => {
 
   const handleSubmit = e => handleSearchSubmit( e, searchTerm );
 
+  const handleClear = e => {
+    setSearchTerm( '' );
+    handleSearchSubmit( e, '' );
+  };
+
   return (
-    <Grid.Column floated="right" width={ 5 } className="tableSearch_wrapper">
+    <Grid.Column floated="right" width={ 6 } className="tableSearch_wrapper">
       <Form onSubmit={ handleSubmit } size="tiny">
+        <VisuallyHidden>
+          <label htmlFor="search-projects">Search projects</label>
+        </VisuallyHidden>
         <Input
-          action={ { icon: 'search' } }
+          action
           className="tableSearch__searchTerm"
           fluid
+          id="search-projects"
           onChange={ handleChange }
           placeholder="search term"
           value={ searchTerm }
-        />
+        >
+          <input />
+          { searchTerm
+            && (
+              <Button
+                aria-label="clear search"
+                basic
+                className="clear"
+                type="button"
+                icon="delete"
+                onClick={ handleClear }
+              />
+            ) }
+          <Button aria-label="search" icon="search" />
+        </Input>
       </Form>
     </Grid.Column>
   );
