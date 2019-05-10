9c91fef3b2ddf31f1daf2c2cdf893d779ca7323e
Edwin Mah <me@edwinmah.com>
2019-05-10 11:03:22 -0400

    Refactor VisuallyHidden to render custom element

--- components/VisuallyHidden/VisuallyHidden.js
+++ components/VisuallyHidden/VisuallyHidden.js
@@ -5,15 +5,20 @@
  */
 
 import React from 'react';
-import { node } from 'prop-types';
+import PropTypes from 'prop-types';
 import './VisuallyHidden.scss';
 
-const VisuallyHidden = props => (
-  <div className="hide-visually">{ props.children }</div>
+const VisuallyHidden = ( { el: Element, children } ) => (
+  <Element className="hide-visually">{ children }</Element>
 );
 
 VisuallyHidden.propTypes = {
-  children: node
+  el: PropTypes.string,
+  children: PropTypes.node
+};
+
+VisuallyHidden.defaultProps = {
+  el: 'div'
 };
 
 export default VisuallyHidden;
