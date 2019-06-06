cdc5ec3c1c95534cecd3d9dca76ea5cc0f6ba9fe
Scott Gustas <scottius@gmail.com>
2019-06-06 09:39:24 -0400

    Refactor post display using hooks

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getItemRequest } from 'lib/elastic/api';
 import { normalizeItem } from 'lib/elastic/parser';
@@ -25,150 +25,112 @@ import Share from '../Share/Share';
 import EmbedPost from './EmbedPost';
 import EmbedHelp from './EmbedHelp';
 
-class Post extends Component {
-  constructor( props ) {
-    super( props );
-    const { item } = this.props;
-    this.state = {
-      item,
-      selectedLanguage: this.getLanguage(),
-      textDirection: item.language.text_direction
-    };
-    this.handleLanguageChange = this.handleLanguageChange.bind( this );
-  }
-
-  /**
-   * Update the url location
-   */
-  componentDidMount() {
-    this.willUpdateUrl();
-  }
-
-  shouldComponentUpdate( nextProps, nextState ) {
-    if ( this.state.item !== nextState.item ) return true;
-    return false;
-  }
-
-  componentDidUpdate() {
-    this.willUpdateUrl();
-  }
-
-  onFetchResult = ( response, value ) => {
-    if ( response && response.hits.total > 0 ) {
-      const item = normalizeItem( response.hits.hits[0] );
-      this.setState( {
-        item,
-        selectedLanguage: value,
-        textDirection: item.language.text_direction
-      } );
-    }
-  }
-
-  getLanguage() {
-    const { language } = this.props.item;
+const Post = props => {
+  const [item, setItem] = useState( props.item );
+  const [selectedLanguage, setSelectedLanguage] = useState( () => {
+    const { language } = props.item;
     if ( !language ) return 'English';
     return language.display_name;
-  }
+  } );
+  const [textDirection, setTextDirection] = useState( props.item.language.text_direction );
 
-  handleLanguageChange( value ) {
-    const { item } = this.state;
-    const language = item.languages.find( lang => lang.language.display_name === value );
+  useEffect( () => {
+    const language = item.languages.find( lang => lang.language.display_name === selectedLanguage );
     if ( language && language.post_id ) {
       getItemRequest( item.site, language.post_id )
-        .then( response => this.onFetchResult( response, value ) );
+        .then( response => {
+          if ( response && response.hits.total > 0 ) {
+            setItem( normalizeItem( response.hits.hits[0] ) );
+          }
+        } );
     }
-  }
+  }, [selectedLanguage] );
 
-  /**
-   * Update the location url the direct link to selected article
-   */
-  willUpdateUrl() {
-    const { pathname } = this.props.router;
-    const { id, site } = this.state.item;
+  useEffect( () => {
+    const { pathname } = props.router;
+    const { id, site, language } = item;
+    setTextDirection( language.text_direction );
     if ( id && site && pathname === '/article' ) {
       updateUrl( `/article?id=${id}&site=${site}` );
     }
-  }
+  }, [item] );
 
-  render() {
-    if ( this.state && this.state.item ) {
-      const { item, textDirection } = this.state;
-      const embedItem = (
-        // eslint-disable-next-line max-len
-        `<div id="cdp-article-embed"></div><script async id="cdpArticle" data-id="${item.id}" data-site="${item.site}" src="${process.env.REACT_APP_CDP_MODULES_URL}${process.env.REACT_APP_SINGLE_ARTICLE_MODULE}"></script>`
-      );
+  const embedItem = (
+    // eslint-disable-next-line max-len
+    `<div id="cdp-article-embed"></div><script async id="cdpArticle" data-id="${item.id}" data-site="${item.site}" src="${process.env.REACT_APP_CDP_MODULES_URL}${process.env.REACT_APP_SINGLE_ARTICLE_MODULE}"></script>`
+  );
 
-      return (
-        <ModalItem headline={ item.title } textDirection={ textDirection }>
-          <div className="modal_options">
-            <div className="modal_options_left">
-              <ModalLangDropdown
-                item={ this.props.item }
-                selected={ this.state.selectedLanguage }
-                handleLanguageChange={ this.handleLanguageChange }
-              />
-            </div>
-            <div className="trigger-container">
-              <PopupTrigger
-                toolTip="Embed this article"
-                icon={ { img: embedIcon, dim: 24 } }
-                show
-                content={ (
-                  <PopupTabbed
-                    title="Embed this article on your site"
-                    panes={ [
-                      {
-                        title: 'Copy Embed Code',
-                        component: (
-                          <EmbedPost
-                            instructions="Copy and paste the code below to embed article on your site"
-                            embedItem={ embedItem }
-                          />
-                        )
-                      },
-                      { title: 'Help', component: <EmbedHelp /> }
-                    ] }
-                  />
-) }
-              />
-              <PopupTrigger
-                toolTip="Share article"
-                icon={ { img: shareIcon, dim: 20 } }
-                show
-                content={ (
-                  <Popup title="Share this article.">
-                    <Share
-                      id={ item.id }
-                      language={ item.language.locale }
-                      link={ item.link }
-                      site={ item.site }
-                      title={ item.title }
-                      type={ item.type }
-                    />
-                  </Popup>
-) }
-              />
-            </div>
+  if ( item ) {
+    return (
+      <ModalItem headline={ item.title } textDirection={ textDirection }>
+        <div className="modal_options">
+          <div className="modal_options_left">
+            <ModalLangDropdown
+              item={ item }
+              selected={ selectedLanguage }
+              handleLanguageChange={ value => setSelectedLanguage( value ) }
+            />
           </div>
-          <ModalImage thumbnail={ item.thumbnail } thumbnailMeta={ item.thumbnailMeta } />
-          <ModalContentMeta type={ item.type } dateUpdated={ item.modified } />
-          <ModalText textContent={ item.content } />
-          <ModalPostMeta
-            type={ item.type }
-            author={ item.author }
-            sourcelink={ item.sourcelink }
-            logo={ item.logo }
-            source={ item.site }
-            datePublished={ item.published }
-            originalLink={ item.link }
-          />
-          <ModalPostTags tags={ item.categories } />
-        </ModalItem>
-      );
-    }
-    return <ModalItem headline="Content Unavailable" />;
+          <div className="trigger-container">
+            <PopupTrigger
+              toolTip="Embed this article"
+              icon={ { img: embedIcon, dim: 24 } }
+              show
+              content={ (
+                <PopupTabbed
+                  title="Embed this article on your site"
+                  panes={ [
+                    {
+                      title: 'Copy Embed Code',
+                      component: (
+                        <EmbedPost
+                          instructions="Copy and paste the code below to embed article on your site"
+                          embedItem={ embedItem }
+                        />
+                      )
+                    },
+                    { title: 'Help', component: <EmbedHelp /> }
+                  ] }
+                />
+              ) }
+            />
+            <PopupTrigger
+              toolTip="Share article"
+              icon={ { img: shareIcon, dim: 20 } }
+              show
+              content={ (
+                <Popup title="Share this article.">
+                  <Share
+                    id={ item.id }
+                    language={ item.language.locale }
+                    link={ item.link }
+                    site={ item.site }
+                    title={ item.title }
+                    type={ item.type }
+                  />
+                </Popup>
+              ) }
+            />
+          </div>
+        </div>
+        <ModalImage thumbnail={ item.thumbnail } thumbnailMeta={ item.thumbnailMeta } />
+        <ModalContentMeta type={ item.type } dateUpdated={ item.modified } />
+        <ModalText textContent={ item.content } />
+        <ModalPostMeta
+          type={ item.type }
+          author={ item.author }
+          sourcelink={ item.sourcelink }
+          logo={ item.logo }
+          source={ item.site }
+          datePublished={ item.published }
+          originalLink={ item.link }
+        />
+        <ModalPostTags tags={ item.categories } />
+      </ModalItem>
+    );
   }
-}
+  return <ModalItem headline="Content Unavailable" />;
+};
 
 Post.propTypes = {
   router: PropTypes.object,
