27612aa9838becad2185dee77330e9047b1d36c9
Scott Gustas <scottius@gmail.com>
2019-06-12 10:52:19 -0400

    Set enabled option

--- components/Register/SelectRole/SelectRole.js
+++ components/Register/SelectRole/SelectRole.js
@@ -55,9 +55,9 @@ const SelectRole = ( {
             checked={ values.permissions === option.value }
             onChange={ handleOnChange }
             error={ !!errors.permissions }
-            disabled={ option.value !== 'EDITOR' }
+            disabled={ !option.enabled }
           />
-          <p className={ `checkbox_content ${( option.value !== 'EDITOR' ) ? 'disabled' : ''}` }>{ option.content }</p>
+          <p className={ `checkbox_content ${( !option.enabled ) ? 'disabled' : ''}` }>{ option.content }</p>
         </Form.Field>
       ) ) }
       <p className="error-message">{ errors.permissions }</p>
