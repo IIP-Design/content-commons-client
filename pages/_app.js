dc426089d73253dc3c6b5b74f8ce7e7749fad593
Terri Regan <contact@terriregan.com>
2019-06-28 09:25:45 -0400

    Added workaround so that styles load in the development environment

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill';
 import App, { Container } from 'next/app';
+import Router from 'next/router';
 import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
@@ -10,6 +11,18 @@ import withApollo from 'hocs/withApollo';
 import Page from 'components/Page';
 import makeStore from 'lib/redux/store';
 import 'styles/styles.scss';
+
+// This is a bit of a hack to ensure styles reload on client side route changes.
+// See: https://github.com/zeit/next-plugins/issues/282#issuecomment-480740246
+if ( process.env.NODE_ENV !== 'production' ) {
+  Router.events.on( 'routeChangeComplete', () => {
+    const path = '/_next/static/css/styles.chunk.css';
+    const chunksSelector = `link[href*="${path}"]`;
+    const chunksNodes = document.querySelectorAll( chunksSelector );
+    const timestamp = new Date().valueOf();
+    chunksNodes[0].href = `${path}?${timestamp}`;
+  } );
+}
 
 class Commons extends App {
   static async getInitialProps( { Component, ctx } ) {
