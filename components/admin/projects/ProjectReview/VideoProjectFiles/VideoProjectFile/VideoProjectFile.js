ef1a37c4e0017e544d9047c9b73d259e1ebe2864
Edwin Mah <me@edwinmah.com>
2019-07-01 09:19:15 -0400

    Add video embed with thumbnail fallback

--- components/admin/projects/ProjectReview/VideoProjectFiles/VideoProjectFile/VideoProjectFile.js
+++ components/admin/projects/ProjectReview/VideoProjectFiles/VideoProjectFile/VideoProjectFile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Embed, Grid } from 'semantic-ui-react';
 import {
   formatBytes,
   formatDate,
   getS3Url,
   getStreamData,
+  getYouTubeId,
+  getVimeoId,
   secondsToHMS
 } from 'lib/utils';
 
@@ -30,20 +32,42 @@ const VideoProjectFile = props => {
   const youTubeUrl = getStreamData( stream, 'youtube', 'url' );
   const vimeoUrl = getStreamData( stream, 'vimeo', 'url' );
 
+  let thumbnailUrl = '';
+  let thumbnailAlt = `a thumbnail image for this file in ${language.displayName}`;
+  if ( thumbnail && Object.keys( thumbnail ).length > 0 ) {
+    thumbnailUrl = getS3Url( thumbnail.image.url );
+    thumbnailAlt = thumbnail.image.alt;
+  }
+
   return (
     <Grid.Row className="project_unit_files">
-      <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_meta">
-        { Object.keys( thumbnail ).length > 0
-          ? (
+      <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_embed_thumbnail">
+        { youTubeUrl && (
+          <Embed
+            id={ getYouTubeId( youTubeUrl ) }
+            placeholder={ thumbnailUrl }
+            source="youtube"
+          />
+        ) }
+        { ( !youTubeUrl && vimeoUrl ) && (
+          <Embed
+            id={ getVimeoId( vimeoUrl ) }
+            placeholder={ thumbnailUrl }
+            source="vimeo"
+          />
+        ) }
+        { ( !youTubeUrl && !vimeoUrl ) && (
+          <figure className="thumbnail overlay">
             <img
-              src={ getS3Url( thumbnail.image.url ) }
-              alt={ thumbnail.image.alt }
+              className="thumbnail-image"
+              src={ thumbnailUrl }
+              alt={ thumbnailAlt }
             />
-          )
-          : null }
+          </figure>
+        ) }
       </Grid.Column>
 
-      <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_info">
+      <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 8 } className="file_meta">
         <p><b className="label">File Name:</b> { filename }</p>
         <p><b className="label">Filesize:</b> { formatBytes( filesize ) }</p>
         <p><b className="label">Dimensions:</b> { `${width} x ${height}` }</p>
