527e968deeb8f9e0e76a3b13b90b733407dc361c
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2019-07-12 09:31:20 -0700

    Initial POC for using apollo subscriptions by migrating away from boost

--- components/ScrollableTableWithMenu/TableBody/TableBody.js
+++ components/ScrollableTableWithMenu/TableBody/TableBody.js
@@ -1,13 +1,13 @@
 import './TableBody.scss';
 import { Loader, Table } from 'semantic-ui-react';
-import { formatDate, getPathToS3Bucket } from 'lib/utils';
+import { formatDate, getPathToS3Bucket, maybeGetPathToS3 } from 'lib/utils';
 import ApolloError from 'components/errors/ApolloError';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
 import React from 'react';
 import TableRow from 'components/ScrollableTableWithMenu/TableRow/TableRow';
 import gql from 'graphql-tag';
 import orderBy from 'lodash/orderBy';
+import { compose, graphql } from 'react-apollo';
 
 const TEAM_VIDEO_PROJECTS_QUERY = gql`
   query VideoProjectsByTeam(
@@ -103,7 +103,7 @@ const normalizeData = videoProjects => {
       visibility: { value: videoProject.visibility },
       thumbnail: {
         value: {
-          url: videoProject.thumbnails && videoProject.thumbnails.length ? `${getPathToS3Bucket()}/${videoProject.thumbnails[0].url}` : '',
+          url: videoProject.thumbnails && videoProject.thumbnails.length ? `${maybeGetPathToS3( videoProject.thumbnails[0].url )}` : '',
           alt: videoProject.thumbnails && videoProject.thumbnails.length ? videoProject.thumbnails[0].alt : ''
         }
       },
@@ -122,108 +122,103 @@ const TableBody = props => {
     tableHeaders,
     toggleItemSelection,
     variables,
-    projectTab
+    projectTab,
+    data
   } = props;
 
+  const {
+    loading,
+    error,
+    videoProjects
+  } = data;
+
+  if ( loading ) {
+    return (
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>
+            <Loader active inline size="small" />
+            <span style={ { marginLeft: '0.5em', fontSize: '1.5em' } }>
+              Loading...
+            </span>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    );
+  }
+  if ( error ) {
+    return (
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>
+            <ApolloError error={ error } />
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    );
+  }
+
+  if ( data && !data.videoProjects ) return null;
+
+  if ( searchTerm && ( !videoProjects || !videoProjects.length ) ) {
+    return (
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>
+            No results for &ldquo;{ searchTerm }&rdquo;
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    );
+  }
+
+  if ( !videoProjects || !videoProjects.length ) {
+    return (
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>No projects</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    );
+  }
+
+  // Sort data by clicked column & direction
+  // Default sort by createdAt & DESC
+  const direction = props.direction ? `${props.direction === 'ascending' ? 'asc' : 'desc'}` : 'desc';
+
+  const tableData = orderBy(
+    normalizeData( videoProjects ),
+    tableDatum => {
+      let { column } = props;
+      if ( !column ) column = 'createdAt';
+      // Sort by parsed Date instead of String
+      if ( column === 'createdAt' || column === 'updatedAt' ) {
+        return Date.parse( tableDatum[column] );
+      }
+      // Format table data for case insensitive sorting
+      const formattedTableDatum = tableDatum[column].toString().toLowerCase();
+      return formattedTableDatum;
+    },
+    [direction]
+  );
+
+  // skip & first query vars are used as start/end slice() params to paginate tableData on client
+  const { skip, first } = variables;
+  const paginatedTableData = tableData.slice( skip, skip + first );
+
   return (
-    <Query
-      query={ TEAM_VIDEO_PROJECTS_QUERY }
-      variables={ { ...variables } }
-      fetchPolicy="cache-and-network"
-    >
-      { ( { loading, error, data } ) => {
-        if ( loading ) {
-          return (
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>
-                  <Loader active inline size="small" />
-                  <span style={ { marginLeft: '0.5em', fontSize: '1.5em' } }>
-                    Loading...
-                  </span>
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          );
-        }
-        if ( error ) {
-          return (
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>
-                  <ApolloError error={ error } />
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          );
-        }
-
-        if ( data && !data.videoProjects ) return null;
-
-        const { videoProjects } = data;
-
-        if ( searchTerm && !videoProjects.length ) {
-          return (
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>
-                  No results for &ldquo;{ searchTerm }&rdquo;
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          );
-        }
-
-        if ( !videoProjects.length ) {
-          return (
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>No projects</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          );
-        }
-
-        // Sort data by clicked column & direction
-        // Default sort by createdAt & DESC
-        const direction = props.direction ? `${props.direction === 'ascending' ? 'asc' : 'desc'}` : 'desc';
-
-        const tableData = orderBy(
-          normalizeData( videoProjects ),
-          tableDatum => {
-            let { column } = props;
-            if ( !column ) column = 'createdAt';
-            // Sort by parsed Date instead of String
-            if ( column === 'createdAt' || column === 'updatedAt' ) {
-              return Date.parse( tableDatum[column] );
-            }
-            // Format table data for case insensitive sorting
-            const formattedTableDatum = tableDatum[column].toString().toLowerCase();
-            return formattedTableDatum;
-          },
-          [direction]
-        );
-
-        // skip & first query vars are used as start/end slice() params to paginate tableData on client
-        const { skip, first } = variables;
-        const paginatedTableData = tableData.slice( skip, skip + first );
-
-        return (
-          <Table.Body className="projects">
-            { paginatedTableData.map( d => (
-              <TableRow
-                key={ d.id }
-                d={ d }
-                selectedItems={ selectedItems }
-                tableHeaders={ tableHeaders }
-                toggleItemSelection={ toggleItemSelection }
-                projectTab={ projectTab }
-              />
-            ) ) }
-          </Table.Body>
-        );
-      } }
-    </Query>
+    <Table.Body className="projects">
+      { paginatedTableData.map( d => (
+        <TableRow
+          key={ d.id }
+          d={ d }
+          selectedItems={ selectedItems }
+          tableHeaders={ tableHeaders }
+          toggleItemSelection={ toggleItemSelection }
+          projectTab={ projectTab }
+        />
+      ) ) }
+    </Table.Body>
   );
 };
 
@@ -234,8 +229,55 @@ TableBody.propTypes = {
   toggleItemSelection: PropTypes.func,
   variables: PropTypes.object,
   direction: PropTypes.string,
-  projectTab: PropTypes.string
+  projectTab: PropTypes.string,
+  data: PropTypes.shape( {
+    loading: PropTypes.bool,
+    error: PropTypes.object,
+    videoProjects: PropTypes.array
+  } )
+};
+const updateVideoProjects = ( data, updates ) => {
+  if ( data && data.videoProjects ) {
+    const { videoProjects } = data;
+    updates.forEach( update => {
+      const idx = videoProjects.findIndex( ( { id } ) => id === update.id );
+      if ( idx > -1 ) {
+        const project = videoProjects[idx];
+        videoProjects[idx] = {
+          ...project,
+          ...update
+        };
+      }
+    } );
+  }
+  return data;
 };
 
-export default TableBody;
+const VIDEO_STATUS_SUBSCRIPTION = gql`
+  subscription VideoStatusSubscription {
+    videoStatus {
+      id
+      status
+      visibility
+    }
+  }
+`;
+
+export default compose(
+  graphql( TEAM_VIDEO_PROJECTS_QUERY, {
+    options: props => ( {
+      variables: { ...props.variables },
+      fetchPolicy: 'cache-and-network'
+    } )
+  } ),
+  graphql( VIDEO_STATUS_SUBSCRIPTION, {
+    props: ( { data, ownProps } ) => {
+      if ( !data ) return ownProps;
+      if ( ownProps.data.videoProjects && data.videoStatus ) {
+        updateVideoProjects( ownProps, [data.videoStatus] );
+      }
+      return ownProps;
+    }
+  } )
+)( TableBody );
 export { TEAM_VIDEO_PROJECTS_QUERY };
--- hocs/withApollo.js
+++ hocs/withApollo.js
@@ -1,6 +1,12 @@
 import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import getConfig from 'next/config';
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { split, ApolloLink, Observable } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { onError } from 'apollo-link-error';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -13,20 +19,81 @@ const onErrorDev = ( ( { networkError, graphQLErrors } ) => {
 // Turn off default Apollo console.log errors
 const onErrorProd = ( () => {} );
 
-const createClient = ( { headers } ) => new ApolloClient( {
-  uri: publicRuntimeConfig.REACT_APP_APOLLO_ENDPOINT,
-  request: operation => {
-    operation.setContext( {
-      fetchOptions: {
-        credentials: 'include',
-      },
-      headers
-    } );
-  },
 
-  onError: onErrorDev
-  // onError: ( process.env.NODE_ENV === 'development' ) ? onErrorDev : onErrorProd
+// Setup the network "links"
+const wsLink = () => new WebSocketLink( {
+  uri: publicRuntimeConfig.REACT_APP_APOLLO_SUBSCRIPTION,
+  options: {
+    reconnect: true
+  }
 } );
+
+const httpLink = headers => new HttpLink( {
+  uri: publicRuntimeConfig.REACT_APP_APOLLO_ENDPOINT,
+  fetch,
+  fetchOptions: {},
+  credentials: 'same-origin',
+  headers: headers || {},
+} );
+
+
+const link = headers => split(
+  // split based on operation type
+  ( { query } ) => {
+    const { kind, operation } = getMainDefinition( query );
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink(),
+  httpLink( headers ),
+);
+
+const request = headers => operation => {
+  operation.setContext( {
+    fetchOptions: {
+      credentials: 'include',
+    },
+    headers
+  } );
+};
+
+const requestHandler = headers => new ApolloLink(
+  ( operation, forward ) => new Observable( observer => {
+    let handle;
+    Promise.resolve( operation )
+      .then( oper => request( headers )( oper ) )
+      .then( () => {
+        handle = forward( operation ).subscribe( {
+          next: observer.next.bind( observer ),
+          error: observer.error.bind( observer ),
+          complete: observer.complete.bind( observer ),
+        } );
+      } )
+      .catch( observer.error.bind( observer ) );
+
+    return () => {
+      if ( handle ) {
+        handle.unsubscribe();
+      }
+    };
+  } ),
+);
+
+const createClient = ( { headers } ) => {
+  const transports = [
+    onError( onErrorDev ),
+    requestHandler( headers )
+  ];
+  if ( process.browser ) {
+    transports.push( link( headers ) );
+  } else {
+    transports.push( httpLink( headers ) );
+  }
+
+  return new ApolloClient( {
+    link: ApolloLink.from( transports ),
+    cache: new InMemoryCache()
+  } );
+};
 
 
 // Second argument: { getDataFromTree: 'ssr' } : should the apollo store be hydrated before the first render ?,
