b3ff69439d41f214ba4f3b5da877c644faecc1c7
Shawn Mathew <shawn.mathew@agencychief.com>
2019-07-29 13:16:23 -0400

    CDP-1370 - add video file validation when adding files to be uploaded for video projects

--- components/admin/Upload/modals/VideoUpload/VideoProjectType/VideoProjectType.js
+++ components/admin/Upload/modals/VideoUpload/VideoProjectType/VideoProjectType.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Modal } from 'semantic-ui-react';
 import ButtonAddFiles from 'components/ButtonAddFiles/ButtonAddFiles';
+import IncludeVideoFileMsg from '../../IncludeVideoFileMsg/IncludeVideoFileMsg';
 import './VideoProjectType.scss';
 
 
 const VideoProjectType = props => {
+  const [includeVideoFileMsg, setIncludeVideoFileMsg] = useState( false );
+
   // since we are using a stateless function, use a hook for mouunting/unmouting calls
   useEffect( () => {
     props.updateModalClassname( 'upload_modal project-type-active' );
@@ -15,6 +18,14 @@ const VideoProjectType = props => {
 
   // only called if files are selected
   const handleOnChangeFiles = e => {
+    const { files } = e.target;
+    const fileTypes = Array.from( files ).map( file => file.type );
+    const includesVideoFile = fileTypes.includes('video/mp4') || fileTypes.includes('video/quicktime');
+
+    if ( !includesVideoFile ) {
+      return setIncludeVideoFileMsg( true );
+    }
+
     props.addAssetFiles( e.target.files );
     props.goNext();
   };
@@ -32,6 +43,10 @@ const VideoProjectType = props => {
         </Button>
         <ButtonAddFiles onChange={ handleOnChangeFiles } multiple>+ Add Files</ButtonAddFiles>
       </div>
+      <IncludeVideoFileMsg
+        includeVideoFileMsg={ includeVideoFileMsg }
+        setIncludeVideoFileMsg={ setIncludeVideoFileMsg }
+      />
     </Form>
 
   );
