cac98e3930b8a3987e71c343822fb550fd6b580f
Terri Regan <contact@terriregan.com>
2019-07-30 10:15:21 -0400

    Ejected Apollo Boost to allow for advanced configuration

--- hocs/withApollo.js
+++ hocs/withApollo.js
@@ -1,33 +1,86 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable, split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
 import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
 
-// TODO: Add more robsut error info
-const onErrorDev = ( ( { networkError, graphQLErrors } ) => {
-  if ( graphQLErrors ) console.log( 'graphQLErrors', graphQLErrors );
-  if ( networkError ) console.log( 'networkError', networkError );
+const request = async ( headers, operation ) => {
+  operation.setContext( {
+    headers
+  } );
+};
+
+
+const httpLink = new HttpLink( {
+  uri: publicRuntimeConfig.REACT_APP_APOLLO_ENDPOINT,
+  credentials: 'include'
 } );
 
-// Turn off default Apollo console.log errors
-const onErrorProd = ( () => {} );
+// if you instantiate on the server, the error will be thrown
+const wsLink = process.browser ? new WebSocketLink( {
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true,
+    lazy: true
+  }
+} ) : null;
+
+const link = process.browser ? split( // only create the split in the browser
+  // split based on operation type
+  ( { query } ) => {
+    const { kind, operation } = getMainDefinition( query );
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+) : httpLink;
 
 const createClient = ( { headers } ) => new ApolloClient( {
-  uri: publicRuntimeConfig.REACT_APP_APOLLO_ENDPOINT,
-  request: operation => {
-    operation.setContext( {
-      fetchOptions: {
-        credentials: 'include',
-      },
-      headers
-    } );
-  },
+  link: ApolloLink.from( [
+    onError( ( { graphQLErrors, networkError } ) => {
+      if ( process.env.NODE_ENV === 'development' ) {
+        if ( graphQLErrors ) {
+          // sendToLoggingService( graphQLErrors );
+          graphQLErrors.map( ( { message, locations, path } ) => console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ), );
+        }
+        if ( networkError ) {
+          // take another action if we have a network error?
+          console.log( `[Network error]: ${networkError}` );
+        }
+      }
+    } ),
 
-  onError: onErrorDev
-  // onError: ( process.env.NODE_ENV === 'development' ) ? onErrorDev : onErrorProd
+    new ApolloLink( ( operation, forward ) => new Observable( observer => {
+      let handle;
+      Promise.resolve( operation )
+        .then( oper => request( headers, oper ) )
+        .then( () => {
+          handle = forward( operation ).subscribe( {
+            next: observer.next.bind( observer ),
+            error: observer.error.bind( observer ),
+            complete: observer.complete.bind( observer ),
+          } );
+        } )
+        .catch( observer.error.bind( observer ) );
+
+      return () => {
+        if ( handle ) handle.unsubscribe();
+      };
+    } ) ),
+
+    link
+  ] ),
+
+  cache: new InMemoryCache()
 } );
-
 
 // Second argument: { getDataFromTree: 'ssr' } : should the apollo store be hydrated before the first render ?,
 // allowed values are always, never or ssr (don't hydrate on client side navigation)
