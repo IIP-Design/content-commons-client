c4fc8319d3e864222b4dd23c691db11eaedfec01
Edwin Mah <me@edwinmah.com>
2019-08-12 16:08:51 -0400

    Add tests for DeleteProjectsList

---
+++ components/ScrollableTableWithMenu/TableActionsMenu/DeleteProjects/DeleteProjectsList/mocks.js
@@ -0,0 +1,334 @@
+export const drafts = [
+  {
+    id: 'cjx0j3ts9066y0708k1hg2g1b',
+    createdAt: '2019-06-17T15:23:46.184Z',
+    updatedAt: '2019-06-17T15:23:46.184Z',
+    team: {
+      id: 'cjrkzhvku000f0756l44blw33',
+      name: 'IIP Video Production',
+      organization: 'Department of State'
+    },
+    author: {
+      id: 'cjrl1omfr002o075614zs8dxz',
+      firstName: 'Edwin',
+      lastName: 'Mah'
+    },
+    projectTitle: 'Philippines Village',
+    status: 'DRAFT',
+    visibility: 'PUBLIC',
+    thumbnails: [
+      {
+        id: 'cjx0j3xqz067z0708frd7sd9g',
+        url: '2019/06/cjx0j3ts9066y0708k1hg2g1b/coverr-philippines-village-views-1559889358075.jpg',
+        alt: null
+      }
+    ],
+    categories: [
+      {
+        id: 'cjubblzwq15r10756m11zntim',
+        translations: [
+          {
+            id: 'cjubbleh10v2607568m3kisep',
+            name: 'environment',
+            language: {
+              id: 'cjsq439dz005607560gwe7k3m',
+              locale: 'en-us'
+            }
+          },
+          {
+            id: 'cjubblehi0v2e0756ase6olan',
+            name: 'medioambiente',
+            language: {
+              id: 'cjsuxnpv700dl0756wg4adr6k',
+              locale: 'es-es'
+            }
+          },
+          {
+            id: 'cjubblehw0v2m075606oy0x39',
+            name: 'Environnement',
+            language: {
+              id: 'cjsq4565v005c0756f0lqbfe4',
+              locale: 'fr-fr'
+            }
+          },
+          {
+            id: 'cjubbleic0v2u075618dkevjf',
+            name: 'meio ambiente',
+            language: {
+              id: 'cjt5qc65o04020756oius55mh',
+              locale: 'pt-br'
+            }
+          },
+          {
+            id: 'cjubbleix0v320756q1xjh9pj',
+            name: 'البيئة',
+            language: {
+              id: 'cjsq47fum005i0756qlhc0wta',
+              locale: 'ar'
+            }
+          },
+          {
+            id: 'cjubblejc0v3a0756phz2der5',
+            name: 'محیط زیست',
+            language: {
+              id: 'cjubbldhj0uqh0756pco0zk2r',
+              locale: 'fa-ir'
+            }
+          },
+          {
+            id: 'cjubblejq0v3i0756lfud6sqg',
+            name: '环境',
+            language: {
+              id: 'cjsq4967o005o0756gvpl1f5k',
+              locale: 'zh-cn'
+            }
+          },
+          {
+            id: 'cjubblek60v3q07566bqtlpif',
+            name: 'Окружающая среда',
+            language: {
+              id: 'cjt06xh0e02q70756dnuo3g8e',
+              locale: 'ru-ru'
+            }
+          },
+          {
+            id: 'cjubblekm0v3y0756o0gk6nou',
+            name: 'lingkungan hidup',
+            language: {
+              id: 'cjt5qviod040v0756m5yhxs05',
+              locale: 'id-id'
+            }
+          },
+          {
+            id: 'cjubblel30v460756humkwrmu',
+            name: 'ماحول',
+            language: {
+              id: 'cjt5qnpem040e07567msmql7j',
+              locale: 'ur'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'cjx0sctfx06ry0708bmtuf37f',
+    createdAt: '2019-06-17T19:42:42.188Z',
+    updatedAt: '2019-06-17T19:42:42.188Z',
+    team: {
+      id: 'cjrkzhvku000f0756l44blw33',
+      name: 'IIP Video Production',
+      organization: 'Department of State'
+    },
+    author: {
+      id: 'cjrl1omfr002o075614zs8dxz',
+      firstName: 'Edwin',
+      lastName: 'Mah'
+    },
+    projectTitle: 'Philippines Village 2',
+    status: 'DRAFT',
+    visibility: 'PUBLIC',
+    thumbnails: [
+      {
+        id: 'cjx0sd0yy06t30708by0b4suo',
+        url: '2019/06/cjx0sctfx06ry0708bmtuf37f/coverr-philippines-village-views-1559889358075.jpg',
+        alt: null
+      }
+    ],
+    categories: [
+      {
+        id: 'cjubblzw915qe07560zkz9xas',
+        translations: [
+          {
+            id: 'cjubble530uxq0756q5bphm9c',
+            name: 'economic opportunity',
+            language: {
+              id: 'cjsq439dz005607560gwe7k3m',
+              locale: 'en-us'
+            }
+          },
+          {
+            id: 'cjubble5n0uxy0756q8qnn3cq',
+            name: 'Oportunidades económicas',
+            language: {
+              id: 'cjsuxnpv700dl0756wg4adr6k',
+              locale: 'es-es'
+            }
+          },
+          {
+            id: 'cjubble640uy607568seiatuw',
+            name: 'Opportunités économiques',
+            language: {
+              id: 'cjsq4565v005c0756f0lqbfe4',
+              locale: 'fr-fr'
+            }
+          },
+          {
+            id: 'cjubble6p0uye0756h6aw02vm',
+            name: 'Oportunidade econômica',
+            language: {
+              id: 'cjt5qc65o04020756oius55mh',
+              locale: 'pt-br'
+            }
+          },
+          {
+            id: 'cjubble7c0uym0756vp6lcupx',
+            name: 'فرص اقتصادية',
+            language: {
+              id: 'cjsq47fum005i0756qlhc0wta',
+              locale: 'ar'
+            }
+          },
+          {
+            id: 'cjubble800uyu07563abeywer',
+            name: 'فرصت های اقتصادی',
+            language: {
+              id: 'cjubbldhj0uqh0756pco0zk2r',
+              locale: 'fa-ir'
+            }
+          },
+          {
+            id: 'cjubble8k0uz20756wgu91l9d',
+            name: '经济机会',
+            language: {
+              id: 'cjsq4967o005o0756gvpl1f5k',
+              locale: 'zh-cn'
+            }
+          },
+          {
+            id: 'cjubble930uza0756rmnl6oij',
+            name: 'Экономические возможности',
+            language: {
+              id: 'cjt06xh0e02q70756dnuo3g8e',
+              locale: 'ru-ru'
+            }
+          },
+          {
+            id: 'cjubblea00uzi0756ciw0vz1j',
+            name: 'Peluang  Ekonomi',
+            language: {
+              id: 'cjt5qviod040v0756m5yhxs05',
+              locale: 'id-id'
+            }
+          },
+          {
+            id: 'cjubbleak0uzq0756l1x17v3p',
+            name: 'اقتصادی موقع',
+            language: {
+              id: 'cjt5qnpem040e07567msmql7j',
+              locale: 'ur'
+            }
+          }
+        ]
+      },
+      {
+        id: 'cjubblzwq15r10756m11zntim',
+        translations: [
+          {
+            id: 'cjubbleh10v2607568m3kisep',
+            name: 'environment',
+            language: {
+              id: 'cjsq439dz005607560gwe7k3m',
+              locale: 'en-us'
+            }
+          },
+          {
+            id: 'cjubblehi0v2e0756ase6olan',
+            name: 'medioambiente',
+            language: {
+              id: 'cjsuxnpv700dl0756wg4adr6k',
+              locale: 'es-es'
+            }
+          },
+          {
+            id: 'cjubblehw0v2m075606oy0x39',
+            name: 'Environnement',
+            language: {
+              id: 'cjsq4565v005c0756f0lqbfe4',
+              locale: 'fr-fr'
+            }
+          },
+          {
+            id: 'cjubbleic0v2u075618dkevjf',
+            name: 'meio ambiente',
+            language: {
+              id: 'cjt5qc65o04020756oius55mh',
+              locale: 'pt-br'
+            }
+          },
+          {
+            id: 'cjubbleix0v320756q1xjh9pj',
+            name: 'البيئة',
+            language: {
+              id: 'cjsq47fum005i0756qlhc0wta',
+              locale: 'ar'
+            }
+          },
+          {
+            id: 'cjubblejc0v3a0756phz2der5',
+            name: 'محیط زیست',
+            language: {
+              id: 'cjubbldhj0uqh0756pco0zk2r',
+              locale: 'fa-ir'
+            }
+          },
+          {
+            id: 'cjubblejq0v3i0756lfud6sqg',
+            name: '环境',
+            language: {
+              id: 'cjsq4967o005o0756gvpl1f5k',
+              locale: 'zh-cn'
+            }
+          },
+          {
+            id: 'cjubblek60v3q07566bqtlpif',
+            name: 'Окружающая среда',
+            language: {
+              id: 'cjt06xh0e02q70756dnuo3g8e',
+              locale: 'ru-ru'
+            }
+          },
+          {
+            id: 'cjubblekm0v3y0756o0gk6nou',
+            name: 'lingkungan hidup',
+            language: {
+              id: 'cjt5qviod040v0756m5yhxs05',
+              locale: 'id-id'
+            }
+          },
+          {
+            id: 'cjubblel30v460756humkwrmu',
+            name: 'ماحول',
+            language: {
+              id: 'cjt5qnpem040e07567msmql7j',
+              locale: 'ur'
+            }
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export const nonDrafts = [
+  {
+    id: 'cjx0jixoa06c60708z2i7t2t3',
+    createdAt: '2019-06-17T15:35:31.065Z',
+    updatedAt: '2019-06-17T15:35:31.065Z',
+    team: {
+      id: 'cjrkzhvku000f0756l44blw33',
+      name: 'IIP Video Production',
+      organization: 'Department of State'
+    },
+    author: {
+      id: 'cjuybpucc1tuk0756bpi84enp',
+      firstName: 'Jane',
+      lastName: 'Doe'
+    },
+    projectTitle: 'Made in America 14',
+    status: 'PUBLISHED',
+    visibility: 'PUBLIC',
+    thumbnails: [],
+    categories: [],
+  }
+];
