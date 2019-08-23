358d66812c23913861d6a875a577a0a4bafe5eab
Shawn Mathew <shawn.mathew@agencychief.com>
2019-08-23 13:08:42 -0400

    CDP-1370 - testing vid file notification display after upload

--- components/admin/projects/ProjectEdit/VideoEdit/VideoEdit.js
+++ components/admin/projects/ProjectEdit/VideoEdit/VideoEdit.js
@@ -347,7 +347,6 @@ const VideoEdit = props => {
           updateNotification={ updateNotification }
           handleUpload={ handleUpload }
           maxCategories={ MAX_CATEGORY_COUNT }
-          setDisableBtns={ setDisableBtns }
         />
       </div>
 
