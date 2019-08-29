3bbd85bd5c285011924b37978f78f9512f5a9b10
Terri Regan <contact@terriregan.com>
2019-08-29 08:16:26 -0400

    Added saving msg and disable actions during save/upload

--- components/admin/ProjectSupportFiles/EditSupportFiles/EditSupportFiles.js
+++ components/admin/ProjectSupportFiles/EditSupportFiles/EditSupportFiles.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Grid, Form, Button, Modal, Header
+  Grid, Form, Button, Modal, Header, Dimmer
 } from 'semantic-ui-react';
 import ButtonAddFiles from 'components/ButtonAddFiles/ButtonAddFiles';
 import { useFileActions } from 'lib/hooks/useFileActions';
+import Notification from 'components/Notification/Notification';
 import EditSupportFileRow from '../EditSupportFileRow/EditSupportFileRow';
 import './EditSupportFiles.scss';
 
@@ -12,6 +13,7 @@ const EditSupportFiles = ( {
   supportFiles, extensions, save
 } ) => {
   const [open, setOpen] = useState( false );
+  const [saving, setSaving] = useState( false );
   const [allFieldsSelected, setAllFieldsSelected] = useState( false );
 
   const {
@@ -22,6 +24,12 @@ const EditSupportFiles = ( {
     removeFile,
     replaceFile
   } = useFileActions();
+
+  const notificationStyles = {
+    position: 'absolute',
+    top: '15px',
+    right: '40px'
+  };
 
 
   /*
@@ -62,11 +70,14 @@ const EditSupportFiles = ( {
   };
 
   const handleSave = async () => {
+    setSaving( true );
     await save( files, filesToRemove );
+    setSaving( false );
     closeModal();
   };
 
   return (
+
     <Modal
       open={ open }
       onClose={ closeModal }
@@ -75,50 +86,66 @@ const EditSupportFiles = ( {
       trigger={ <Button className="btn--edit" onClick={ openModal } size="small" basic>Edit</Button> }
     >
       <Header content="Edit thumbnail files in this project" />
-      <Modal.Content>
-        <Form>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={ 8 }>Files Selected</Grid.Column>
-              <Grid.Column width={ 6 }>Language<span className="required"> *</span></Grid.Column>
-              <Grid.Column width={ 2 }></Grid.Column>
-            </Grid.Row>
 
-            { files.sort( compareFilenames ).map( file => (
-              <EditSupportFileRow
-                key={ file.id }
-                file={ file }
-                update={ updateField }
-                removeFile={ removeFile }
-                replaceFile={ replaceFile }
-                accept={ allowedExtensions }
-              />
-            ) ) }
-          </Grid>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button
-          className="secondary alternative"
-          content="Cancel"
-          onClick={ closeModal }
-        />
-        <ButtonAddFiles
-          onChange={ e => addFiles( e.target.files ) }
-          multiple
-          accept={ allowedExtensions }
-          className="secondary"
-        >Add Files
-        </ButtonAddFiles>
+      { saving
+      && (
+      <Notification
+        el="p"
+        show
+        customStyles={ notificationStyles }
+        msg="Saving changes..."
+      />
+      )
+    }
+      <Dimmer.Dimmable dimmed={ saving }>
+        <Dimmer inverted simple />
 
-        <Button
-          className="primary"
-          type="button"
-          content="Save"
-          onClick={ handleSave }
-          disabled={ !allFieldsSelected }
-        />
-      </Modal.Actions>
+        <Modal.Content>
+          <Form>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column width={ 8 }>Files Selected</Grid.Column>
+                <Grid.Column width={ 6 }>Language<span className="required"> *</span></Grid.Column>
+                <Grid.Column width={ 2 }></Grid.Column>
+              </Grid.Row>
+
+              { files.sort( compareFilenames ).map( file => (
+                <EditSupportFileRow
+                  key={ file.id }
+                  file={ file }
+                  update={ updateField }
+                  removeFile={ removeFile }
+                  replaceFile={ replaceFile }
+                  accept={ allowedExtensions }
+                />
+              ) ) }
+            </Grid>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            className="secondary alternative"
+            content="Cancel"
+            onClick={ closeModal }
+          />
+          <ButtonAddFiles
+            onChange={ e => addFiles( e.target.files ) }
+            multiple
+            accept={ allowedExtensions }
+            className="secondary"
+          >Add Files
+          </ButtonAddFiles>
+
+          <Button
+            className="primary"
+            type="button"
+            content="Save"
+            onClick={ handleSave }
+            disabled={ !allFieldsSelected }
+          />
+        </Modal.Actions>
+
+      </Dimmer.Dimmable>
     </Modal>
   );
 };
