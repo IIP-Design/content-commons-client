52fb5e7b5e10bcb6120d635df6c7cf2de409816f
Edwin Mah <me@edwinmah.com>
2019-09-03 13:11:58 -0400

    Update tests for ProjectSupportFiles

--- components/admin/ProjectSupportFiles/ProjectSupportFiles.test.js
+++ components/admin/ProjectSupportFiles/ProjectSupportFiles.test.js
@@ -1,17 +1,20 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import { projects, supportFilesConfig } from 'components/admin/ProjectEdit/mockData';
+import { config } from './VideoProjectSupportFiles/config';
 import ProjectSupportFiles from './ProjectSupportFiles';
+
+jest.mock(
+  './SupportFileTypeList/SupportFileTypeList',
+  () => function SupportFileTypeList() {
+    return '<SupportFileTypeList />';
+  }
+);
 
 const props = {
   heading: 'Support Files',
-  projectId: { videoID: 'made-in-america' },
-  supportFiles: projects[0].supportFiles,
-  hasSubmittedData: false,
-  protectImages: true,
-  handleChange: jest.fn(),
-  config: supportFilesConfig,
-  hasUploaded: true
+  projectId: '123',
+  save: jest.fn(),
+  config: config.supportFiles
 };
 
 const Component = <ProjectSupportFiles { ...props } />;
@@ -30,44 +33,10 @@ describe( '<ProjectSupportFiles />', () => {
     expect( heading.text() ).toEqual( props.heading );
   } );
 
-  it( 'renders the correct number of `SupportFileTypeList`s', () => {
+  it( 'renders the correct number of <SupportFileTypeList /> components', () => {
     const wrapper = shallow( Component );
     const lists = wrapper.find( 'SupportFileTypeList' );
-    const typesCount = Object.keys( props.config ).length;
+    const typesCount = Object.keys( props.config.types ).length;
     expect( lists ).toHaveLength( typesCount );
-  } );
-
-  it( 'renders a `Checkbox` for protect images if the type is `other`', () => {
-    const wrapper = shallow( Component );
-
-    expect( wrapper.find( 'Checkbox' ).exists() )
-      .toEqual( true );
-    wrapper.setProps( {
-      config: {
-        srt: {
-          headline: 'Test Files',
-          fileType: 'abc',
-          popupMsg: 'A test message'
-        }
-      }
-    } );
-    expect( wrapper.find( 'Checkbox' ).exists() )
-      .toEqual( false );
-  } );
-
-  it( 'checking/unchecking `Checkbox` calls `handleChange`', () => {
-    const wrapper = shallow( Component );
-    const checkbox = wrapper.find( 'Checkbox' );
-    if ( checkbox.exists() ) {
-      checkbox.simulate( 'change' );
-      expect( props.handleChange ).toHaveBeenCalled();
-    }
-  } );
-
-  it( 'does not render if no support files', () => {
-    const wrapper = shallow( Component );
-    wrapper.setProps( { supportFiles: {} } );
-    expect( wrapper.find( '.support-files' ) )
-      .toHaveLength( 0 );
   } );
 } );
