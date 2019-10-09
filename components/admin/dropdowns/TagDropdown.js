efac3cd8823f998463cd725fd535efc07159ab37
mrewers <mrewers@users.noreply.github.com>
2019-10-09 17:34:33 -0400

    Only pull in tags from specified locale

--- components/admin/dropdowns/TagDropdown.js
+++ components/admin/dropdowns/TagDropdown.js
@@ -8,82 +8,73 @@ import gql from 'graphql-tag';
 import { titleCase } from 'lib/utils';
 
 const TAG_QUERY = gql`
-  query TAG_QUERY {
+  query TAG_QUERY( $locale: String!) {
     tags {
       id
-      translations {
+      translations( where: {
+        language: {
+          locale: $locale
+        }
+      } ) {
         id
         name
-        language {
-          id
-          locale
-        }
       }
     }
   }
 `;
 
-const TagDropdown = props => {
-  const getTagsByLang = ( tags, locale ) => tags.map( tag => ( {
-    id: tag.id,
-    translations: tag.translations.filter(
-      translation => translation.language.locale === locale
-    )
-  } ) );
+const TagDropdown = props => (
+  <Query query={ TAG_QUERY } variables={ { locale: props.locale } }>
+    { ( { data, loading, error } ) => {
+      if ( error ) return 'Error!';
+      if ( loading || !data ) return <p>Loading...</p>;
 
-  return (
-    <Query query={ TAG_QUERY } variables={ { langId: props.locale } }>
-      { ( { data, loading, error } ) => {
-        if ( error ) return 'Error!';
-        if ( loading || !data ) return <p>Loading...</p>;
+      let options = [];
 
-        let options = [];
+      if ( data && data.tags ) {
+        const tagsInLang = data.tags;
+        options = tagsInLang
+          .map( tag => ( {
+            key: tag.id,
+            text: tag.translations.length
+              ? titleCase( tag.translations[0].name )
+              : '',
+            value: tag.id
+          } ) )
+          .sort( ( tagA, tagB ) => {
+            const textA = tagA.text.toLowerCase();
+            const textB = tagB.text.toLowerCase();
+            if ( textA < textB ) return -1;
+            if ( textA > textB ) return 1;
+            return 0;
+          } );
+      }
 
-        if ( data && data.tags ) {
-          const tagsInLang = getTagsByLang( data.tags, props.locale );
-          options = tagsInLang
-            .map( tag => ( {
-              key: tag.id,
-              text: tag.translations.length
-                ? titleCase( tag.translations[0].name )
-                : '',
-              value: tag.id
-            } ) )
-            .sort( ( tagA, tagB ) => {
-              const textA = tagA.text.toLowerCase();
-              const textB = tagB.text.toLowerCase();
-              if ( textA < textB ) return -1;
-              if ( textA > textB ) return 1;
-              return 0;
-            } );
-        }
+      return (
+        <Fragment>
+          { !props.label && (
+            <VisuallyHidden>
+              <label htmlFor={ props.id }>{ `${props.id} tag` }</label>
+            </VisuallyHidden>
+          ) }
 
-        return (
-          <Fragment>
-            { !props.label && (
-              <VisuallyHidden>
-                <label htmlFor={ props.id }>{ `${props.id} tag` }</label>
-              </VisuallyHidden>
-            ) }
-
-            <Form.Dropdown
-              className={ props.dir === 'RTL' ? 'rtl' : 'ltr' }
-              id={ props.id }
-              fluid
-              loading={ loading }
-              multiple
-              name="tags"
-              options={ options }
-              search
-              selection
-              { ...props }
-            />
-          </Fragment>
-        );
-      } }
-    </Query>
-  );
-};
+          <Form.Dropdown
+            className={ props.dir === 'RTL' ? 'rtl' : 'ltr' }
+            id={ props.id }
+            fluid
+            loading={ loading }
+            multiple
+            name="tags"
+            options={ options }
+            search
+            selection
+            { ...props }
+          />
+        </Fragment>
+      );
+    } }
+  </Query>
+);
 
 TagDropdown.defaultProps = {
   id: ''
