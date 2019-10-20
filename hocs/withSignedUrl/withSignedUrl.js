9ad3f25bcafb71590b2f22cd9f46cf092ad2dd78
Terri Regan <contact@terriregan.com>
2019-10-20 10:17:03 -0400

    Added hoc for signed urls

---
+++ hocs/withSignedUrl/withSignedUrl.js
@@ -0,0 +1,67 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { compose, graphql } from 'react-apollo';
+import {
+  SIGNED_S3_URL_PUT_MUTATION,
+  SIGNED_S3_URL_GET_MUTATION
+} from 'lib/graphql/queries/util';
+
+const withSignedUrl = WrappedComponent => {
+  class _withSignedUrl extends PureComponent {
+    static propTypes = {
+      getSignedS3UrlGet: PropTypes.func.isRequired,
+      getSignedS3UrlPut: PropTypes.func.isRequired
+    };
+
+    getSignedS3Url = async ( variables, mutation ) => mutation( {
+      variables
+    } ).catch( err => {
+      console.dir( err );
+    } );
+
+    getSignedUrlGet = async assetPath => {
+      try {
+        const res = await this.getSignedS3Url( {
+          key: assetPath
+        }, this.props.getSignedS3UrlGet );
+
+        return res.data.getSignedS3UrlGet.url;
+      } catch ( err ) {
+        console.log( `Unable to get signed url${err}` );
+        return null;
+      }
+    };
+
+    getSignedUrlPut = async assetPath => {
+      try {
+        const res = await this.getSignedS3Url( {
+          key: assetPath
+        }, this.props.getSignedS3UrlPut );
+
+        return res.data.getSignedS3UrlPut.url;
+      } catch ( err ) {
+        console.log( `Unable to get signed url${err}` );
+        return null;
+      }
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          { ...this.props }
+          getSignedUrlGet={ this.getSignedUrlGet }
+          getSignedUrlPut={ this.getSignedUrlPut }
+        />
+      );
+    }
+  }
+
+
+  return compose(
+    graphql( SIGNED_S3_URL_PUT_MUTATION, { name: 'getSignedS3UrlPut' } ),
+    graphql( SIGNED_S3_URL_GET_MUTATION, { name: 'getSignedS3UrlGet' } )
+  )( _withSignedUrl );
+};
+
+
+export default withSignedUrl;
