41b409dae41cefd1058c7109e311fb33e2f52123
Edwin Mah <me@edwinmah.com>
2019-10-23 09:51:39 -0400

    Move category data to mocks file

--- components/admin/dropdowns/CategoryDropdown/CategoryDropdown.test.js
+++ components/admin/dropdowns/CategoryDropdown/CategoryDropdown.test.js
@@ -3,6 +3,7 @@ import wait from 'waait';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { titleCase } from 'lib/utils';
 import CategoryDropdown, { CATEGORIES_QUERY } from './CategoryDropdown';
+import { categories } from './mocks';
 
 jest.mock( 'next-server/config', () => () => ( { publicRuntimeConfig: { REACT_APP_AWS_S3_AUTHORING_BUCKET: 's3-bucket-url' } } ) );
 
@@ -19,145 +20,7 @@ const mocks = [
       variables: { locale: props.locale }
     },
     result: {
-      data: {
-        categories: [
-          {
-            id: 'cjubblzw815qb0756cfwa9vd7',
-            translations: [
-              {
-                id: 'cjubbldkt0ur20756zjfw5209',
-                name: 'about america'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzw815qc07563nzatqyn',
-            translations: [
-              {
-                id: 'cjubbldsp0uta0756p4as8gl9',
-                name: 'arts & culture'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzw915qd0756808n3h3q',
-            translations: [
-              {
-                id: 'cjubbldz10uvi0756o7pg0lqm',
-                name: 'democracy & civil society'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzw915qe07560zkz9xas',
-            translations: [
-              {
-                id: 'cjubble530uxq0756q5bphm9c',
-                name: 'economic opportunity'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwk15qk0756nenwp3rz',
-            translations: [
-              {
-                id: 'cjubbleb70uzy0756tay96s62',
-                name: 'education'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwq15r10756m11zntim',
-            translations: [
-              {
-                id: 'cjubbleh10v2607568m3kisep',
-                name: 'environment'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwq15r20756m3108d8s',
-            translations: [
-              {
-                id: 'cjubbleli0v4e075641kjd0jj',
-                name: 'geography'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwq15r30756l8e8zdvu',
-            translations: [
-              {
-                id: 'cjubbler60v6m0756231jvsgj',
-                name: 'global issues'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwq15r40756kb7neely',
-            translations: [
-              {
-                id: 'cjubblevs0v8u075668mzno2t',
-                name: 'health'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwr15r507566hzkgmpk',
-            translations: [
-              {
-                id: 'cjubblf0v0vb207568iibzp1t',
-                name: 'human rights'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwr15r607568f7sy9pz',
-            translations: [
-              {
-                id: 'cjubblf5m0vda07565wesiogo',
-                name: 'press & journalism'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzwr15r70756o5jr69p9',
-            translations: [
-              {
-                id: 'cjubblfap0vfi0756pva51gji',
-                name: 'good governance'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzws15rb0756pi1gv0mn',
-            translations: [
-              {
-                id: 'cjubblffg0vhq0756gutom6cs',
-                name: 'religion & values'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzws15rc0756ypyls4bf',
-            translations: [
-              {
-                id: 'cjubblfkk0vjy0756zr2qwjii',
-                name: 'science & technology'
-              }
-            ]
-          },
-          {
-            id: 'cjubblzx615sc0756oh8pk89e',
-            translations: [
-              {
-                id: 'cjubblfp90vm60756cdzur3vo',
-                name: 'sports'
-              }
-            ]
-          }
-        ]
-      }
+      data: { categories }
     }
   }
 ];
@@ -258,7 +121,6 @@ describe( '<CategoryDropdown />', () => {
     wrapper.update();
     const formDropdown = wrapper.find( 'FormDropdown' );
     const dropdownItems = wrapper.find( 'DropdownItem' );
-    const { categories } = mocks[0].result.data;
     const semanticUICats = categories.map( category => ( {
       key: category.id,
       value: category.id,
