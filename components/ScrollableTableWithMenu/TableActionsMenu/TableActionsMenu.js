bef2de2a3314b61773d8b3d3a610c7d974128d86
Scott Gustas <scottius@gmail.com>
2019-11-05 09:46:27 -0500

    Reinstate unpublish button

--- components/ScrollableTableWithMenu/TableActionsMenu/TableActionsMenu.js
+++ components/ScrollableTableWithMenu/TableActionsMenu/TableActionsMenu.js
@@ -179,23 +179,17 @@ class TableActionsMenu extends React.Component {
     return arr;
   }
 
-  // return true to suppress unpublish all feature for initial beta release
-  hasSelectedAllDrafts = () => true;
+  hasSelectedAllDrafts = () => {
+    const draftProjects = this.getDraftProjects( this.props.teamVideoProjects.videoProjects || null );
+    const selections = this.getSelectedProjectsIds();
 
-  // hasSelectedAllDrafts = () => {
-  //   // return true to suppress unpublish all feature for initial beta release
-  //   return true;
-
-  //   const draftProjects = this.getDraftProjects( this.props.teamVideoProjects.videoProjects || null );
-  //   const selections = this.getSelectedProjectsIds();
-
-  //   if ( selections.length > 0 ) {
-  //     return selections.every(
-  //       id => draftProjects.includes( id )
-  //     );
-  //   }
-  //   return false;
-  // }
+    if ( selections.length > 0 ) {
+      return selections.every(
+        id => draftProjects.includes( id )
+      );
+    }
+    return false;
+  }
 
   showConfirmationMsg = () => {
     this.setState( {
