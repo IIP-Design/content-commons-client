0632a7eb9f3b6ae07f74f4934beb497652b932c6
Edwin Mah <me@edwinmah.com>
2019-11-13 08:39:32 -0500

    Update tests for UseDropdown

--- components/admin/dropdowns/UseDropdown/UseDropdown.test.js
+++ components/admin/dropdowns/UseDropdown/UseDropdown.test.js
@@ -3,7 +3,7 @@ import wait from 'waait';
 import { MockedProvider } from 'react-apollo/test-utils';
 import sortBy from 'lodash/sortBy';
 import { addEmptyOption } from 'lib/utils';
-import UseDropdown, { VIDEO_USE_QUERY, IMAGE_USE_QUERY } from './UseDropdown';
+import UseDropdown, { VIDEO_USE_QUERY, IMAGE_USE_QUERY, DOCUMENT_USE_QUERY } from './UseDropdown';
 
 const props = {
   id: '',
@@ -14,6 +14,12 @@ const props = {
 const imageProps = {
   ...props,
   type: 'Image'
+};
+
+const documentProps = {
+  id: '',
+  label: 'Release Type',
+  type: 'Document'
 };
 
 const mocks = [
@@ -82,6 +88,77 @@ const mocks = [
         ]
       }
     }
+  },
+  {
+    request: {
+      query: DOCUMENT_USE_QUERY
+    },
+    result: {
+      data: {
+        documentUses: [
+          {
+            id: 'ck2wbvj5h10kq0720hr0nkfgz',
+            name: 'Background Briefing'
+          },
+          {
+            id: 'ck2wbvj6010kz0720c358mbrt',
+            name: 'Department Press Briefing'
+          },
+          {
+            id: 'ck2wbvj6w10l80720gzhwlr9s',
+            name: 'Fact Sheet'
+          },
+          {
+            id: 'ck2wbvj7b10lg0720htdvmgru',
+            name: 'Interview'
+          },
+          {
+            id: 'ck2wbvj7u10lo07207aa55qmz',
+            name: 'Media Note'
+          },
+          {
+            id: 'ck2wbvj8810lx0720ruj5eylz',
+            name: 'Notice to the Press'
+          },
+          {
+            id: 'ck2wbvj8n10m50720eu2ob3pq',
+            name: 'On-the-record Briefing'
+          },
+          {
+            id: 'ck2wbvj8y10md0720vsuqxq87',
+            name: 'Press Guidance'
+          },
+          {
+            id: 'ck2wbvj9b10ml0720245vk3uy',
+            name: 'Remarks'
+          },
+          {
+            id: 'ck2wbvj9v10mt0720hwq7013q',
+            name: 'Speeches'
+          },
+          {
+            id: 'ck2wbvjaa10n20720fg5ayhn9',
+            name: 'Statement'
+          },
+          {
+            id: 'ck2wbvjao10na0720ncwefnm6',
+            name: 'Taken Questions'
+          },
+          {
+            id: 'ck2wbvjb210nj07205m16k3xx',
+            name: 'Travel Alert'
+          },
+          {
+            id: 'ck2wbvjbf10nr0720o6zpopyt',
+            name: 'Readout'
+          },
+          {
+            id: 'ck2wbvjbu10nz072060p67wk5',
+            name: 'Travel Warning'
+          }
+        ]
+      }
+    }
   }
 ];
 
@@ -94,6 +171,12 @@ const errorMocks = [
   },
   {
     ...mocks[1],
+    result: {
+      errors: [{ message: 'There was an error.' }]
+    }
+  },
+  {
+    ...mocks[2],
     result: {
       errors: [{ message: 'There was an error.' }]
     }
@@ -112,6 +195,12 @@ const nullMocks = [
     result: {
       data: { imageUses: null }
     }
+  },
+  {
+    ...mocks[2],
+    result: {
+      data: { documentUses: null }
+    }
   }
 ];
 
@@ -126,6 +215,12 @@ const emptyMocks = [
     ...mocks[1],
     result: {
       data: { imageUses: [] }
+    }
+  },
+  {
+    ...mocks[2],
+    result: {
+      data: { documentUses: [] }
     }
   }
 ];
@@ -289,7 +384,7 @@ describe( '<UseDropdown /> for image type', () => {
     expect( formDropdown.prop( 'options' ) ).toEqual( emptyOption );
   } );
 
-  it( 'does not crash if imageValues is []', async () => {
+  it( 'does not crash if imageUses is []', async () => {
     const wrapper = mount( EmptyComponent );
     await wait( 0 );
     wrapper.update();
@@ -316,6 +411,106 @@ describe( '<UseDropdown /> for image type', () => {
 
     expect( formDropdown.prop( 'options' ) ).toEqual( options );
     expect( dropdownItems.length ).toEqual( imageUses.length + 1 );
+  } );
+
+  it( 'assigns a matching id & htmlFor value to the Dropdown and label, respectively', async () => {
+    const wrapper = mount( Component );
+    await wait( 0 );
+    wrapper.update();
+    const dropdown = wrapper.find( 'Dropdown > div' );
+    const label = wrapper.find( 'label' );
+
+    expect( dropdown.prop( 'id' ) ).toEqual( props.id );
+    expect( label.prop( 'htmlFor' ) ).toEqual( props.id );
+  } );
+} );
+
+describe( '<UseDropdown /> for document type', () => {
+  const Component = (
+    <MockedProvider mocks={ mocks } addTypename={ false }>
+      <UseDropdown { ...documentProps } />
+    </MockedProvider>
+  );
+
+  const ErrorComponent = (
+    <MockedProvider mocks={ errorMocks } addTypename={ false }>
+      <UseDropdown { ...documentProps } />
+    </MockedProvider>
+  );
+
+  const NullComponent = (
+    <MockedProvider mocks={ nullMocks } addTypename={ false }>
+      <UseDropdown { ...documentProps } />
+    </MockedProvider>
+  );
+
+  const EmptyComponent = (
+    <MockedProvider mocks={ emptyMocks } addTypename={ false }>
+      <UseDropdown { ...documentProps } />
+    </MockedProvider>
+  );
+
+  it( 'renders loading state without crashing', () => {
+    const wrapper = mount( Component );
+    const dropdown = wrapper.find( 'UseDropdown' );
+    const formDropdown = wrapper.find( 'FormDropdown' );
+
+    expect( dropdown.exists() ).toEqual( true );
+    expect( formDropdown.prop( 'loading' ) ).toEqual( true );
+  } );
+
+  it( 'renders an error message if there is a GraphQL error', async () => {
+    const wrapper = mount( ErrorComponent );
+    await wait( 0 );
+    wrapper.update();
+    const dropdown = wrapper.find( 'UseDropdown' );
+    const error = errorMocks[2].result.errors[0];
+    const errorMsg = `Error! GraphQL error: ${error.message}`;
+
+    expect( dropdown.contains( errorMsg ) ).toEqual( true );
+  } );
+
+  it( 'does not crash if documentUses is null', async () => {
+    const wrapper = mount( NullComponent );
+    await wait( 0 );
+    wrapper.update();
+    const formDropdown = wrapper.find( 'FormDropdown' );
+    const emptyOption = [{
+      key: '-',
+      text: '-',
+      value: null
+    }];
+
+    expect( formDropdown.prop( 'options' ) ).toEqual( emptyOption );
+  } );
+
+  it( 'does not crash if documentUses is []', async () => {
+    const wrapper = mount( EmptyComponent );
+    await wait( 0 );
+    wrapper.update();
+    const formDropdown = wrapper.find( 'FormDropdown' );
+    const emptyOption = [{
+      key: '-',
+      text: '-',
+      value: null
+    }];
+
+    expect( formDropdown.prop( 'options' ) ).toEqual( emptyOption );
+  } );
+
+  it( 'renders the final state without crashing', async () => {
+    const wrapper = mount( Component );
+    await wait( 0 );
+    wrapper.update();
+    const formDropdown = wrapper.find( 'FormDropdown' );
+    const dropdownItems = wrapper.find( 'DropdownItem' );
+    const { documentUses } = mocks[2].result.data;
+    const semanticUIValues = sortBy( documentUses, use => use.name )
+      .map( u => ( { key: u.id, text: u.name, value: u.id } ) );
+    const options = addEmptyOption( semanticUIValues );
+
+    expect( formDropdown.prop( 'options' ) ).toEqual( options );
+    expect( dropdownItems.length ).toEqual( documentUses.length + 1 );
   } );
 
   it( 'assigns a matching id & htmlFor value to the Dropdown and label, respectively', async () => {
