c27f38c8788c63ea33de05ec1a8a66b5710b1676
Edwin Mah <me@edwinmah.com>
2019-11-15 12:14:43 -0500

    Add save notification

--- components/admin/PackageEdit/PackageEdit.js
+++ components/admin/PackageEdit/PackageEdit.js
@@ -4,6 +4,7 @@ import ButtonAddFiles from 'components/ButtonAddFiles/ButtonAddFiles';
 import ConfirmModalContent from 'components/admin/ConfirmModalContent/ConfirmModalContent';
 import FileUploadProgressBar from 'components/admin/ProjectEdit/FileUploadProgressBar/FileUploadProgressBar';
 import FormInstructions from 'components/admin/ProjectEdit/FormInstructions/FormInstructions';
+import Notification from 'components/Notification/Notification';
 import ProjectHeader from 'components/admin/ProjectHeader/ProjectHeader';
 import UploadSuccessMsg from 'components/admin/ProjectEdit/UploadSuccessMsg/UploadSuccessMsg';
 import PressPackageDetailsForm from './PackageDetailsForm/PressPackageDetailsForm/PressPackageDetailsForm';
@@ -16,6 +17,11 @@ const PackageEdit = () => {
   const [projectId, setProjectId] = useState( /* props.id */ '' );
   const [displayTheUploadSuccessMsg, setDisplayTheUploadSuccessMsg] = useState( false );
   const [deleteConfirmOpen, setDeleteConfirmOpen] = useState( false );
+
+  const [notification, setNotification] = useState( {
+    notificationMessage: '',
+    showNotification: false
+  } );
 
   const displayConfirmDelete = () => setDeleteConfirmOpen( true );
   const handleDeleteCancel = () => setDeleteConfirmOpen( false );
@@ -31,6 +37,22 @@ const PackageEdit = () => {
   const handleSaveExit = () => {
     console.log( 'Save & Exit' );
   };
+
+  const updateNotification = msg => {
+    setNotification( {
+      notificationMessage: msg,
+      showNotification: !!msg
+    } );
+  };
+
+  const centeredStyles = {
+    position: 'absolute',
+    top: '9em',
+    left: '50%',
+    transform: 'translateX(-50%)'
+  };
+
+  const { showNotification, notificationMessage } = notification;
 
   // for UI dev, remove afterwards
   const isUploading = false;
@@ -81,6 +103,14 @@ const PackageEdit = () => {
         </ProjectHeader>
       </div>
 
+      { /* Form data saved notification */ }
+      <Notification
+        el="p"
+        customStyles={ centeredStyles }
+        show={ showNotification }
+        msg={ notificationMessage }
+      />
+
       { /* upload progress */ }
       <div className="edit-package__status">
         { !projectId && !isUploading && <FormInstructions type="package" /> }
@@ -99,10 +129,10 @@ const PackageEdit = () => {
       <div className="edit-package__form">
         <PressPackageDetailsForm
           id={ testProps.id }
+          handleUpload={ () => {} }
+          updateNotification={ updateNotification }
           // send mock data here for UI dev, remove after GraphQL
           data={ mocks[0].result.data }
-          handleUpload={ () => {} }
-          updateNotification={ () => {} }
         />
       </div>
 
