2d347162fc968359c8cf9b4a722aff9f257bdf2a
Edwin Mah <me@edwinmah.com>
2019-11-18 11:31:00 -0500

    Add required form dropdowns to validationSchema

--- components/admin/PackageEdit/PackageDetailsForm/PressPackageDetailsForm/validationSchema.js
+++ components/admin/PackageEdit/PackageDetailsForm/PressPackageDetailsForm/validationSchema.js
@@ -2,7 +2,14 @@ import * as Yup from 'yup';
 
 const _baseSchema = {
   title: Yup.string()
-    .required( 'A package title is required.' )
+    .required( 'A package title is required.' ),
+  bureaus: Yup.array()
+    .required( 'At least 1 bureau is required.' ),
+  categories: Yup.array()
+    .max( 2, 'Maximum of 2 categories can be selected' )
+    .required( 'At least 1 category is required.' ),
+  visibility: Yup.string()
+    .required( 'A visibility setting is required.' )
 };
 
 const _initialSchema = {};
