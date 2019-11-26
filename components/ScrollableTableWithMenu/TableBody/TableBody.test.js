42029adc8b976b06b773340babbeaf79355faf77
Edwin Mah <me@edwinmah.com>
2019-11-26 13:06:31 -0500

    Add adjustments for updated Next & Apollo

--- components/ScrollableTableWithMenu/TableBody/TableBody.test.js
+++ components/ScrollableTableWithMenu/TableBody/TableBody.test.js
@@ -1,7 +1,7 @@
 import { mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import wait from 'waait';
-import { MockedProvider } from 'react-apollo/test-utils';
+import { MockedProvider } from '@apollo/react-testing';
 import { Loader, Table } from 'semantic-ui-react';
 import ApolloError from 'components/errors/ApolloError';
 import TableBody, {
@@ -13,8 +13,8 @@ import { videoProjects, mocks } from './mocks';
  * Use custom element to avoid "incorrect casing" error msg
  * @see https://jestjs.io/docs/en/tutorial-react.html#snapshot-testing-with-mocks-enzyme-and-react-16
  */
-// jest.mock( 'next-server/dynamic', () => () => 'VideoDetailsPopup' );
-jest.mock( 'next-server/dynamic', () => () => 'video-details-popup' );
+// jest.mock( 'next/dynamic', () => () => 'VideoDetailsPopup' );
+jest.mock( 'next/dynamic', () => () => 'video-details-popup' );
 
 // Mock DetailsPopup component since it's tested elsewhere
 jest.mock( 'components/admin/Dashboard/TeamProjects/DetailsPopup/DetailsPopup', () => () => 'DetailsPopup' );
@@ -51,6 +51,26 @@ const Component = (
 );
 
 describe( '<TableBody />', () => {
+  /**
+   * @todo Suppress React 16.8 `act()` warnings globally.
+   * The React team's fix won't be out of alpha until 16.9.0.
+   * @see https://github.com/facebook/react/issues/14769
+   */
+   const consoleError = console.error;
+   beforeAll( () => {
+     const actMsg = 'Warning: An update to %s inside a test was not wrapped in act';
+     jest.spyOn( console, 'error' ).mockImplementation( ( ...args ) => {
+       if ( !args[0].includes( actMsg ) ) {
+         consoleError( ...args );
+       }
+     } );
+   } );
+  
+   afterAll( () => {
+    // restore console.error
+    console.error = consoleError;
+  } );
+
   it( 'renders initial loading state without crashing', () => {
     const wrapper = mount( Component );
     const tableBody = wrapper.find( TableBody );
@@ -121,7 +141,7 @@ describe( '<TableBody />', () => {
 
     const tableBody = wrapper.find( TableBody );
 
-    expect( tableBody.html() ).toEqual( null );
+    expect( tableBody.html() ).toEqual( '' );
   } );
 
   it( 'renders a "No projects" message if there are no video projects', async () => {
