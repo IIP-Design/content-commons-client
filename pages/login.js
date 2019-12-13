f23876fac2633fdfb06ba5ff164efb07bc927cda
Scott Gustas <scottius@gmail.com>
2019-12-13 17:59:45 -0500

    CF auth updates

--- pages/login.js
+++ pages/login.js
@@ -2,29 +2,47 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Login from 'components/Login/Login';
 import LogoutButton from 'components/Logout/Logout';
+import Router from 'next/router';
 import User, { CURRENT_USER_QUERY } from 'components/User/User';
-import { redirectTo } from 'lib/browser';
+//import { redirectTo } from 'lib/browser';
 import cookies from 'next-cookies';
+import { graphql } from 'react-apollo';
+import compose from 'lodash.flowright';
 import jwt from 'jsonwebtoken';
 
 import { CLOUDFLARE_SIGNIN_MUTATION } from 'components/Login/CloudflareLogin';
 
 class LoginPage extends Component {
   static async getInitialProps ( ctx ) {
-    const cooks = cookies( ctx );
-    const cfAuth = cooks.CF_Authorization;
+    return { cookies: cookies( ctx ) };
+    // const cfAuth = cooks.CF_Authorization;
+    // if ( cfAuth ) {
+    //   const res = await ctx.apolloClient
+    //     .mutate( {
+    //       mutation: CLOUDFLARE_SIGNIN_MUTATION,
+    //       variables: { token: cfAuth },
+    //       refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    //     } )
+    //     .catch( err => console.dir( err ) );
+    //   console.log(res);
+    //   redirectTo( '/', ctx );
+    // }
+  }
 
-    if ( cfAuth ) {
-      await ctx.apolloClient
-        .mutate( {
-          mutation: CLOUDFLARE_SIGNIN_MUTATION,
-          variables: { token: cfAuth },
-          refetchQueries: [{ query: CURRENT_USER_QUERY }]
-        } )
-        .catch( err => console.dir( err ) );
-      redirectTo( '/', ctx );
+  state = {
+    attemptedCf: false
+  };
+
+  async componentDidMount() {
+    if ( !this.props.cookies.CF_Authorization ) this.setState( { attemptedCf: true } );
+    if ( !this.state.attemptedCf ) {
+      try {
+        await this.props.cloudflareSigninMutation();
+        Router.push('/');
+      } catch ( err ) {
+        this.setState( { attemptedCf: true } );
+      }
     }
-    return { cookies: cooks };
   }
 
   detectCloudflare = () => {
@@ -42,7 +60,7 @@ class LoginPage extends Component {
   render() {
     const cfToken = this.detectCloudflare();
     const email = ( cfToken ) ? this.decodeEmail( cfToken ) : '';
-
+    if ( !this.state.attemptedCf ) return false;
     return (
       <User>
         {
@@ -64,7 +82,17 @@ class LoginPage extends Component {
 }
 
 LoginPage.propTypes = {
-  cookies: PropTypes.object
+  cookies: PropTypes.object,
+  cloudflareSigninMutation: PropTypes.func
 };
 
-export default LoginPage;
+export default compose(
+  graphql( CLOUDFLARE_SIGNIN_MUTATION, {
+    name: 'cloudflareSigninMutation',
+    options: props => ( {
+      variables: { token: props.cookies.CF_Authorization },
+      refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    } )
+  } ),
+)( LoginPage );
+//export default LoginPage;
