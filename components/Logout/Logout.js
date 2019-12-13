11372734397f6f2f05351672eddc4269903588d6
Scott Gustas <scottius@gmail.com>
2019-12-13 14:50:03 -0500

    Cloudflare authentication updates

--- components/Logout/Logout.js
+++ components/Logout/Logout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Button } from 'semantic-ui-react';
-import Router from 'next/router';
 import { CURRENT_USER_QUERY } from '../User/User';
 
 
@@ -22,7 +21,6 @@ const Logout = props => (
         { ...props }
         onClick={ async () => {
           await signOut();
-          Router.push( { pathname: '/' } );
         } }
       >Logout
       </Button>
--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Login from 'components/Login/Login';
-import User from 'components/User/User';
+import LogoutButton from 'components/Logout/Logout';
+import User, { CURRENT_USER_QUERY } from 'components/User/User';
+import { redirectTo } from 'lib/browser';
+import cookies from 'next-cookies';
+import jwt from 'jsonwebtoken';
 
-const LoginPage = () => (
-  <User>
-    {
-      ( { data } ) => {
-        const user = data ? data.authenticatedUser : null;
-        return ( <Login user={ user } /> );
-      }
+import { CLOUDFLARE_SIGNIN_MUTATION } from 'components/Login/CloudflareLogin';
+
+class LoginPage extends Component {
+  static async getInitialProps ( ctx ) {
+    const cooks = cookies( ctx );
+    const cfAuth = cooks['CF_Authorization'];
+
+    if ( cfAuth ) {
+      await ctx.apolloClient
+        .mutate( {
+          mutation: CLOUDFLARE_SIGNIN_MUTATION,
+          variables: { token: cfAuth },
+          refetchQueries: [{ query: CURRENT_USER_QUERY }]
+        } )
+        .catch( err => console.dir( err ) );
+      redirectTo( '/', ctx );
     }
-  </User>
-);
+    return { cookies: cooks };
+  }
 
+  detectCloudflare = () => {
+    const cfAuth = this.props.cookies['CF_Authorization'];
+    if ( cfAuth ) return cfAuth;
+    return '';
+  }
+
+  decodeEmail = cfToken => {
+    const decoded = jwt.decode( cfToken, { complete: true } );
+    const email = ( decoded && decoded.payload ) ? decoded.payload.email : '';
+    return email;
+  }
+
+  render() {
+    const cfToken = this.detectCloudflare();
+    const email = ( cfToken ) ? this.decodeEmail( cfToken ) : '';
+
+    return (
+      <User>
+        {
+          ( { data } ) => {
+            const user = data ? data.authenticatedUser : null;
+            if ( user ) {
+              return (
+                <div className="login login_wrapper">
+                  <LogoutButton />
+                </div>
+              );
+            }
+            return <Login user={ user } cfToken={ cfToken } email={ email } />;
+          }
+        }
+      </User>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  cookies: PropTypes.object
+};
 
 export default LoginPage;
