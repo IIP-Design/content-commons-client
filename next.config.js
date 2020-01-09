0e0810a4b429ac7dc36ed14d1dc73279e5a4fdfa
mrewers <mrewers@users.noreply.github.com>
2020-01-09 14:40:17 -0500

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,32 +1,41 @@
-const withSass = require( '@zeit/next-sass' );
-const FilterWarningsPlugin = require( 'webpack-filter-warnings-plugin' );
-require( 'dotenv' ).config();
+const withSass = require('@zeit/next-sass');
 
-module.exports = withSass( {
-  publicRuntimeConfig: {
-    REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME,
-    REACT_APP_PUBLIC_API: process.env.REACT_APP_PUBLIC_API,
-    REACT_APP_APOLLO_ENDPOINT: process.env.REACT_APP_APOLLO_ENDPOINT,
-    REACT_APP_APOLLO_SUBSCRIPTIONS_ENDPOINT: process.env.REACT_APP_APOLLO_SUBSCRIPTIONS_ENDPOINT,
-    REACT_APP_GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
-    REACT_APP_GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    REACT_APP_VIMEO_TOKEN: process.env.REACT_APP_VIMEO_TOKEN,
-    REACT_APP_YOUTUBE_API_KEY: process.env.REACT_APP_YOUTUBE_API_KEY,
-    REACT_APP_AWS_S3_AUTHORING_BUCKET: process.env.REACT_APP_AWS_S3_AUTHORING_BUCKET,
-    REACT_APP_AWS_S3_PRODUCTION_BUCKET: process.env.REACT_APP_AWS_S3_PRODUCTION_BUCKET,
-    REACT_APP_SINGLE_ARTICLE_MODULE: process.env.REACT_APP_SINGLE_ARTICLE_MODULE,
-    REACT_APP_CDP_MODULES_URL: process.env.REACT_APP_CDP_MODULES_URL
-  },
-  serverRuntimeConfig: {},
-  poweredByHeader: false,
-  webpack ( config ) {
-    config.plugins.push(
-      new FilterWarningsPlugin( {
-        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
-      } )
-    );
-    config.module.rules.push(
-      {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+require('dotenv').config();
+
+module.exports = withBundleAnalyzer(
+  withSass({
+    publicRuntimeConfig: {
+      REACT_APP_WEBSITE_NAME: process.env.REACT_APP_WEBSITE_NAME,
+      REACT_APP_PUBLIC_API: process.env.REACT_APP_PUBLIC_API,
+      REACT_APP_APOLLO_ENDPOINT: process.env.REACT_APP_APOLLO_ENDPOINT,
+      REACT_APP_APOLLO_SUBSCRIPTIONS_ENDPOINT:
+        process.env.REACT_APP_APOLLO_SUBSCRIPTIONS_ENDPOINT,
+      REACT_APP_GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
+      REACT_APP_GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      REACT_APP_VIMEO_TOKEN: process.env.REACT_APP_VIMEO_TOKEN,
+      REACT_APP_YOUTUBE_API_KEY: process.env.REACT_APP_YOUTUBE_API_KEY,
+      REACT_APP_AWS_S3_AUTHORING_BUCKET:
+        process.env.REACT_APP_AWS_S3_AUTHORING_BUCKET,
+      REACT_APP_AWS_S3_PRODUCTION_BUCKET:
+        process.env.REACT_APP_AWS_S3_PRODUCTION_BUCKET,
+      REACT_APP_SINGLE_ARTICLE_MODULE:
+        process.env.REACT_APP_SINGLE_ARTICLE_MODULE,
+      REACT_APP_CDP_MODULES_URL: process.env.REACT_APP_CDP_MODULES_URL
+    },
+    serverRuntimeConfig: {},
+    poweredByHeader: false,
+    webpack(config) {
+      config.plugins.push(
+        new FilterWarningsPlugin({
+          exclude: /mini-css-extract-plugin[^]*Conflicting order between:/
+        })
+      );
+      config.module.rules.push({
         test: /\.(png|svg|jpg|eot|otf|ttf|woff|woff2)$/,
         use: {
           loader: 'url-loader',
@@ -37,9 +46,9 @@ module.exports = withSass( {
             name: '[name].[ext]'
           }
         }
-      }
-    );
+      });
 
-    return config;
-  }
-} );
+      return config;
+    }
+  })
+);
