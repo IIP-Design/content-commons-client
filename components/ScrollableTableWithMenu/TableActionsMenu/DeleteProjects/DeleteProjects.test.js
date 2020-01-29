46ea6dd92e0960729bc813dbcaadffb0c0781826
Shawn Mathew <shawn.mathew@agencychief.com>
2020-01-29 15:22:18 -0500

    CDP-1673 - update DeleteProject tests

--- components/ScrollableTableWithMenu/TableActionsMenu/DeleteProjects/DeleteProjects.test.js
+++ components/ScrollableTableWithMenu/TableActionsMenu/DeleteProjects/DeleteProjects.test.js
@@ -1,7 +1,8 @@
 import { mount } from 'enzyme';
-import wait from 'waait';
+import { MockedProvider, wait } from '@apollo/react-testing';
+// import wait from 'waait';
 import toJSON from 'enzyme-to-json';
-import { MockedProvider } from 'react-apollo/test-utils';
+// import { MockedProvider } from 'react-apollo/test-utils';
 import { getPluralStringOrNot } from 'lib/utils';
 import DeleteProjects from './DeleteProjects';
 import {
@@ -9,6 +10,12 @@ import {
 } from './mocks';
 
 const props = {
+  team: {
+    id: 'cjrkzhvku000f0756l44blw33',
+    name: 'IIP Video Production',
+    organization: 'Department of State',
+    contentTypes: ['VIDEO', 'PACKAGES']
+  },
   deleteConfirmOpen: false,
   handleActionResult: jest.fn(),
   handleDeleteCancel: jest.fn(),
@@ -17,6 +24,12 @@ const props = {
   deleteVideoProject: jest.fn(),
   selections: [...drafts, ...nonDrafts],
   showConfirmationMsg: jest.fn()
+};
+
+const displayProjectTypeText = () => {
+  if ( props.team.contentTypes.includes( 'VIDEO' ) ) return 'video';
+  if ( props.team.contentTypes.includes( 'PACKAGE' ) ) return 'package';
+  return '';
 };
 
 const openConfirmProps = {
@@ -50,7 +63,28 @@ const NonDraftsComponent = (
   </MockedProvider>
 );
 
+const suppressActWarning = consoleError => {
+  const actMsg = 'Warning: An update to %s inside a test was not wrapped in act';
+  jest.spyOn( console, 'error' ).mockImplementation( ( ...args ) => {
+    if ( !args[0].includes( actMsg ) ) {
+      consoleError( ...args );
+    }
+  } );
+};
+
 describe( '<DeleteProjects />', () => {
+  /**
+   * @todo Suppress React 16.8 `act()` warnings globally.
+   * The React team's fix won't be out of alpha until 16.9.0.
+   * @see https://github.com/facebook/react/issues/14769
+   */
+  const consoleError = console.error;
+  beforeAll( () => suppressActWarning( consoleError ) );
+
+  afterAll( () => {
+    console.error = consoleError;
+  } );
+
   it( 'renders without crashing', () => {
     const wrapper = mount( Component );
     const deleteMutation = wrapper.find( 'DeleteProjects' );
@@ -80,9 +114,9 @@ describe( '<DeleteProjects />', () => {
     const wrapper = mount( OpenConfirmComponent );
     const deleteMutation = wrapper.find( 'DeleteProjects' );
     const confirmModalContent = deleteMutation.find( 'ConfirmModalContent' );
-    const heading = `Are you sure you want to delete the selected DRAFT video ${getPluralStringOrNot( drafts, 'project' )}?`; // eslint-disable-line
-    const msg1 = `The following DRAFT video ${getPluralStringOrNot( drafts, 'project' )} will be removed permanently from the Content Cloud:`;
-    const msg2 = `${drafts.length > 0 && nonDrafts.length > 0 ? 'Only DRAFT video projects can be deleted from the dashboard. ' : ''}The following non-DRAFT video ${getPluralStringOrNot( nonDrafts, 'project' )} will NOT be removed:`;
+    const heading = `Are you sure you want to delete the selected DRAFT ${displayProjectTypeText()} ${getPluralStringOrNot( drafts, 'project' )}?`; // eslint-disable-line
+    const msg1 = `The following DRAFT ${displayProjectTypeText()} ${getPluralStringOrNot( drafts, 'project' )} will be removed permanently from the Content Cloud:`;
+    const msg2 = `${drafts.length > 0 && nonDrafts.length > 0 ? `Only DRAFT ${displayProjectTypeText()} projects can be deleted from the dashboard. ` : ''}The following non-DRAFT ${displayProjectTypeText()} ${getPluralStringOrNot( nonDrafts, 'project' )} will NOT be removed:`;
 
     expect( confirmModalContent.contains( heading ) ).toEqual( true );
     expect( confirmModalContent.contains( msg1 ) ).toEqual( true );
@@ -93,8 +127,8 @@ describe( '<DeleteProjects />', () => {
     const wrapper = mount( NonDraftsComponent );
     const deleteMutation = wrapper.find( 'DeleteProjects' );
     const confirmModalContent = deleteMutation.find( 'ConfirmModalContent' );
-    const heading = 'Only DRAFT video projects can be deleted from the dashboard.';
-    const msg = `The following non-DRAFT video ${getPluralStringOrNot( nonDrafts, 'project' )} will NOT be removed:`;
+    const heading = `Only DRAFT ${displayProjectTypeText()} projects can be deleted from the dashboard.`;
+    const msg = `The following non-DRAFT ${displayProjectTypeText()} ${getPluralStringOrNot( nonDrafts, 'project' )} will NOT be removed:`;
 
     expect( confirmModalContent.contains( heading ) ).toEqual( true );
     expect( confirmModalContent.contains( msg ) ).toEqual( true );
@@ -110,7 +144,7 @@ describe( '<DeleteProjects />', () => {
     expect( props.handleDeleteCancel ).toHaveBeenCalled();
   } );
 
-  it( 'clicking the confirm button calls handleActionResult with the apporpriate results', async () => {
+  it( 'clicking the confirm button calls handleActionResult with the appropriate results', async () => {
     const wrapper = mount( OpenConfirmComponent );
     const deleteProjects = wrapper.find( 'DeleteProjects' );
     const confirm = deleteProjects.find( 'Modal.delete' );
