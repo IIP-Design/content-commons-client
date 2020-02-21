cbf600b278e6c07eb71aa9b9ca5345de2271cd90
Edwin Mah <me@edwinmah.com>
2020-02-21 11:40:47 -0500

    Implement Next.js Google Analytics example

--- components/Meta.js
+++ components/Meta.js
@@ -1,9 +1,32 @@
 import Head from 'next/head';
 import React from 'react';
 import PropTypes from 'prop-types';
+import getConfig from 'next/config';
+
+const {
+  publicRuntimeConfig: {
+    REACT_APP_GOOGLE_ANALYTICS_ID
+  }
+} = getConfig();
 
 const Meta = props => (
   <Head>
+    <script
+      async
+      src={ `https://www.googletagmanager.com/gtag/js?id=${REACT_APP_GOOGLE_ANALYTICS_ID}` }
+    />
+    { /* eslint-disable-next-line */ }
+    <script dangerouslySetInnerHTML={ {
+      __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push( arguments ); }
+      gtag( 'js', new Date() );
+      gtag( 'config', '${REACT_APP_GOOGLE_ANALYTICS_ID}', {
+        page_path: window.location.pathname,
+      } );
+    `
+    } }
+    />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <meta property="og:site_name" content="Content Commons" />
--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import isEmpty from 'lodash/isEmpty';
 import withApollo from 'hocs/withApollo';
 import Page from 'components/Page';
 import makeStore from 'lib/redux/store';
+import * as gtag from 'lib/gtag';
 import 'styles/styles.scss';
 
 // This is a bit of a hack to ensure styles reload on client side route changes.
@@ -22,6 +23,10 @@ if ( process.env.NODE_ENV !== 'production' ) {
     const timestamp = new Date().valueOf();
     chunksNodes[0].href = `${path}?${timestamp}`;
   } );
+}
+
+if ( process.env.NODE_ENV === 'production' ) {
+  Router.events.on( 'routeChangeComplete', url => gtag.pageview( url ) );
 }
 
 class Commons extends App {
