23e3cb341b8ea8e2965e0be159318a2cd38cb81e
Shawn Mathew <shawn.mathew@agencychief.com>
2020-03-04 11:23:02 -0500

    CDP-1738 - convert GoogleLoginComponent to functional component, integrate redux useDispatch hook

--- components/Login/GoogleLogin.js
+++ components/Login/GoogleLogin.js
@@ -1,6 +1,5 @@
-import React, { useState, Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { userLoggedIn } from 'lib/redux/actions/authentication';
 import { GoogleLogin } from 'react-google-login';
 import { Button } from 'semantic-ui-react';
@@ -23,31 +22,29 @@ const GOOGLE_SIGNIN_MUTATION = gql`
   }
 `;
 
-class GoogleLoginComponent extends Component {
-  state = {
-    googleError: '',
-    accessToken: ''
-  }
+const GoogleLoginComponent = () => {
+  const dispatch = useDispatch();
+  const [googleError, setGoogleError] = useState( '' );
+  const [accessToken, setAccessToken] = useState( '' );
 
-  failureGoogle = response => {
+  const failureGoogle = response => {
     const { error, details } = response;
     console.log( details );
     // do show an error if user closes window w/o logging in
     if ( error !== 'popup_closed_by_user' ) {
-      this.setState( {
-        googleError: error
-      } );
+      setGoogleError( error );
     }
   };
 
   // Need to set state via function so that state is ready immediately
-  setToken = accessToken => {
-    this.setState( () => ( { accessToken, googleError: '' } ) );
-  }
+  const setToken = token => {
+    setAccessToken( token );
+    setGoogleError( '' );
+  };
 
   // A catch block is needed to prevent a browser console error
   // as the mutate function returns a promise
-  willGoogleSignin = async googleSigninMutation => {
+  const willGoogleSignin = async googleSigninMutation => {
     try {
       await googleSigninMutation();
       Router.push( {
@@ -56,49 +53,43 @@ class GoogleLoginComponent extends Component {
       // redirect to dashboard
       /* eslint-disable no-empty */
     } catch ( err ) {}
-  }
+  };
 
-  render() {
-    return (
-      <Mutation
-        mutation={ GOOGLE_SIGNIN_MUTATION }
-        variables={ { token: this.state.accessToken } }
-        refetchQueries={ [{
-          query: CURRENT_USER_QUERY,
-          fetchPolicy: 'network-only',
-          ssr: false
-        }] }
-      >
-        { ( googleSignin, { loading, error } ) => (
-          <div>
-            { /* <ApolloError error={ error } /> */ }
-            <Error error={ this.state.googleError } graphQLError={ error } />
-            <GoogleLogin
-              clientId={ publicRuntimeConfig.REACT_APP_GOOGLE_CLIENT_ID }
-              render={ renderProps => (
-                <Button loading={ loading } onClick={ renderProps.onClick }>Log in with America.gov</Button>
-              ) }
-              onSuccess={ async response => {
-                // 1. Fetch token from google and set on state to send to mutation
-                this.setToken( response.tokenId );
+  return (
+    <Mutation
+      mutation={ GOOGLE_SIGNIN_MUTATION }
+      variables={ { token: accessToken } }
+      refetchQueries={ [{
+        query: CURRENT_USER_QUERY,
+        fetchPolicy: 'network-only',
+        ssr: false
+      }] }
+    >
+      { ( googleSignin, { loading, error } ) => (
+        <div>
+          { /* <ApolloError error={ error } /> */ }
+          <Error error={ googleError } graphQLError={ error } />
+          <GoogleLogin
+            clientId={ publicRuntimeConfig.REACT_APP_GOOGLE_CLIENT_ID }
+            render={ renderProps => (
+              <Button loading={ loading } onClick={ renderProps.onClick }>Log in with America.gov</Button>
+            ) }
+            onSuccess={ async response => {
+              // 1. Fetch token from google and set on state to send to mutation
+              setToken( response.tokenId );
 
-                // 2. Send google token server to verfiy and fetch User
-                await this.willGoogleSignin( googleSignin );
+              // 2. Send google token server to verfiy and fetch User
+              await willGoogleSignin( googleSignin );
 
-                // 3. Set redux authenticated prop
-                this.props.userLoggedIn();
-              } }
-              onFailure={ this.failureGoogle }
-            />
-          </div>
-        ) }
-      </Mutation>
-    );
-  }
-}
-
-GoogleLoginComponent.propTypes = {
-  userLoggedIn: PropTypes.bool
+              // 3. Set redux authenticated prop
+              dispatch( userLoggedIn() );
+            } }
+            onFailure={ failureGoogle }
+          />
+        </div>
+      ) }
+    </Mutation>
+  );
 };
 
-export default connect( null, { userLoggedIn } )( GoogleLoginComponent );
+export default GoogleLoginComponent;
