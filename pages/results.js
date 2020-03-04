d41a5e6c69634dba14740995f5852d7d1dd6695e
Shawn Mathew <shawn.mathew@agencychief.com>
2020-03-04 16:33:49 -0500

    CDP-1738 - check if user logged in via cookies and update store in results.js & convert to functional component

--- pages/results.js
+++ pages/results.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import cookies from 'next-cookies';
 import Results from 'components/Results/Results/';
-
 import {
   updateLanguage, updateSort, updateSearchTerm, createRequest
 } from 'lib/redux/actions/search';
@@ -11,64 +11,68 @@ import { loadPostTypes } from 'lib/redux/actions/postType';
 import { loadSources } from 'lib/redux/actions/source';
 import { loadCategories } from 'lib/redux/actions/category';
 import { loadLanguages } from 'lib/redux/actions/language';
+import { userLoggedIn } from 'lib/redux/actions/authentication';
 
-class ResultsPage extends Component {
-  // Get search params from url
-  static async getInitialProps ( {
+const ResultsPage = () => <Results />;
+
+ResultsPage.getInitialProps = async ( ctx ) => {
+  const { americaCommonsToken } = cookies( ctx );
+  const {
     query: {
       language, term, sortBy, postTypes, date, categories, sources
-    }, store
-  } ) {
-    // trigger parallel loading calls
-    const languageUpdate = store.dispatch( updateLanguage( language || 'en-us' ) );
-    const sortByUpdate = store.dispatch( updateSort( sortBy || 'published' ) );
-    const typeUpdate = store.dispatch( postTypeUpdate( postTypes ) );
-    const termUpdate = store.dispatch( updateSearchTerm( term ) );
-    const dateChange = store.dispatch( dateUpdate( date ) );
-    const categoryChange = store.dispatch( categoryUpdate( categories ) );
-    const sourceChange = store.dispatch( sourceUpdate( sources ) );
+    },
+    store
+  } = ctx;
 
-    await languageUpdate;
-    await sortByUpdate;
-    await typeUpdate;
-    await termUpdate;
-    await dateChange;
-    await categoryChange;
-    await sourceChange;
+  // trigger parallel loading calls
+  const languageUpdate = store.dispatch( updateLanguage( language || 'en-us' ) );
+  const sortByUpdate = store.dispatch( updateSort( sortBy || 'published' ) );
+  const typeUpdate = store.dispatch( postTypeUpdate( postTypes ) );
+  const termUpdate = store.dispatch( updateSearchTerm( term ) );
+  const dateChange = store.dispatch( dateUpdate( date ) );
+  const categoryChange = store.dispatch( categoryUpdate( categories ) );
+  const sourceChange = store.dispatch( sourceUpdate( sources ) );  
 
-    // after all search values are updated, execute search request
-    store.dispatch( createRequest() );
-    // load filter menus if needed
-    const { global } = store.getState();
+  await languageUpdate;
+  await sortByUpdate;
+  await typeUpdate;
+  await termUpdate;
+  await dateChange;
+  await categoryChange;
+  await sourceChange;
 
-    // Always load srcs and cats based on query
-    const srcs = store.dispatch( loadSources() );
-    const cats = store.dispatch( loadCategories() );
-
-    let types;
-    let langs;
-
-    if ( !global.postTypes.list.length ) {
-      types = store.dispatch( loadPostTypes() );
-    }
-
-    if ( !global.languages.list.length ) {
-      langs = store.dispatch( loadLanguages() );
-    }
-
-    if ( types ) await types;
-    if ( srcs ) await srcs;
-    if ( cats ) await cats;
-    if ( langs ) await langs;
-
-    return {};
+  // If cookie is defined, then user is logged in, update authentication prop on store
+  if ( americaCommonsToken ) {
+    const isLoggedIn = store.dispatch( userLoggedIn() );
+    await isLoggedIn;
   }
 
-  render() {
-    return (
-      <Results />
-    );
+  // after all search values are updated, execute search request
+  store.dispatch( createRequest() );
+  // load filter menus if needed
+  const { global } = store.getState();
+
+  // Always load srcs and cats based on query
+  const srcs = store.dispatch( loadSources() );
+  const cats = store.dispatch( loadCategories() );
+
+  let types;
+  let langs;
+
+  if ( !global.postTypes.list.length ) {
+    types = store.dispatch( loadPostTypes() );
   }
-}
+
+  if ( !global.languages.list.length ) {
+    langs = store.dispatch( loadLanguages() );
+  }
+
+  if ( types ) await types;
+  if ( srcs ) await srcs;
+  if ( cats ) await cats;
+  if ( langs ) await langs;
+
+  return {};
+};
 
 export default ResultsPage;
