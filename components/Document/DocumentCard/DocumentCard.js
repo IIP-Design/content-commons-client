dd420e31465ef0b3aa49d2f2038c7b7edf64b48b
Edwin Mah <me@edwinmah.com>
2020-03-30 10:22:12 -0400

    Crop long excerpts

--- components/Document/DocumentCard/DocumentCard.js
+++ components/Document/DocumentCard/DocumentCard.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Card } from 'semantic-ui-react';
 import ReactMarkdown from 'react-markdown';
@@ -22,6 +22,13 @@ import './DocumentCard.scss';
 const DocumentCard = props => {
   const { isAdminPreview, file: doc } = props;
   const [isOpen, setIsOpen] = useState( false );
+  const [excerptHeight, setExcerptHeight] = useState( 0 );
+
+  const excerptRef = useCallback( node => {
+    if ( node !== null ) {
+      setExcerptHeight( node.getBoundingClientRect().height );
+    }
+  }, [] );
 
   if ( !doc || !getCount( doc ) ) return null;
 
@@ -105,12 +112,13 @@ const DocumentCard = props => {
             ? (
               <Fragment>
                 <p>Excerpt:</p>
-                <ReactMarkdown
-                  className="excerpt"
-                  source={ excerpt }
-                  escapeHtml={ false }
-                  astPlugins={ [parseHtml] }
-                />
+                <div ref={ excerptRef } className={ `excerpt${excerptHeight > 160 && ' clamped'}` }>
+                  <ReactMarkdown
+                    source={ excerpt }
+                    escapeHtml={ false }
+                    astPlugins={ [parseHtml] }
+                  />
+                </div>
               </Fragment>
             )
             : <p>No excerpt available</p> }
