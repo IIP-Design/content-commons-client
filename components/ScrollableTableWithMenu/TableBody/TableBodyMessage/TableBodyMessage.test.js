981282bc2c9abad405af32f3d34f774cdeccca10
mrewers <mrewers@users.noreply.github.com>
2020-04-28 17:30:04 -0400

    Wrap TableBody in a table component to avoid warning message about invalid nesting

--- components/ScrollableTableWithMenu/TableBody/TableBodyMessage/TableBodyMessage.test.js
+++ components/ScrollableTableWithMenu/TableBody/TableBodyMessage/TableBodyMessage.test.js
@@ -1,11 +1,23 @@
 import { mount } from 'enzyme';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Table } from 'semantic-ui-react';
 
 import TableBodyMessage from './TableBodyMessage';
 
+// Wrap TableBody in a table component to avoid warning message about invalid nesting of a <tbody>
+const createTable = messageProps => (
+  <Table>
+    <TableBodyMessage { ...messageProps } />
+  </Table>
+);
+
 describe( '<TableBodyMessage>', () => {
   it( 'renders error message if set to type error', () => {
-    const wrapper = mount( <TableBodyMessage error={ { message: 'There was an error.' } } type="error" /> );
+    const messageProps = {
+      error: { message: 'There was an error.' },
+      type: 'error'
+    };
+
+    const wrapper = mount( createTable( messageProps ) );
 
     const errorComponent = wrapper.find( 'ApolloError' );
 
@@ -13,7 +25,7 @@ describe( '<TableBodyMessage>', () => {
   } );
 
   it( 'renders a loader if set to type loading', () => {
-    const wrapper = mount( <TableBodyMessage type="loading" /> );
+    const wrapper = mount( createTable( { type: 'loading' } ) );
 
     const loader = <Loader active inline size="small" />;
 
@@ -22,21 +34,27 @@ describe( '<TableBodyMessage>', () => {
   } );
 
   it( 'renders the message "No projects" if set to type no-projects', () => {
-    const wrapper = mount( <TableBodyMessage type="no-projects" /> );
+    const wrapper = mount( createTable( { type: 'no-projects' } ) );
 
     expect( wrapper.contains( 'No projects' ) ).toEqual( true );
   } );
 
   it( 'renders a no results if set to type no-results', () => {
     const term = 'Test search term';
-    const wrapper = mount( <TableBodyMessage searchTerm={ term } type="no-results" /> );
+
+    const wrapper = mount( createTable( {
+      searchTerm: term,
+      type: 'no-results'
+    } ) );
 
     expect( wrapper.contains( `No results for &ldquo;${term}&rdquo;` ) ).toEqual( true );
   } );
 
   it( 'renders null if no type is passed', () => {
-    const wrapper = mount( <TableBodyMessage /> );
+    const wrapper = mount( createTable() );
 
-    expect( wrapper.html( ) ).toEqual( null );
+    const message = wrapper.find( 'TableBodyMessage' );
+
+    expect( message.html( ) ).toEqual( null );
   } );
 } );
