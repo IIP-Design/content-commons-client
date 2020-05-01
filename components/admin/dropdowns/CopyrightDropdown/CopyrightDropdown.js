ffb1b987b99dca12e882c936b57a56a45f7a7af3
Edwin Mah <me@edwinmah.com>
2020-05-01 08:02:13 -0400

    Refactor CopyrightDropdown for enum values

--- components/admin/dropdowns/CopyrightDropdown/CopyrightDropdown.js
+++ components/admin/dropdowns/CopyrightDropdown/CopyrightDropdown.js
@@ -1,61 +1,71 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import VisuallyHidden from 'components/VisuallyHidden/VisuallyHidden';
 import { Form } from 'semantic-ui-react';
-import sortBy from 'lodash/sortBy';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import VisuallyHidden from 'components/VisuallyHidden/VisuallyHidden';
 
 import '../dropdown.scss';
 
-export const data = [
-  {
-    id: 'jrzmg9415cxhhg5f89ke',
-    name: 'Copyright terms outlined in internal description',
-    value: 'Copyright terms outlined in internal description'
-  },
-  {
-    id: 'm2luyv4bowv18a6lvmwr',
-    name: 'No copyright beyond provided watermark attribution',
-    value: 'No copyright beyond provided watermark attribution'
+const COPYRIGHT_QUERY = gql`
+  query COPYRIGHT_QUERY {
+    __type(name: "Copyright"){
+     enumValues {
+       name
+     }
+    }
   }
-];
+`;
 
 const areEqual = ( prevProps, nextProps ) => prevProps.value === nextProps.value;
 
-const CopyrightDropdown = props => {
-  let options = [];
+const CopyrightDropdown = props => (
+  <Query query={ COPYRIGHT_QUERY }>
+    { ( { data, loading, error } ) => {
+      if ( error ) return `Error! ${error.message}`;
 
-  if ( data ) {
-    options = sortBy( data, obj => obj.name ).map( obj => ( {
-      key: obj.id,
-      text: obj.name,
-      value: obj.name
-    } ) );
-  }
+      let options = [];
 
-  return (
-    <Fragment>
-      { !props.label && (
+      if ( data?.__type?.enumValues ) {
+        options = data.__type.enumValues.map( obj => {
+          const text = obj.name === 'COPYRIGHT'
+            ? 'Copyright terms outlined in internal description'
+            : 'No copyright beyond provided watermark attribution';
 
-        <VisuallyHidden>
-          <label htmlFor={ props.id }>
-            { `${props.id} graphic styles` }
-          </label>
-        </VisuallyHidden>
-      ) }
+          return {
+            key: obj.name,
+            text,
+            value: obj.name
+          };
+        } );
+      }
 
-      <Form.Dropdown
-        id={ props.id }
-        name="copyright"
-        options={ options }
-        placeholder="–"
-        fluid
-        selection
-        { ...props }
-      />
-    </Fragment>
-  );
-};
+      return (
+        <Fragment>
+          { !props.label && (
 
+            <VisuallyHidden>
+              <label htmlFor={ props.id }>
+                { `${props.id} graphic styles` }
+              </label>
+            </VisuallyHidden>
+          ) }
+
+          <Form.Dropdown
+            id={ props.id }
+            name="copyright"
+            options={ options }
+            placeholder="–"
+            loading={ loading }
+            fluid
+            selection
+            { ...props }
+          />
+        </Fragment>
+      );
+    } }
+  </Query>
+);
 
 CopyrightDropdown.defaultProps = {
   id: ''
@@ -67,3 +77,4 @@ CopyrightDropdown.propTypes = {
 };
 
 export default React.memo( CopyrightDropdown, areEqual );
+export { COPYRIGHT_QUERY };
--- components/admin/dropdowns/CopyrightDropdown/CopyrightDropdown.test.js
+++ components/admin/dropdowns/CopyrightDropdown/CopyrightDropdown.test.js
@@ -1,46 +1,178 @@
 import { mount } from 'enzyme';
-import sortBy from 'lodash/sortBy';
-import CopyrightDropdown, { data } from './CopyrightDropdown';
+import wait from 'waait';
+import { MockedProvider } from '@apollo/react-testing';
+import CopyrightDropdown, { COPYRIGHT_QUERY } from './CopyrightDropdown';
+
+const props = {
+  id: 'copyright',
+  label: 'Copyright'
+};
+
+const mocks = [
+  {
+    request: {
+      query: COPYRIGHT_QUERY
+    },
+    result: {
+      data: {
+        __type: {
+          enumValues: [
+            { name: 'COPYRIGHT' },
+            { name: 'NO_COPYRIGHT' }
+          ]
+        }
+      }
+    }
+  }
+];
+
+const errorMocks = [
+  {
+    ...mocks[0],
+    result: {
+      errors: [{ message: 'There was an error.' }]
+    }
+  }
+];
+
+const nullMocks = [
+  {
+    ...mocks[0],
+    result: {
+      data: {
+        __type: { enumValues: null }
+      }
+    }
+  }
+];
+
+const emptyMocks = [
+  {
+    ...mocks[0],
+    result: {
+      data: {
+        __type: { enumValues: [] }
+      }
+    }
+  }
+];
+
+const suppressActWarning = consoleError => {
+  const actMsg = 'Warning: An update to %s inside a test was not wrapped in act';
+
+  jest.spyOn( console, 'error' ).mockImplementation( ( ...args ) => {
+    if ( !args[0].includes( actMsg ) ) {
+      consoleError( ...args );
+    }
+  } );
+};
 
 describe( '<CopyrightDropdown />', () => {
-  const props = {
-    id: '123xyz',
-    label: 'Copyright'
-  };
+  const consoleError = console.error;
 
-  const Component = <CopyrightDropdown { ...props } />;
-
-  let wrapper;
-  let dropdown;
-
-  beforeEach( () => {
-    wrapper = mount( Component );
-    dropdown = wrapper.find( 'CopyrightDropdown' );
+  beforeAll( () => suppressActWarning( consoleError ) );
+  afterAll( () => {
+    console.error = consoleError;
   } );
 
-  it( 'renders without crashing', () => {
+  const Component = (
+    <MockedProvider mocks={ mocks } addTypename={ false }>
+      <CopyrightDropdown { ...props } />
+    </MockedProvider>
+  );
+
+  const ErrorComponent = (
+    <MockedProvider mocks={ errorMocks } addTypename={ false }>
+      <CopyrightDropdown { ...props } />
+    </MockedProvider>
+  );
+
+  const NullComponent = (
+    <MockedProvider mocks={ nullMocks } addTypename={ false }>
+      <CopyrightDropdown { ...props } />
+    </MockedProvider>
+  );
+
+  const EmptyComponent = (
+    <MockedProvider mocks={ emptyMocks } addTypename={ false }>
+      <CopyrightDropdown { ...props } />
+    </MockedProvider>
+  );
+
+  it( 'renders loading state without crashing', () => {
+    const wrapper = mount( Component );
+    const dropdown = wrapper.find( 'CopyrightDropdown' );
+    const formDropdown = wrapper.find( 'FormDropdown' );
+
     expect( dropdown.exists() ).toEqual( true );
+    expect( formDropdown.prop( 'loading' ) ).toEqual( true );
   } );
 
-  it( 'renders the correct dropdown options', () => {
+  it( 'renders an error message if there is a GraphQL error', async () => {
+    const wrapper = mount( ErrorComponent );
+
+    await wait( 0 );
+    wrapper.update();
+    const dropdown = wrapper.find( 'CopyrightDropdown' );
+    const error = errorMocks[0].result.errors[0];
+    const errorMsg = `Error! GraphQL error: ${error.message}`;
+
+    expect( dropdown.contains( errorMsg ) ).toEqual( true );
+  } );
+
+  it( 'does not crash if enumValues is null', async () => {
+    const wrapper = mount( NullComponent );
+
+    await wait( 0 );
+    wrapper.update();
+    const formDropdown = wrapper.find( 'FormDropdown' );
+
+    expect( formDropdown.prop( 'options' ) ).toEqual( [] );
+  } );
+
+  it( 'does not crash if enumValues is []', async () => {
+    const wrapper = mount( EmptyComponent );
+
+    await wait( 0 );
+    wrapper.update();
+    const formDropdown = wrapper.find( 'FormDropdown' );
+
+    expect( formDropdown.prop( 'options' ) ).toEqual( [] );
+  } );
+
+  it( 'renders the final state without crashing', async () => {
+    const wrapper = mount( Component );
+
+    await wait( 0 );
+    wrapper.update();
     const formDropdown = wrapper.find( 'FormDropdown' );
     const dropdownItems = wrapper.find( 'DropdownItem' );
-    const options = sortBy( data, obj => obj.name )
-      .map( obj => ( {
-        key: obj.id,
-        text: obj.name,
+    const { enumValues } = mocks[0].result.data.__type;
+    const options = enumValues.map( obj => {
+      const text = obj.name === 'COPYRIGHT'
+        ? 'Copyright terms outlined in internal description'
+        : 'No copyright beyond provided watermark attribution';
+
+      return {
+        key: obj.name,
+        text,
         value: obj.name
-      } ) );
+      };
+    } );
 
     expect( formDropdown.prop( 'options' ) ).toEqual( options );
-    expect( dropdownItems.length ).toEqual( data.length );
+    expect( dropdownItems.length ).toEqual( enumValues.length );
   } );
 
-  it( 'assigns a matching id & htmlFor value to the Dropdown and label, respectively', () => {
-    const dropdownDiv = wrapper.find( 'Dropdown div[name="copyright"]' );
+  it( 'assigns a matching id & htmlFor value to the Dropdown and label, respectively', async () => {
+    const wrapper = mount( Component );
+
+    await wait( 0 );
+    wrapper.update();
+    const dropdown = wrapper.find( 'Dropdown div[name="copyright"]' );
     const label = wrapper.find( 'label' );
 
-    expect( dropdownDiv.prop( 'id' ) ).toEqual( props.id );
+    expect( dropdown.prop( 'id' ) ).toEqual( props.id );
     expect( label.prop( 'htmlFor' ) ).toEqual( props.id );
   } );
 } );
