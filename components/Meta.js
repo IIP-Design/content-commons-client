7ee39f3bf4412fb36dd1e377954caeeb14832e97
Steven Hascher <shascher@gmail.com>
2020-05-11 14:16:08 -0400

    Updates Meta Head with GTM code production snippet

--- components/Meta.js
+++ components/Meta.js
@@ -1,38 +1,25 @@
 import Head from 'next/head';
 import React from 'react';
 import PropTypes from 'prop-types';
-import getConfig from 'next/config';
-
-const {
-  publicRuntimeConfig: {
-    REACT_APP_GOOGLE_ANALYTICS_ID
-  }
-} = getConfig();
 
 const Meta = props => (
   <Head>
-    <script
-      async
-      src={ `https://www.googletagmanager.com/gtag/js?id=${REACT_APP_GOOGLE_ANALYTICS_ID}` }
-    />
-    { /* eslint-disable-next-line */ }
-    <script dangerouslySetInnerHTML={ {
+    <script dangerouslySetInnerHTML={{
       __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push( arguments ); }
-      gtag( 'js', new Date() );
-      gtag( 'config', '${REACT_APP_GOOGLE_ANALYTICS_ID}', {
-        page_path: window.location.pathname,
-      } );
-    `
-    } }
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=l-Fvm9iNgppOq80TmTKskg&gtm_preview=env-2&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NTQJVZD');
+        `
+    }}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <meta property="og:site_name" content="Content Commons" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
-    <title>{ props.title }</title>
+    <title>{props.title}</title>
   </Head>
 );
 
