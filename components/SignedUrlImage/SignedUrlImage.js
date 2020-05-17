3f28753b7d9e422a2f09ac9004a56f2ac4047f78
mrewers <mrewers@users.noreply.github.com>
2020-05-16 23:23:25 -0400

    Pass in classname as rest props rather than it's own prop

--- components/SignedUrlImage/SignedUrlImage.js
+++ components/SignedUrlImage/SignedUrlImage.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 
 import useSignedUrl from 'lib/hooks/useSignedUrl';
 
-const SignedUrlImage = ( { children, classes, url, ...rest } ) => {
+const SignedUrlImage = ( { children, url, ...rest } ) => {
   const { signedUrl } = useSignedUrl( url );
 
   return (
     <div
-      className={ classes }
       style={ { backgroundImage: `url( ${signedUrl} )` } }
       { ...rest }
     >
@@ -19,7 +18,6 @@ const SignedUrlImage = ( { children, classes, url, ...rest } ) => {
 
 SignedUrlImage.propTypes = {
   children: PropTypes.node,
-  classes: PropTypes.string,
   url: PropTypes.string,
 };
 
