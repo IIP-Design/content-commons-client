f91a8e0e8c649aea9e54cde0befcd4268d0ea0b6
mrewers <mrewers@users.noreply.github.com>
2020-05-18 19:03:34 -0400

    Write tests for video download components

---
+++ components/Video/__tests__/DownloadCaption.test.js
@@ -0,0 +1,48 @@
+import { mount } from 'enzyme';
+
+import DownloadCaption from '../DownloadCaption';
+import { mockItem } from '../mocks';
+
+jest.mock( '../DownloadItem', () => 'download-item' );
+
+const mockIntructions = 'Download caption file(s) for this video.';
+
+describe( '<DownloadCaption />', () => {
+  it( 'renders without crashing', () => {
+    const wrapper = mount( <DownloadCaption instructions={ mockIntructions } item={ mockItem } /> );
+
+    expect( wrapper.exists() ).toEqual( true );
+  } );
+
+  it( 'renders the instructions if provided', () => {
+    const wrapper = mount( <DownloadCaption item={ mockItem } /> );
+
+    const instructions = wrapper.find( '.form-group_instructions' );
+
+    expect( instructions.exists() ).toEqual( false );
+
+    wrapper.setProps( { instructions: mockIntructions } );
+
+    const newInstructions = wrapper.find( '.form-group_instructions' );
+
+    expect( newInstructions.exists() ).toEqual( true );
+    expect( newInstructions.contains( mockIntructions ) ).toEqual( true );
+  } );
+
+  it( 'passes the correct data to the DownloadItem', () => {
+    const expectedDownload = 'English_SRT';
+    const expectedHeader = 'Download English SRT';
+    const expectedHover = 'Download English SRT';
+    const expectedUrl = 'https://amgov-publisher-dev.s3.amazonaws.com/2020/0788lwfvaep7/c9a4533157a9797a1ecee6ca2d8c382c.srt';
+
+    const wrapper = mount( <DownloadCaption item={ mockItem } /> );
+
+    const downloadItem = wrapper.find( 'download-item' );
+
+    expect( downloadItem.exists() ).toEqual( true );
+    expect( downloadItem.prop( 'download' ) ).toEqual( expectedDownload );
+    expect( downloadItem.prop( 'header' ) ).toEqual( expectedHeader );
+    expect( downloadItem.prop( 'hover' ) ).toEqual( expectedHover );
+    expect( downloadItem.prop( 'url' ) ).toEqual( expectedUrl );
+  } );
+} );
---
+++ components/Video/mocks.js
@@ -0,0 +1,119 @@
+const english = { language_code: 'en', text_direction: 'ltr', locale: 'en-us', display_name: 'English', native_name: 'English' };
+const french = { language_code: 'fr', text_direction: 'ltr', locale: 'fr-fr', display_name: 'French', native_name: 'Français' };
+
+export const mockItem = {
+  author: 'Marek Rewers',
+  categories: [
+    {
+      id: '-Qay9HABowNfX05-nLHH',
+      name: 'democracy & civil society',
+    },
+    {
+      id: 'Sway9HABowNfX05-obJg',
+      name: 'global issues',
+    },
+  ],
+  description: '',
+  duration: undefined,
+  icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACHUlEQVRYR+VX21ECQRDszkAiUDKQDCQCJQI1AiUCNAIwAiECzUCMQIxAQtAI1mpqj9rb27t9nOiH80MVtzvdN4+eOeKPjSX4xpgzAKcAjpz7WwBbkuscn8kEjDFXAM4BXCQAPANYkdRvp0UJGGMEOAdwEnMWeL4BMO2KSisBY4zCK2C9eV9bkJyGnAQJWPAXm+e+4NV91caE5KfrsEHgQOB7EiTHMQJPiYVWGplaOmoRsJX+WOo54964Ksw9ARv6D6+3M3xmHZVeDHXDJXBrqz7LU4/D1ySXLoG3QNXv1C0RxFfG2LVnkpMdAWOMREbh923gt02bVytYKmDX1Hpquzb1HFQEJDaN4iMZVcoKzc4HaYcAJcEPJDfO/yHu44rAHYCZf6KAgF5i5EYtQuC+IhDs/QICM19oUgkodBqxNftXBH6qBuYkR24YU1MQFKGCFCiV2gHUAUvb4kqt/g/ZXgckIhKivjXgAqkdReIr1GEWaOgqoYTI33okJK8xSbPPLzO3po3S5RII1kEieMmxxixok+MS57E7Ss9QguXvA78VBS2qC7EMrWShqRh7o5zna1ctQwTUEapm96MjB6DrrEZ7bVa0bcWHIKG8axWTTuyt67tAJDTd9NvXBCrw2koerAFPRpUGqWRjVCcyEqBUUcUdtKSFw25MN/YrKaU2lOsVAK3gjbdOSkEbYztcpO/HnvIJ9B2AqryW565ofQP0hPJGComT1wAAAABJRU5ErkJggg==',
+  id: 'ck990wnr705ne0788lwfvaep7',
+  indexId: 'qKKw5XEBUe9hWhHtJn57',
+  link: '',
+  logo: '',
+  modified: '2020-05-18T17:48:00.103Z',
+  owner: 'GPA Video',
+  published: '2020-05-18T17:48:00.103Z',
+  selectedLanguageUnit: {
+    categories: [
+      {
+        id: '-Qay9HABowNfX05-nLHH',
+        name: 'democracy & civil society',
+      },
+      {
+        id: 'Sway9HABowNfX05-obJg',
+        name: 'global issues',
+      },
+    ],
+    desc: '',
+    language: english,
+    source: [
+      {
+        burnedInCaptions: 'false',
+        downloadUrl: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/05/commons.america.gov_ck990wnr705ne0788lwfvaep7/clip3.mp4',
+        duration: '2.085417',
+        filetype: 'video/mp4',
+        size: {
+          bitrate: 5315327,
+          filesize: 1461455,
+          height: 1080,
+          width: 1920,
+        },
+        stream: {
+          link: 'https://vimeo.com/413978467',
+          site: 'vimeo',
+          uid: '413978467',
+          url: 'https://player.vimeo.com/video/413978467',
+        },
+        streamUrl: [{ uid: '413978467', site: 'vimeo', link: 'https://vimeo.com/413978467', url: 'https://player.vimeo.com/video/413978467' }],
+        video_quality: 'WEB',
+        visibility: 'PUBLIC',
+      },
+    ],
+    srt: {
+      srcUrl: '',
+      visibility: 'PUBLIC',
+    },
+    tags: [],
+    thumbnail: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/05/commons.america.gov_ck990wnr705ne0788lwfvaep7/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg',
+    title: 'Test 2',
+    transcript: {
+      srcUrl: '',
+      text: '',
+      visibility: 'PUBLIC',
+    },
+  },
+  site: 'localhost',
+  sourcelink: 'https://localhost',
+  supportFiles: [
+    {
+      visibility: 'PUBLIC',
+      srcUrl: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/0788lwfvaep7/c9a4533157a9797a1ecee6ca2d8c382c.srt',
+      supportFileType: 'srt',
+      language: english,
+    },
+    {
+      visibility: 'PUBLIC',
+      srcUrl: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/0788lwfvaep7/c9a4533157a9797a1ecee6ca2d8c382c.pdf',
+      supportFileType: 'transcript',
+      language: english,
+    },
+  ],
+  tags: [],
+  thumbnail: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/05/commons.america.gov_ck990wnr705ne0788lwfvaep7/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg',
+  title: 'Test 2',
+  type: 'video',
+  units: [
+    {
+      categories: [],
+      desc: '',
+      language: english,
+      source: [],
+      srt: { visibility: 'PUBLIC', srcUrl: '' },
+      tags: [],
+      thumbnail: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/05/commons.america.gov_ck990wnr705ne0788lwfvaep7/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg',
+      title: 'Test 2',
+      transcript: { visibility: 'PUBLIC', srcUrl: '', text: '' },
+    },
+    {
+      categories: [],
+      desc: '',
+      language: french,
+      source: [],
+      srt: { visibility: 'PUBLIC', srcUrl: '' },
+      tags: [],
+      thumbnail: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/05/commons.america.gov_ck990wnr705ne0788lwfvaep7/AP_18067409351812.jpg',
+      title: 'Test 2 covid',
+      transcript: { visibility: 'PUBLIC', srcUrl: '', text: '' },
+    },
+  ],
+};
