863e8e267051695e4225e001cf4947da7cc51fe5
Edwin Mah <me@edwinmah.com>
2020-05-19 07:57:40 -0400

    Add additional tests for GraphicFilesForm

--- components/admin/ProjectEdit/GraphicEdit/GraphicFilesFormContainer/GraphicFilesForm/GraphicFilesForm.test.js
+++ components/admin/ProjectEdit/GraphicEdit/GraphicFilesFormContainer/GraphicFilesForm/GraphicFilesForm.test.js
@@ -2,6 +2,7 @@ import { mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import { MockedProvider } from '@apollo/react-testing';
 import GraphicFilesForm from './GraphicFilesForm';
+import { formatBytes, truncateAndReplaceStr } from 'lib/utils';
 
 jest.mock(
   'components/admin/ConfirmModalContent/ConfirmModalContent',
@@ -457,11 +458,41 @@ describe( '<GraphicFilesForm />, when a projectId does not exist', () => {
   let graphicsForm;
   let fieldsets;
 
-  const fileId = props.files[0].id;
-
   const newProps = {
     ...props,
     projectId: undefined,
+    files: [
+      {
+        id: 'de71e989-5701-4d28-ad1d-9fb152016ab7',
+        name: '4_3_Serious_FB.jpg',
+        loaded: 0,
+        input: {
+          dataUrl: 'the-data-url',
+          name: '4_3_Serious_FB.jpg',
+          size: 433851,
+          __typename: 'LocalInputFile',
+        },
+        language: 'ck2lzfx710hkq07206thus6pt',
+        style: 'ck9h3kyb326ak0720wkbk01q6',
+        social: ['ck9h3m9bl26bm0720rm69c60s', 'ck9h3meu626bw07201o36tapc'],
+        __typename: 'LocalImageFile',
+      },
+      {
+        id: '727a755c-8274-4df5-8417-f7720c06b6eb',
+        name: '4_3_Serious_TW.jpg',
+        loaded: 0,
+        input: {
+          dataUrl: 'the-data-url',
+          name: '4_3_Serious_TW.jpg',
+          size: 297343,
+          __typename: 'LocalInputFile',
+        },
+        language: 'ck2lzfx710hkq07206thus6pt',
+        style: 'ck9h3ka3o269y0720t7wzp5uq',
+        social: ['ck9h3m3g626bd07201gh712vk'],
+        __typename: 'LocalImageFile',
+      },
+    ],
   };
 
   beforeEach( () => {
@@ -476,28 +507,80 @@ describe( '<GraphicFilesForm />, when a projectId does not exist', () => {
     fieldsets = graphicsForm.find( 'fieldset' );
   } );
 
-  it( 'renders a disabled language dropdown', () => {
-    const language = graphicsForm.find( `#language-${fileId}` );
-    const imgWrapper = fieldsets.find( `.graphic-file-${fileId} > .image-wrapper` );
+  it( 'renders a disabled graphic title field', () => {
+    const titleFields = graphicsForm.find( 'input[id^="title-"]' );
 
-    expect( language.prop( 'disabled' ) ).toEqual( true );
-    expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    titleFields.forEach( ( field, i ) => {
+      const { id, input } = newProps.files[i];
+      const imgWrapper = fieldsets.find( `.graphic-file-${id} > .image-wrapper` );
+
+      expect( field.prop( 'name' ) ).toEqual( `${id}.title` );
+      expect( field.prop( 'value' ) ).toEqual( input.name );
+      expect( field.prop( 'disabled' ) ).toEqual( true );
+      expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    } );
+  } );
+
+  it( 'renders a disabled language dropdown', () => {
+    const langDropdowns = graphicsForm.find( '[id^="language-"]' );
+
+    langDropdowns.forEach( ( field, i ) => {
+      const { id, language } = newProps.files[i];
+      const imgWrapper = fieldsets.find( `.graphic-file-${id} > .image-wrapper` );
+
+      expect( field.prop( 'name' ) ).toEqual( `${id}.language` );
+      expect( field.prop( 'value' ) ).toEqual( language );
+      expect( field.prop( 'disabled' ) ).toEqual( true );
+      expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    } );
   } );
 
   it( 'renders a disabled graphic style dropdown', () => {
-    const style = graphicsForm.find( `#graphic-style-${fileId}` );
-    const imgWrapper = fieldsets.find( `.graphic-file-${fileId} > .image-wrapper` );
+    const styleFields = graphicsForm.find( '[id^="graphic-style-"]' );
 
-    expect( style.prop( 'disabled' ) ).toEqual( true );
-    expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    styleFields.forEach( ( field, i ) => {
+      const { id, style } = newProps.files[i];
+      const imgWrapper = fieldsets.find( `.graphic-file-${id} > .image-wrapper` );
+
+      expect( field.prop( 'name' ) ).toEqual( `${id}.style` );
+      expect( field.prop( 'value' ) ).toEqual( style );
+      expect( field.prop( 'disabled' ) ).toEqual( true );
+      expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    } );
   } );
 
   it( 'renders a disabled social platform dropdown', () => {
-    const social = graphicsForm.find( `#social-platform-${fileId}` );
-    const imgWrapper = fieldsets.find( `.graphic-file-${fileId} > .image-wrapper` );
+    const socialFields = graphicsForm.find( '[id^="social-platform-"]' );
 
-    expect( social.prop( 'disabled' ) ).toEqual( true );
-    expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    socialFields.forEach( ( field, i ) => {
+      const { id, social } = newProps.files[i];
+      const imgWrapper = fieldsets.find( `.graphic-file-${id} > .image-wrapper` );
+
+      expect( field.prop( 'name' ) ).toEqual( `${id}.social` );
+      expect( field.prop( 'value' ) ).toEqual( social );
+      expect( field.prop( 'disabled' ) ).toEqual( true );
+      expect( imgWrapper.hasClass( 'unavailable' ) ).toEqual( true );
+    } );
+  } );
+
+  it( 'renders the filename correctly', () => {
+    const filenames = graphicsForm.find( '.filename' );
+
+    filenames.forEach( ( filename, i ) => {
+      const inputName = newProps.files[i].input.name;
+
+      expect( filename.contains( inputName ) ).toEqual( true );
+    } );
+  } );
+
+  it( 'renders the filesize correctly', () => {
+    const filesizes = graphicsForm.find( '.filesize' );
+
+    filesizes.forEach( ( filesize, i ) => {
+      const inputSize = formatBytes( newProps.files[i].input.size, 1 );
+
+      expect( filesize.text() ).toEqual( inputSize );
+    } );
   } );
 } );
 
@@ -525,8 +608,6 @@ describe( '<GraphicFilesForm />, when there is a long file name', () => {
     ],
   };
 
-  const { id: fileId, filename } = newProps.files[0];
-
   beforeEach( () => {
     Component = (
       <MockedProvider mocks={ [] } addTypename>
@@ -540,12 +621,18 @@ describe( '<GraphicFilesForm />, when there is a long file name', () => {
   } );
 
   it( 'renders a truncated filename with the full filename visually hidden', () => {
-    const imgWrapper = fieldsets.find( `.graphic-file-${fileId} > .image-wrapper` );
-    const btn = imgWrapper.find( '.filename.truncated' );
-    const visuallyHidden = imgWrapper.find( '.hide-visually' );
+    fieldsets.forEach( ( fieldset, i ) => {
+      const { filename } = newProps.files[i];
+      const imgWrapper = fieldset.find( '.image-wrapper' );
+      const btn = imgWrapper.find( '.filename.truncated' );
+      const visuallyHidden = imgWrapper.find( '.hide-visually' );
+      const displayName = filename?.length > 30
+        ? truncateAndReplaceStr( filename, 20, 10 )
+        : filename;
 
-    expect( btn.prop( 'tooltip' ) ).toEqual( filename );
-    expect( btn.text() ).toEqual( '4_3_Serious_aieaue_k...wyz_TW.jpg' );
-    expect( visuallyHidden.text() ).toEqual( filename );
+      expect( btn.prop( 'tooltip' ) ).toEqual( filename );
+      expect( btn.text() ).toEqual( displayName );
+      expect( visuallyHidden.text() ).toEqual( filename );
+    } );
   } );
 } );
