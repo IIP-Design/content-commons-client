9ebceb2565476b0fa6c5596faeaeaf5acbbf297e
mrewers <mrewers@users.noreply.github.com>
2020-05-20 18:13:26 -0400

    Add test to ModalImage component

--- components/modals/ModalImage/ModalImage.js
+++ components/modals/ModalImage/ModalImage.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { object, string } from 'prop-types';
+import PropTypes from 'prop-types';
+
 import './ModalImage.scss';
 
-const ModalImage = props => {
-  const { thumbnail, thumbnailMeta } = props;
-  return (
-    <figure className="modal_thumbnail">
-      <img src={ thumbnail } alt={ thumbnailMeta.alt } />
-      <figcaption className="modal_meta_content">{ thumbnailMeta.caption }</figcaption>
-    </figure>
-  );
-};
+const ModalImage = ( { thumbnail, thumbnailMeta } ) => (
+  <figure className="modal_thumbnail">
+    <img src={ thumbnail } alt={ thumbnailMeta.alt } />
+    <figcaption className="modal_meta_content">{ thumbnailMeta.caption }</figcaption>
+  </figure>
+);
 
 ModalImage.propTypes = {
-  thumbnail: string,
-  thumbnailMeta: object
+  thumbnail: PropTypes.string,
+  thumbnailMeta: PropTypes.object,
 };
 
 export default ModalImage;
