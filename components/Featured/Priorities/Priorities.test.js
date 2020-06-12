5f04e06559f8a378e4d455dc8e4d5d988aea88cc
Edwin Mah <me@edwinmah.com>
2020-06-12 15:41:23 -0400

    Revert "Fix tests for Priorities"
    This reverts commit a2d2a004ca2c2531d9886472e12c38d865ee6377.

--- components/Featured/Priorities/Priorities.test.js
+++ components/Featured/Priorities/Priorities.test.js
@@ -5,7 +5,7 @@ const propsArray = [
   {
     featured: {
       loading: false,
-      error: false,
+      error: false
     },
     priorities: [
       {
@@ -15,7 +15,7 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The First Title',
         type: 'video',
@@ -27,34 +27,34 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The Second Title',
-        type: 'post',
+        type: 'post'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image3.jpg',
         icon: 'https://www.icon.com/image3.jpg',
         categories: [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
-          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
+          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' }
         ],
         title: 'The Third Title',
-        type: 'video',
-      },
+        type: 'video'
+      }
     ],
     categories: [
       { key: 'MlqWJ2MBNxuyMP4E6Cj2', display_name: 'Democracy' },
-      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' },
+      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' }
     ],
     term: 'test',
     label: 'Test Label',
-    locale: 'en-us',
+    locale: 'en-us'
   },
   {
     featured: {
       loading: false,
-      error: false,
+      error: false
     },
     priorities: [
       {
@@ -64,10 +64,10 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The First Title',
-        type: 'post',
+        type: 'post'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image2.jpg',
@@ -76,34 +76,34 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The Second Title',
-        type: 'video',
+        type: 'video'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image3.jpg',
         icon: 'https://www.icon.com/image3.jpg',
         categories: [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
-          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
+          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' }
         ],
         title: 'The Third Title',
-        type: 'post',
-      },
+        type: 'post'
+      }
     ],
     categories: [
       { key: 'MlqWJ2MBNxuyMP4E6Cj2', display_name: 'Democracy' },
-      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' },
+      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' }
     ],
     term: 'test',
     label: 'Test Label',
-    locale: 'en-us',
+    locale: 'en-us'
   },
   {
     featured: {
       loading: false,
-      error: true,
+      error: true
     },
     priorities: [
       {
@@ -113,10 +113,10 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The First Title',
-        type: 'post',
+        type: 'post'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image2.jpg',
@@ -125,34 +125,34 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The Second Title',
-        type: 'video',
+        type: 'video'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image3.jpg',
         icon: 'https://www.icon.com/image3.jpg',
         categories: [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
-          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
+          { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' }
         ],
         title: 'The Third Title',
-        type: 'post',
-      },
+        type: 'post'
+      }
     ],
     categories: [
       { key: 'MlqWJ2MBNxuyMP4E6Cj2', display_name: 'Democracy' },
-      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' },
+      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' }
     ],
     term: 'test',
     label: 'Test Label',
-    locale: 'en-us',
+    locale: 'en-us'
   },
   {
     featured: {
       loading: false,
-      error: false,
+      error: false
     },
     priorities: [
       {
@@ -162,10 +162,10 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The First Title',
-        type: 'post',
+        type: 'post'
       },
       {
         thumbnail: 'https://www.thumbnail.com/image2.jpg',
@@ -174,65 +174,56 @@ const propsArray = [
           { id: 'MlqWJ2MBNxuyMP4E6Cj2', name: 'democracy' },
           { id: 'lLWWJ2MBCLPpGnLD5z8X', name: 'human rights' },
           { id: 'crWWJ2MBCLPpGnLD3D8Y', name: 'transparency' },
-          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' },
+          { id: 'mbWWJ2MBCLPpGnLD6D-X', name: 'media & press' }
         ],
         title: 'The Second Title',
-        type: 'video',
-      },
+        type: 'video'
+      }
     ],
     categories: [
       { key: 'MlqWJ2MBNxuyMP4E6Cj2', display_name: 'Democracy' },
-      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' },
+      { key: 'lLWWJ2MBCLPpGnLD5z8X', display_name: 'Human Rights' }
     ],
     term: 'test',
     label: 'Test Label',
-    locale: 'en-us',
-  },
+    locale: 'en-us'
+  }
 ];
 
 jest.mock( 'components/Video/Video', () => 'Video' );
 jest.mock( 'components/Post/Post', () => 'Post' );
 
-propsArray.forEach( ( props, index ) => {
+propsArray.forEach( props => {
   const Component = <PrioritiesUnconnected { ...props } />;
 
   describe( '<PrioritiesUnconnected />', () => {
     it( 'renders without crashing', () => {
       const wrapper = shallow( Component );
-
       expect( wrapper.exists() ).toEqual( true );
     } );
-
     it( 'correctly displays error message if one exists', () => {
       const wrapper = shallow( Component );
       const errorText = wrapper.find( 'Message' );
-
       if ( props.featured.error ) {
         expect( errorText.exists() ).toEqual( true );
       } else {
         expect( errorText.exists() ).toEqual( false );
       }
     } );
-
     if ( props.priorities.length < 3 ) {
       it( 'renders an empty div when less than 3 entries', () => {
         const wrapper = shallow( Component );
-
         expect( wrapper.html() ).toBe( '<div></div>' );
       } );
     } else if ( props.priorities.length >= 3 ) {
       it( 'renders component as a section', () => {
         const wrapper = shallow( Component );
-
         expect( wrapper.name() ).toEqual( 'section' );
       } );
-
       it( 'renders a section with classname priorities', () => {
         const wrapper = shallow( Component );
-
         expect( wrapper.hasClass( 'priorities' ) ).toEqual( true );
       } );
-
       it( 'renders the correct postType component', () => {
         const wrapper = shallow( Component );
         const modalContent = wrapper.find( 'ModalContent' );
@@ -245,7 +236,6 @@ propsArray.forEach( ( props, index ) => {
           expect( mc.childAt( 0 ) ).toEqual( component );
         } );
       } );
-
       it( 'renders the correct header', () => {
         const wrapper = shallow( Component );
         const header = wrapper.find( 'Header' );
@@ -253,7 +243,6 @@ propsArray.forEach( ( props, index ) => {
         expect( header.exists() ).toEqual( true );
         expect( header.childAt( 0 ).text() ).toBe( `Department Priority: ${props.label}` );
       } );
-
       it( 'renders two items on the right starting at index 1', () => {
         const wrapper = shallow( Component );
         const items = wrapper.find( 'ItemGroup Modal' );
@@ -262,7 +251,6 @@ propsArray.forEach( ( props, index ) => {
         expect( items.length ).toEqual( 2 );
         items.forEach( ( item, i ) => {
           const trigger = item.prop( 'trigger' );
-
           if ( i === 0 ) {
             expect( shallow( trigger ).contains( 'The Second Title' ) ).toEqual( true );
           } else {
@@ -270,7 +258,6 @@ propsArray.forEach( ( props, index ) => {
           }
         } );
       } );
-
       it( 'renders no more than 3 categories', () => {
         const wrapper = shallow( Component );
         const items = wrapper.find( 'ItemGroup Modal' );
@@ -278,15 +265,10 @@ propsArray.forEach( ( props, index ) => {
         expect( items.exists() ).toEqual( true );
         items.forEach( item => {
           const trigger = item.prop( 'trigger' );
-
-          expect( shallow( trigger ).find( 'span.categories' )
-            .exists() ).toEqual( true );
-          expect( shallow( trigger ).find( 'span.categories' )
-            .text()
-            .split( ' · ' ).length ).toBeLessThanOrEqual( 3 );
+          expect( shallow( trigger ).find( 'span.categories' ).exists() ).toEqual( true );
+          expect( shallow( trigger ).find( 'span.categories' ).text().split( ' · ' ).length ).toBeLessThanOrEqual( 3 );
         } );
       } );
-
       it( 'renders the Browse All link and the correct href', () => {
         const wrapper = shallow( Component );
         const link = wrapper.find( 'Link' );
@@ -295,7 +277,9 @@ propsArray.forEach( ( props, index ) => {
         expect( link.find( 'a' ).text() ).toBe( 'Browse All' );
         expect( link.find( 'a' ).hasClass( 'browseAll' ) ).toEqual( true );
         expect( link.prop( 'href' ).pathname ).toBe( '/results' );
-        expect( link.prop( 'href' ).query.term ).toEqual( propsArray[index].term );
+        link.prop( 'href' ).query.term.forEach( t => {
+          expect( t ).toBe( props.term );
+        } );
       } );
     }
   } );
