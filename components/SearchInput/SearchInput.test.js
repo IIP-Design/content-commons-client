535de958e88cc768bb1d219dc6110030083d533d
Edwin Mah <me@edwinmah.com>
2020-06-30 09:36:33 -0400

    Make es-lint fixes

--- components/SearchInput/SearchInput.test.js
+++ components/SearchInput/SearchInput.test.js
@@ -3,16 +3,15 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import SearchInput from './SearchInput';
 import * as state from './mocks';
-import { exportAllDeclaration } from '@babel/types';
 
-jest.mock("next/config", () => () => ({ publicRuntimeConfig: {} }));
-jest.mock("context/authContext", () => ({
-  useAuth: jest.fn(() => true),
-}));
+jest.mock( 'next/config', () => () => ( { publicRuntimeConfig: {} } ) );
+jest.mock( 'context/authContext', () => ( {
+  useAuth: jest.fn( () => true ),
+} ) );
 
 const mockStore = configureStore( [] );
 
-describe( '<SearchInput>', () => {
+describe( '<SearchInput />', () => {
   let store;
   let Component;
   let wrapper;
@@ -21,18 +20,17 @@ describe( '<SearchInput>', () => {
   beforeEach( () => {
     store = mockStore( state.noCategories );
 
-
     Component = (
       <Provider store={ store }>
         <SearchInput />
       </Provider>
     );
-    
-    wrapper = mount(Component);
-    searchInput = wrapper.find("SearchInput");
+
+    wrapper = mount( Component );
+    searchInput = wrapper.find( 'SearchInput' );
   } );
 
   it( 'renders without crashing', () => {
-    expect(searchInput.exists()).toEqual(true);
+    expect( searchInput.exists() ).toEqual( true );
   } );
 } );
