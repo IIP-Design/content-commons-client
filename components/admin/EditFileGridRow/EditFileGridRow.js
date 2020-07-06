a30ed94bfc88264118dd0e22cce38803c2c02ca9
Shawn Mathew <shawn.mathew@agencychief.com>
2020-07-06 10:32:59 -0400

    CDP-2129 - add style selection text to reducer state for normalized graphic files

--- components/admin/EditFileGridRow/EditFileGridRow.js
+++ components/admin/EditFileGridRow/EditFileGridRow.js
@@ -89,7 +89,22 @@ const EditFileGridRow = ( {
         key={ `selectBox_${select?.label?.toLowerCase()}` }
         id={ file.id }
         className={ `${styles.selectBox} ${isLast( select, i ) ? '' : styles.marginRight}` }
-        onChange={ ( _, data ) => onChange( data ) }
+        // onChange={ ( _, data ) => onChange( data ) }
+        onChange={ ( e, data ) => {
+          console.log( e?.target.textContent );
+          if ( data.name === 'style' ) {
+            const dropdownSelectionText = e?.target.textContent;
+            const dataWithSelectionName = {
+              ...data,
+              styleSelection: dropdownSelectionText || '',
+            };
+
+            onChange( dataWithSelectionName );
+
+            return;
+          }
+          onChange( data );
+        } }
         filename={ file.name }
         value={ value }
         { ...props }
