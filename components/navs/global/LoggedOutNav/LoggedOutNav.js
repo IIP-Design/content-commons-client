097f06b80cd2ccf2b67285c4c33eb244f736a03e
TJ Stalcup <stalcuptj@america.gov>
2020-07-07 13:43:39 -0400

    [CDP-2066] Adds login reminder after 5 seconds, timeout and manual close

--- components/navs/global/LoggedOutNav/LoggedOutNav.js
+++ components/navs/global/LoggedOutNav/LoggedOutNav.js
@@ -4,11 +4,11 @@
  *
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import {
-  Menu, Icon
+  Menu, Icon,
 } from 'semantic-ui-react';
 import config from 'config';
 import HamburgerIcon from '../HamburgerIcon';
@@ -18,20 +18,20 @@ const menuItems = [
     key: 1,
     name: 'about',
     to: '/about',
-    label: 'About'
+    label: 'About',
   },
   {
     key: 2,
     name: 'help',
     to: '/help',
-    label: 'Help'
+    label: 'Help',
   },
   {
     key: 3,
     name: 'documentation',
     to: '/documentation',
-    label: 'Documentation'
-  }
+    label: 'Documentation',
+  },
 ];
 
 const LoggedOutNav = props => {
@@ -44,9 +44,15 @@ const LoggedOutNav = props => {
     toggleMobileMenu( false );
   };
 
+  const [reminder, setReminder] = useState( false );
+
 
   useEffect( () => {
     window.addEventListener( 'resize', closeMobileMenu );
+
+    setTimeout( () => setReminder( true ), 5000 );
+    setTimeout( () => setReminder( false ), 35000 );
+
     return () => {
       window.removeEventListener( 'resize', closeMobileMenu );
     };
@@ -96,19 +102,19 @@ const LoggedOutNav = props => {
         />
       );
     }
- 
+
     return (
       <ul>
         <li>
-          <Icon name="close" onClick={() => toggleMobileMenu(false)} onKeyUp={keyUp} tabIndex={0} />
+          <Icon name="close" onClick={ () => toggleMobileMenu( false ) } onKeyUp={ keyUp } tabIndex={ 0 } />
         </li>
-        {items.map((item) => renderListItem(item))}
-        <li> 
+        {items.map( item => renderListItem( item ) )}
+        <li>
           <a href="/login">
-            <span onClick={() => toggleMobileMenu(false)} onKeyUp={keyUp} role="presentation">
+            <span onClick={ () => toggleMobileMenu( false ) } onKeyUp={ keyUp } role="presentation">
               Login
             </span>
-          </a> 
+          </a>
         </li>
         {renderFeedbackButton()}
       </ul>
@@ -116,15 +122,21 @@ const LoggedOutNav = props => {
   };
 
   const renderDesktopNav = items => (
-    <>
-      <Menu className="nav_loggedout">
-        { items.map( item => renderMenuItem( item ) ) }
-        <Menu.Item key="4" name="login">
-          <a href="/login">Login</a>
-        </Menu.Item>
-        { renderFeedbackButton() }
-      </Menu>
-    </>
+    <Menu className="nav_loggedout">
+      {items.map( item => renderMenuItem( item ) )}
+      <Menu.Item key="4" name="login">
+        <a href="/login">Login</a>
+      </Menu.Item>
+      {reminder && (
+        <div className="login_reminder">
+          DOS employees, you can log in to see more content.
+          <a href="/remove-login-reminder" aria-label="Close Login Reminder" className="login_reminder_close" onClick={ e => { e.preventDefault(); setReminder( false ); } }>
+            Got it
+          </a>
+        </div>
+      )}
+      {renderFeedbackButton()}
+    </Menu>
   );
 
 
@@ -146,7 +158,7 @@ const LoggedOutNav = props => {
 LoggedOutNav.propTypes = {
   mobileMenuVisible: PropTypes.bool,
   toggleMobileMenu: PropTypes.func,
-  keyUp: PropTypes.func
+  keyUp: PropTypes.func,
 };
 
 export default LoggedOutNav;
