b22baa8d093281b75750ae68784504eb3836ca41
Shawn Mathew <shawn.mathew@agencychief.com>
2020-08-19 15:59:26 -0400

    CDP-2192 - update elastic search query for subfilters

--- components/FilterMenu/FilterSubMenu/PressSubMenu/FilterMenuCountries.js
+++ components/FilterMenu/FilterSubMenu/PressSubMenu/FilterMenuCountries.js
@@ -49,7 +49,6 @@ const FilterMenuCountries = props => {
       options={ getMenuOptions() }
       formItem="checkbox"
       searchInput={ (
-        // <div style={ { margin: 0, padding: '0.5em 1em' } }>
         <div>
           <VisuallyHidden>
             {/* eslint-disable jsx-a11y/label-has-associated-control */}
@@ -60,7 +59,6 @@ const FilterMenuCountries = props => {
             placeholder="Search countries"
             name="countries"
             icon="search"
-            iconPosition="right"
             value={ searchedCountry }
             onChange={ handleChange }
           />
