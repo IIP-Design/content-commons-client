10d03eecba9135be6f1184289b6570cc4395f743
mrewers <hello@marek.dev>
2021-04-07 15:17:28 -0400

    fix: refactor _app to restore initial props and properly redirect to login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,17 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 /* eslint-enable */
-
+import { useEffect } from 'react';
 import Amplify from 'aws-amplify';
 import { ApolloProvider } from '@apollo/client';
 import App from 'next/app';
 import isEmpty from 'lodash/isEmpty';
+import propTypes from 'prop-types';
 
 import Page from 'components/Page';
 
 import { AuthProvider, canAccessPage } from 'context/authContext';
 import awsconfig from '../aws-exports';
-import { redirectTo } from 'lib/browser';
 import storeWrapper from 'lib/redux/store';
 import withApollo from 'hocs/withApollo';
 
@@ -23,47 +23,55 @@ Amplify.configure( {
   ssr: true,
 } );
 
-class Commons extends App {
-  static getStaticProps = async ( { Component, ctx } ) => {
-    // if user does not have appropriate page permissions redirect
-    if ( !await canAccessPage( ctx ) ) {
-      // only set redirect url to a non login url
-      if ( ctx.pathname !== '/login' ) {
-        // add redirect url as a query param
-        // cannot use client side storage or libraries as this is executing on the server
-        redirectTo( `/login?return=${ctx.asPath}`, ctx );
-      }
+const Commons = ( { apollo, Component, pageProps, router } ) => {
+  const { redirect } = pageProps;
+
+  useEffect( () => {
+    if ( redirect ) {
+      router.push( `/login?return=${redirect}` );
     }
+  }, [redirect] );
 
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps( ctx ) : {};
+  return (
+    <ApolloProvider client={ apollo }>
+      <AuthProvider>
+        <Page redirect={ pageProps.redirect }>
+          <Component { ...pageProps } />
+        </Page>
+      </AuthProvider>
+    </ApolloProvider>
+  );
+};
 
-    // exposes apollo query to component
-    if ( !isEmpty( ctx.query ) ) {
-      pageProps.query = ctx.query;
+Commons.getInitialProps = async appContext => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps( appContext );
+
+  const { ctx } = appContext;
+
+  // if user does not have appropriate page permissions redirect
+  if ( !await canAccessPage( ctx ) ) {
+    // only set redirect url to a non login url
+    if ( ctx.pathname !== '/login' ) {
+      // add redirect url as a query param
+      // cannot use client side storage or libraries as this is executing on the server
+      appProps.pageProps.redirect = ctx.asPath;
     }
-
-    if ( Object.keys( pageProps ).length > 0 ) {
-      return { pageProps };
-    }
-
-    return {};
   }
 
-  render() {
-    const {
-      Component, apollo, pageProps,
-    } = this.props;
-
-    return (
-      <ApolloProvider client={ apollo }>
-        <AuthProvider>
-          <Page>
-            <Component { ...pageProps } />
-          </Page>
-        </AuthProvider>
-      </ApolloProvider>
-    );
+  // exposes apollo query to component
+  if ( !isEmpty( ctx.query ) ) {
+    appProps.pageProps.query = ctx.query;
   }
-}
+
+  return { ...appProps };
+};
+
+Commons.propTypes = {
+  apollo: propTypes.object,
+  Component: propTypes.func,
+  pageProps: propTypes.object,
+  router: propTypes.object,
+};
 
 export default withApollo( storeWrapper.withRedux( Commons ) );
