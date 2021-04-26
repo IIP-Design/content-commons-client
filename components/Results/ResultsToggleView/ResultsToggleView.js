da16d4b1aa29e8cc9887c5f367588af5f85826a2
Edwin Mah <me@edwinmah.com>
2021-04-26 10:42:35 -0400

    fix: make results view keyboard & screen reader accessible

--- components/Results/ResultsToggleView/ResultsToggleView.js
+++ components/Results/ResultsToggleView/ResultsToggleView.js
@@ -1,40 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { func, string } from 'prop-types';
+import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
+import VisuallyHidden from 'components/VisuallyHidden/VisuallyHidden';
 import { isMobile } from 'lib/browser';
 import './ResultsToggleView.scss';
 import 'styles/tooltip.scss';
 
-const ResultsToggleView = props => {
+const ResultsToggleView = ( { currentView, toggle } ) => {
   const [isMobileDevice, setIsMobileDevice] = useState( null );
 
   useEffect( () => setIsMobileDevice( isMobile() ) );
 
   return (
-    <div className="results_toggleView">
-      <span tooltip="Gallery View" className={ isMobileDevice ? 'noTooltip' : '' }>
-        <Icon
-          name="grid layout"
-          data-view="gallery"
-          onClick={ props.toggle }
-          className={ props.currentView === 'gallery' ? 'active' : '' }
-        />
-      </span>
-      <span tooltip="List View" className={ isMobileDevice ? 'noTooltip' : '' }>
-        <Icon
-          name="list"
-          data-view="list"
-          onClick={ props.toggle }
-          className={ props.currentView === 'list' ? 'active' : '' }
-        />
-      </span>
+    <div className="results_toggleView" aria-label="set gallery/list view">
+      <VisuallyHidden>
+        <div role="status" aria-live="polite">{ `${currentView} view` }</div>
+      </VisuallyHidden>
+      <button
+        type="button"
+        onClick={ toggle }
+        disabled={ currentView === 'gallery' }
+        data-view="gallery"
+      >
+        <VisuallyHidden>set to gallery view</VisuallyHidden>
+        <span
+          aria-hidden="true"
+          className={ isMobileDevice ? 'noTooltip' : '' }
+          tooltip="Gallery View"
+        >
+          <Icon
+            name="grid layout"
+            className={ currentView === 'gallery' ? 'active' : '' }
+          />
+        </span>
+      </button>
+
+      <button
+        type="button"
+        onClick={ toggle }
+        disabled={ currentView === 'list' }
+        data-view="list"
+      >
+        <VisuallyHidden>set to list view</VisuallyHidden>
+        <span
+          aria-hidden="true"
+          className={ isMobileDevice ? 'noTooltip' : '' }
+          tooltip="List View"
+        >
+          <Icon
+            name="list"
+            className={ currentView === 'list' ? 'active' : '' }
+          />
+        </span>
+      </button>
     </div>
   );
 };
 
 ResultsToggleView.propTypes = {
-  toggle: func,
-  currentView: string,
+  toggle: PropTypes.func,
+  currentView: PropTypes.string,
 };
 
 export default ResultsToggleView;
