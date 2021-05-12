471f896a88552d5f8d8a1c8d46bb8905a0ea39f6
mrewers <hello@marek.dev>
2021-05-12 19:18:37 -0400

    test: mock out test playbook content

---
+++ components/Playbook/mocks.js
@@ -0,0 +1,119 @@
+const mockContent = `
+<h3>Messaging Action Items</h3>
+
+<ol>
+  <li><strong>State and USAID encourage Missions to promote official deliveries of COVID-19 vaccines AFTER announced by Gavi, if appropriate for the country or regional context.</strong></li>
+  <li>Please make sure Gavi has made a public announcement before you promote in-country.</li>
+  <li><strong>If you become aware of a pending delivery</strong> through the COVAX Advance Market Commitment, please inform <a>covidcomms@usaid.gov</a>, <a>MCMCoordinationGroup@state.gov</a>, and <a>CGRCUInternal@state.gov</a> for awareness. We recognize that deliveries may occur on short notice for various reasons.  Local UNICEF officials may have more information on timing of deliveries. (Note: Post should <strong>not</strong> contact UNICEF related to deliveries for self-financing participants.  U.S. investments in COVAX only support procurement and distribution to 92 low- and middle- income economies receiving doses via the COVAX Advanced Market Commitments (AMC)).</li>
+    <ol>
+      <li><strong>For posts with countries receiving doses via the COVAX AMCs</strong>, it is recommended for posts to coordinate with the appropriate UN official or through USAID to the Ministry of Health on timing for vaccine deliveries. (<strong>NOTE:</strong> Posts should NOT brand the packaging with USAID or USG branding or logos)</li>
+    </ol>
+  <li><strong>For country-level announcements:</strong> Only after Gavi has officially announced a vaccine allocation or delivery, Post can re-share or promote the announcement using the resources below, including social media content, talking points and building blocks for op-eds, and USAID’s frequently asked questions.</li>
+  <li><strong>For country-level events:</strong> If an Ambassador or Mission Director is asked to participate in an announcement, such as an event or video, please send the planned remarks to <a>covidcomms@usaid.gov</a>, <a>MCMCoordinationGroup@state.gov</a>, and <a>CGRCUInternal@state.gov</a> for awareness.</li>
+  <li><strong>For the latest public information on vaccine deliveries</strong>, you can track <strong>live updates</strong> on <strong><a>Gavi’s website</a></strong>. Gavi has also published the <strong>first round of country allocations <a>here</a></strong>.</li>
+</ol>
+
+<h3>Press Guidance</h3>
+
+<ul>
+  <li><strong>NEW:</strong> <a>COVAX DELIVERIES</a> | March 19, 2021</li>
+  <li><a>U.S. to Support COVAX 19</a> | February 19, 2021</li>
+  <li><a>U.S. Remaining in the WHO</a> | February 3, 2021</li>
+  <li><a>U.S. Multilateral Approach to COVID-19</a> | February 1, 2021</li>
+</ul>
+
+<h3>Op-Ed Building Blocks</h3>
+
+<ul>
+  <li><a>COVAX Vaccine Arrival Op-Ed Building Blocks</a></li>
+  <ul>
+    <li><strong><em>Cleared for international placement only</em></strong></li>
+  </ul>
+</ul>
+
+
+<h3>Social Media Graphics</h3>
+
+<h4>COVAX Specific Graphics</h4>
+<ul>
+  <li><strong>CUSTOMIZABLE GRAPHICS FOR COVAX DELIVERIES:</strong></li>
+  <ul>
+    <li><a>Version One</a></li>
+    <li><a>Version Two</a></li>
+    <li><a>Version Three</a></li>
+    <ul>
+      <li>Notes:</li>
+      <ul>
+        <li><strong>Instructions:</strong> Blank text-free template provided on Content Commons for posts to insert translations and host country flag into the graphic using Canva or a graphic editing software</li>
+        <li><strong>Editable Text for Translations:</strong> “The United States congratulates [insert country] and celebrates the arrival of [XX] doses of [AstraZeneca]/[Pfizer] vaccine under COVAX on [insert date]”</li>
+      </ul>
+    </ul>
+  </ul>
+  <li><strong>NEW GRAPHIC:</strong> <a>“The U.S. is proud supporter of COVAX and its distribution of safe and effective vaccines”</a></li>
+  <li><a>“The U.S. has pledged $2 billion to the global vaccine initiative to defeat COVID-19." Infographic</a></li>
+</ul>
+
+<h4>General COVID-19 Graphics</h4>
+<ul>
+<li><a>S Quote "We must defeat COVID-19 and prevent future pandemics."</a></li>
+<li><a>S Quote “The United States will work as a partner to address global challenges.”</a></li>
+<li><a>S Quote "I believe we can and we will overcome the COVID crisis, the greatest shared challenge since World War II."</a></li>
+</ul>
+
+<p><strong><em>Please reach out to the GPA’s Design and Editorial (<a>graphics@state.gov</a>) office with any questions on graphics or how to utilize the customizable templates.</em></strong></p>
+
+<h3>Quotes</h3>
+
+<ul>
+<li>“Today, I’m announcing that the United States is making a $2 billion pledge to COVAX, with the promise of an additional $2 billion to urge others to step up as well.” | <a>Remarks by President Biden at the 2021 Virtual Munich Security Conference</a>, February 19, 2021</li>
+<li>“On COVID-19: We reengaged the World Health Organization and contributed an initial $2 billion to COVAX, the global vaccination initiative.” | Secretary Blinken, <a>Opening Remarks by Secretary of State Antony J. Blinken Before the House Committee on Foreign Affairs</a>, March 10, 2021</li>
+<li>“We have joined this COVAX arrangement. We’re contributing billions of dollars to creating greater access to vaccines. And I think as the months go on and as we vaccinate our own people and make sure that every American is protected, we’ll also be engaged in helping the world get vaccinated. Because at the end of the day, we will not be fully secure until the world is vaccinated, not just Americans.” | <a>Secretary Antony J. Blinken with Judy Woodruff of PBS NewsHour</a>, March 3, 2021</li>
+</ul>
+
+<h3>ShareAmerica Articles</h3>
+
+<ul>
+  <li><a>Biden: Quad partners boosting COVID-19 vaccine production in India</a> | March 15, 2021</li>
+  <li><a>U.S. and Canada: Strong partners, shared values</a> | Feb. 26, 2021</li>
+  <li><a>Biden: U.S. will work with democratic partners</a> | Feb. 22, 2021</li>
+  <li><a>U.S. supports global COVID-19 response efforts</a> | Feb. 18, 2021</li>
+  <li><a>Biden bolsters U.S. support for COVID-19 vaccinations abroad</a> | Jan. 27, 2021</li>
+  <li><a>U.S. reaffirms partnership with World Health Organization</a> | Jan. 21, 2021</li>
+</ul>
+
+<h3>Fact Sheets & Memorandums</h3>
+
+<ul>
+  <li><a>Fact Sheet: Quad Summit</a> | March 12, 2021</li>
+  <li><a>Fact Sheet: President Biden to Take Action on Global Health through Support of COVAX and Calling for Health Security Financing</a> | February 18, 2021</li>
+  <li><a>National Security Memorandum on United States Global Leadership to Strengthen the International COVID-19 Response and to Advance Global Health Security and Biological Preparedness</a> | January 21, 2021</li>
+</ul>
+
+<h3>Tweets</h3>
+
+<ul>
+<li>@StateDept: .@SecBlinken on COVID-19: We've-engaged the @WHO and contributed an initial $2 billion to COVAX, the global vaccination initiative. <a>https://go.usa.gov/xsjAp</a> [+Graphic] | <a>March 16, 2021</a></li>
+<li>@StateDeputySpox: The U.S. welcomes the good news that COVAX completed delivery of COVID-19 vaccines today to Ghana. Global crises require global responses, and the U.S. is proud to have contributed $2 billion to COVAX. We invite other partners to do their part. | <a>February 24, 2021</a></li>
+<li>@StateDept: .@POTUS: I’m announcing that the United States is making a $2 billion pledge to COVAX [COVID-19 Vaccines Global Access], with the promise of an additional $2 billion to urge others to step up as well. | <a>February 21, 2021</a></li>
+</ul>
+`;
+
+export const mockItem = {
+  id: '1234',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  publishedAt: new Date(),
+  type: 'PLAYBOOK',
+  title: 'COVAX Deliveries and Announcement Guidance Playbook',
+  // assetPath: String,
+  // author: User,
+  // team: Team,
+  // desc:  String,
+  content: mockContent,
+  // status: PublishStatus @default(value: DRAFT),
+  // visibility: Visibility @default(value: INTERNAL),
+  // policy: PolicyPriority,
+  categories: [],
+  tags: [],
+  supportFiles: [],
+};
