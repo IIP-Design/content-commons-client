8b2368f8a63a62c5bdd4c9e5e94a83c838213a8e
mrewers <hello@marek.dev>
2021-05-14 12:33:07 -0400

    test: add initial tests for the playbooks component

---
+++ components/Playbook/Playbook.test.js
@@ -0,0 +1,31 @@
+import { mount } from 'enzyme';
+
+import Playbook from './Playbook';
+
+import { mockItem } from './mocks';
+
+jest.mock( 'next/config', () => () => ( {
+  publicRuntimeConfig: {
+    REACT_APP_AWS_S3_AUTHORING_BUCKET: 's3-bucket-url',
+    REACT_APP_PUBLIC_API: 'http://localhost:8080',
+  },
+} ) );
+
+jest.mock( 'components/download/DownloadItem/DownloadItemContent', () => 'download-item-content' );
+jest.mock( 'components/Share/Share', () => 'share' );
+jest.mock( 'components/popups/Popover/Popover', () => 'popover' );
+jest.mock( 'components/TexturedSection/TexturedSection', () => 'textured-section' );
+
+describe( '<Playbook />', () => {
+  const wrapper = mount( <Playbook item={ mockItem } /> );
+
+  it( 'renders without crashing', () => {
+    expect( wrapper.exists() ).toEqual( true );
+  } );
+
+  it( 'each supported file is added to the downloadable attachments list', () => {
+    const resources = wrapper.find( '.resources-list' );
+
+    expect( resources.children() ).toHaveLength( mockItem.supportFiles.length );
+  } );
+} );
