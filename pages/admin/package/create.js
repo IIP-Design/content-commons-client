9d008ad6e24b0425f022bd95709a9cdd6ecc4d70
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-05-24 13:08:13 -0400

    feat: create new intermediary page for pacakge creation

---
+++ pages/admin/package/create.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PackageCreate from 'components/admin/PackageCreate/PackageCreate';
+
+const PackageCreatePage = () => <PackageCreate />;
+
+export default PackageCreatePage;
