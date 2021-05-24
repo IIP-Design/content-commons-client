e5473890c54ec4c9f14ada2d59166b849a91f515
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-05-24 13:28:04 -0400

    refactor: remove initial schema

--- components/admin/PlaybookEdit/PlaybookDetailsFormContainer/validationSchema.js
+++ components/admin/PlaybookEdit/PlaybookDetailsFormContainer/validationSchema.js
@@ -10,11 +10,4 @@ const _baseSchema = {
     .required( 'An internal description is required.' ),
 };
 
-const _initialSchema = {
-  termsConditions: Yup.bool()
-    .test( 'consent', 'You have to agree with our Terms of Use!', value => value === true )
-    .required( 'You have to agree with our Terms of Use!' ),
-};
-
-export const initialSchema = Yup.object().shape( { ..._baseSchema, ..._initialSchema } );
 export const baseSchema = Yup.object().shape( { ..._baseSchema } );
