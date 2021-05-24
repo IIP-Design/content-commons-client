8d954d395ed0e40023137aaed6545990f7b6629d
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-05-24 13:04:38 -0400

    refactor: route packages to package/playbook and package/guidance routes

---
+++ pages/admin/package/playbook/[id].js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PlaybookEdit from '../../../../components/admin/PlaybookEdit/PlaybookEdit';
+
+/**
+ * 1. Redirect to dashboard if id is not present
+ * 2. Execute a SSR query for initial page population from server
+ * @param {*} props
+ */
+const PlaybookPage = props => {
+  const { query: { id } } = props;
+
+  return <PlaybookEdit id={ id } />;
+};
+
+PlaybookPage.propTypes = {
+  query: PropTypes.object,
+};
+
+export default PlaybookPage;
