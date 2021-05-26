66879796b7f297e75c9398922765c3a129cc12cf
Edwin Mah <edwinmah@users.noreply.github.com>
2021-05-26 12:16:21 -0400

    test: update tests for TextEditor

--- components/admin/TextEditor/TextEditor.test.js
+++ components/admin/TextEditor/TextEditor.test.js
@@ -1,16 +1,49 @@
 import { mount } from 'enzyme';
 import TextEditor from './TextEditor';
 
+jest.mock(
+  'lib/hooks/useCKEditor',
+  () => jest.fn( () => ( {
+    isEditorLoaded: true,
+    CKEditor: () => '',
+    Editor: () => '',
+  } ) ),
+);
+
 describe( '<TextEditor />', () => {
   let Component;
   let wrapper;
 
+  const props = {
+    id: 'playbook123',
+    content: '<p>test content</p>',
+  };
+
   beforeEach( () => {
-    Component = <TextEditor />;
+    Component = <TextEditor { ...props } />;
     wrapper = mount( Component );
   } );
 
   it( 'renders without crashing', () => {
     expect( wrapper.exists() ).toEqual( true );
+  } );
+
+  it( 'renders an accessible region label', () => {
+    const section = wrapper.find( 'section[aria-label="Body"]' );
+
+    expect( section.exists() ).toEqual( true );
+  } );
+
+  it( 'renders a region heading', () => {
+    const section = wrapper.find( 'section[aria-label="Body"]' );
+    const heading = section.find( 'h2' );
+
+    expect( heading.contains( 'Body' ) ).toEqual( true );
+  } );
+
+  it( 'renders the CKEditor', () => {
+    const editor = wrapper.find( 'CKEditor' );
+
+    expect( editor.exists() ).toEqual( true );
   } );
 } );
