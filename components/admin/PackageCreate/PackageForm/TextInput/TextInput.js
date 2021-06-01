90fddd34afa59d68206826af3da8becca7de618c
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-06-01 09:11:23 -0400

    feat: added optional character count to TextInput component

--- components/admin/PackageCreate/PackageForm/TextInput/TextInput.js
+++ components/admin/PackageCreate/PackageForm/TextInput/TextInput.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useField } from 'formik';
+
 import styles from './TextInput.module.scss';
 
 const TextArea = props => (
@@ -11,20 +12,54 @@ const Input = props => (
   <input autoComplete="off" { ...props } />
 );
 
-const TextInput = ( { label, ...props } ) => {
+const TextInput = ( { label, helperTxt, maxLength, ...props } ) => {
+  const [chars, setChars] = useState( 0 );
   const [field, meta] = useField( props.name );
+
   const Component = props.type === 'text' ? Input : TextArea;
 
+  /**
+   * Intercept and forward change event to formik onchange handler
+   * to add additional logic to handle text character count. Could
+   * receive additional change handler as a prop to make component
+   * a bit more flexible
+   * @param {object} native event obj
+   */
+  const handleOnChange = e => {
+    field.onChange( e );
+    setChars( e.target.value.length );
+  };
+
+  // append additional props to forward to native input control
+  const _props = {
+    ...props,
+    'aria-describedby': `describedby${props.id}`,
+    onChange: handleOnChange,
+    maxLength,
+  };
+
   return (
-    <div className={ styles.textInput }>
+    <div className={ styles['text-input'] }>
       <label htmlFor={ props.id }>
         <span className={ props.required ? styles.required : '' }>{ label }</span>
         <Component
           { ...field }
-          { ...props }
+          { ..._props }
           { ...( meta.touched && meta.error && { 'aria-invalid': 'true' } ) }
         />
       </label>
+      <div className={ styles['text-input-helper'] }>
+        <p id={ `describedby${props.id}` } className={ styles['field__helper-text'] }>{ helperTxt }</p>
+        { !!maxLength && (
+          <p
+            aria-live="polite"
+            className={ `${styles['field__helper-text']} ${chars === maxLength && styles['max-char']}` }
+          >
+            { `${chars} of ${maxLength} characters` }
+          </p>
+        ) }
+      </div>
+
       <p aria-live="polite" className={ styles.required_error }>{ meta.touched ? meta.error : '' }</p>
     </div>
   );
@@ -36,6 +71,8 @@ TextInput.propTypes = {
   type: PropTypes.string,
   required: PropTypes.bool,
   label: PropTypes.string,
+  helperTxt: PropTypes.string,
+  maxLength: PropTypes.number,
 };
 
 export default TextInput;
