25318df495bcf30a533b6ceb457e504226c57b3b
Edwin Mah <edwinmah@users.noreply.github.com>
2021-06-04 12:03:51 -0400

    refactor: switch to catch all dynamic route for playbook

--- pages/admin/package/playbook/[...slug].js
+++ pages/admin/package/playbook/[...slug].js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PlaybookEdit from '../../../../components/admin/PlaybookEdit/PlaybookEdit';
+import PlaybookEdit from 'components/admin/PlaybookEdit/PlaybookEdit';
 
 /**
  * 1. Redirect to dashboard if id is not present
  * 2. Execute a SSR query for initial page population from server
  * @param {*} props
  */
-const PlaybookPage = props => {
-  const { query: { id } } = props;
+const PlaybookPage = ( { query } ) => {
+  const [id] = query.slug;
 
   return <PlaybookEdit id={ id } />;
 };
