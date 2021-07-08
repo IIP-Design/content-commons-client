e98e8e96f6a0b5e36aaa125db4781e4866f1d53e
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-07-08 16:00:31 -0400

    fix: remove border on parent container when sortby dropdown is not displayed

--- components/Results/ResultsHeader/ResultsHeader.js
+++ components/Results/ResultsHeader/ResultsHeader.js
@@ -70,7 +70,7 @@ const ResultsHeader = ( {
     <div>
       <div className={ styles.controls }>
         <div className={ styles.sortResults }>
-          <div className={ styles.fields }>
+          <div className={ `${styles.fields} ${viewingAllPkgs ? styles['no-border'] : ''}` }>
             { !viewingAllPkgs && (
               <Fragment>
                 <span aria-hidden="true">Sort by</span>
